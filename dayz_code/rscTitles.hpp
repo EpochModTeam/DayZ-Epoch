e24e65cd53930ddca72331253b5e9697850eea23
vbawol <awol@veteranbastards.com>
2013-02-12 16:47:47 -0600

    version bump

--- dayz_code/rscTitles.hpp
+++ dayz_code/rscTitles.hpp
@@ -87,7 +87,7 @@ class RscDisplayMain : RscStandardDisplay
 		class DAYZ_Version : CA_Version
 		{
 			idc = -1;
-			text = "DayZ Epoch 0.942 (1.7.5.1)";
+			text = "DayZ Epoch 0.943 (1.7.5.1)";
 			y = "(SafeZoneH + SafeZoneY) - (1 - 0.95)";
 		};
 		class CA_TitleMainMenu;
