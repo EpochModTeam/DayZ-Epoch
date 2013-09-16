f674a921fed1cf0f289de97b202a650886ef97ca
[VB]AWOL <vbawol@veteranbastards.com>
2013-09-15 22:57:26 -0500

    1.0.2.17 dev test

--- SQF/dayz_code/Configs/rscTitles.hpp
+++ SQF/dayz_code/Configs/rscTitles.hpp
@@ -149,7 +149,7 @@ class RscDisplayMain : RscStandardDisplay
 		class DAYZ_Version : CA_Version
 		{
 			idc = -1;
-			text = "DayZ Epoch 1.0.2.2 (DEV16)";
+			text = "DayZ Epoch 1.0.2.2 (DEV17)";
 			y = "(SafeZoneH + SafeZoneY) - (1 - 0.95)";
 		};
 		delete CA_TitleMainMenu;
