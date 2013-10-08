a1f2511379470d1f2c399cf48e52d0fe0eae4a2f
[VB]AWOL <vbawol@veteranbastards.com>
2013-10-08 01:19:54 -0500

    version bump

--- SQF/dayz_code/config.cpp
+++ SQF/dayz_code/config.cpp
@@ -41,7 +41,7 @@ class CfgMods
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.dayzepoch.com";
-		version = "1.0.2.393";
+		version = "1.0.2.396";
 		hiveVersion = 0.96; //0.93
 	};
 };
