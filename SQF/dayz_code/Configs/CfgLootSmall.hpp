ff9de44411bef8eb5927a5418f3a99484787aa0e
[VB]AWOL <vbawol@veteranbastards.com>
2013-10-10 13:33:52 -0500

    added clothes back to small spawns

--- SQF/dayz_code/Configs/CfgLootSmall.hpp
+++ SQF/dayz_code/Configs/CfgLootSmall.hpp
@@ -438,31 +438,104 @@ class CfgLootSmall {
 			0.02
 		}
 	};
-	
 	clothes[] = {
-		{ },
-		{ }
+		{
+			"Skin_RU_Policeman_DZ",
+			"Skin_Pilot_EP1_DZ",
+			"Skin_Functionary1_EP1_DZ",
+			"Skin_Priest_DZ",
+			"Skin_Rocker1_DZ",
+			"Skin_Rocker2_DZ",
+			"Skin_Rocker3_DZ",
+			"Skin_Rocker4_DZ",
+			"Skin_SurvivorW3_DZ",
+			"Skin_SurvivorWpink_DZ",
+			"Skin_SurvivorWurban_DZ",
+			//"Skin_SurvivorWsequisha_DZ",
+			//"Skin_SurvivorWsequishaD_DZ",
+		},
+		{
+			0.13,
+			0.12,
+			0.1,
+			0.1,
+			0.07,
+			0.07,
+			0.08,
+			0.08,
+			0.09,
+			0.09,
+			0.07,
+			//0.04,
+			//0.02
+		}
 	};
-	
 	militaryclothes[] = {
-		{ },
-		{ }
+		{
+			"Skin_Camo1_DZ",
+			"Skin_Sniper1_DZ",
+			"Skin_Rocket_DZ",
+			"Skin_Soldier1_DZ",
+			"Skin_Drake_Light_DZ",
+			"Skin_Soldier_TL_PMC_DZ",
+			"Skin_Soldier_Sniper_PMC_DZ",
+			"Skin_Soldier_Bodyguard_AA12_PMC_DZ",
+			"Skin_CZ_Special_Forces_GL_DES_EP1_DZ",
+			"Skin_FR_OHara_DZ",
+			"Skin_FR_Rodriguez_DZ",
+			"Skin_CZ_Soldier_Sniper_EP1_DZ",
+			"Skin_Graves_Light_DZ"
+		},
+		{
+			0.1,
+			0.08,
+			0.05,
+			0.13,
+			0.07,
+			0.07,
+			0.05,
+			0.07,
+			0.08,
+			0.08,
+			0.09,
+			0.04,
+			0.09
+		}
 	};
 	specialclothes[] = {
-		{ },
-		{ }
-	};
-	
-	tents[] = {
-		{ },
-		{ }
-	};
-	backpacks[] = {
-		{ },
-		{ }
-	};
-	militarybackpacks[] = {
-		{ },
-		{ }
+		{
+			"Skin_Ins_Soldier_GL_DZ",
+			"Skin_GUE_Commander_DZ",
+			"Skin_Bandit1_DZ",
+			"Skin_Bandit2_DZ",
+			"Skin_BanditW1_DZ",
+			"Skin_BanditW2_DZ",
+			"Skin_TK_INS_Soldier_EP1_DZ",
+			"Skin_TK_INS_Warlord_EP1_DZ",
+			"Skin_SurvivorWcombat_DZ",
+			"Skin_SurvivorWdesert_DZ",
+			"Skin_GUE_Soldier_MG_DZ",
+			"Skin_GUE_Soldier_Sniper_DZ",
+			"Skin_GUE_Soldier_Crew_DZ",
+			"Skin_GUE_Soldier_CO_DZ",
+			"Skin_GUE_Soldier_2_DZ",
+		},
+		{
+			0.1,
+			0.08,
+			0.13,
+			0.06,
+			0.07,
+			0.07,
+			0.05,
+			0.07,
+			0.08,
+			0.08,
+			0.05,
+			0.04,
+			0.04,
+			0.04,
+			0.04
+		}
 	};
 };
