10bcf802ca665f0d5e1b5f8812e7a9e627468385
[VB]AWOL <vbawol@veteranbastards.com>
2013-10-10 14:58:33 -0500

    documents

--- SQF/dayz_code/Configs/CfgMagazines.hpp
+++ SQF/dayz_code/Configs/CfgMagazines.hpp
@@ -159,12 +159,12 @@ class CfgMagazines {
 		{
 			class Crafting
 			{
-				text = "Open Document";
+				text = "Examine";
 				script = ";['Crafting','CfgMagazines', _id] spawn player_craftItem;";
 				neednearby[] = {};
 				requiretools[] = {};
 				randomOutput = 1;
-				output[] = {{"ItemLetter",1},{"ItemDocumentRamp",1}};
+				output[] = {{"ItemLetter",1},{"ItemDocumentRamp",1},{"ItemBook1",1},{"ItemBook2",1},{"ItemBook3",1},{"ItemBook4",1},{"ItemNewspaper",1}};
 				input[] = {{"ItemDocument",1}};
 			};
 		};
@@ -181,12 +181,67 @@ class CfgMagazines {
 		descriptionShort = "Random letter";
 		weight = 0.1;
 	};
+	class ItemBook1: CA_Magazine
+	{
+		scope = 2;
+		count = 1;
+		type = 256;
+		displayName = "Romance Novel";
+		model = "\z\addons\dayz_epoch\models\doc_trashy1.p3d";
+		picture = "\z\addons\dayz_epoch\pictures\equip_trashy1_ca.paa";
+		descriptionShort = "Trashy Romance Novel - ""The seas are laden with lust and jealousy.""";
+		weight = 0.1;
+	};
+	class ItemBook2: CA_Magazine
+	{
+		scope = 2;
+		count = 1;
+		type = 256;
+		displayName = "Romance Novel";
+		model = "\z\addons\dayz_epoch\models\doc_trashy2.p3d";
+		picture = "\z\addons\dayz_epoch\pictures\equip_trashy2_ca.paa";
+		descriptionShort = "Trashy Romance Novel - ""He had a chest of iron, and a heart of gold.""";
+		weight = 0.1;
+	};
+	class ItemBook3: CA_Magazine
+	{
+		scope = 2;
+		count = 1;
+		type = 256;
+		displayName = "Book";
+		model = "\z\addons\dayz_epoch\models\doc_child1.p3d";
+		picture = "\z\addons\dayz_epoch\pictures\equip_child1_ca.paa";
+		descriptionShort = "Random Children's Book";
+		weight = 0.1;
+	};
+	class ItemBook4: CA_Magazine
+	{
+		scope = 2;
+		count = 1;
+		type = 256;
+		displayName = "Book";
+		model = "\z\addons\dayz_epoch\models\doc_child2.p3d";
+		picture = "\z\addons\dayz_epoch\pictures\equip_child2_ca.paa";
+		descriptionShort = "Random Children's Book";
+		weight = 0.1;
+	};
+	class ItemNewspaper: CA_Magazine
+	{
+		scope = 2;
+		count = 1;
+		type = 256;
+		displayName = "Torn Newspaper";
+		model = "\z\addons\dayz_epoch\models\doc_deaths.p3d";
+		picture = "\z\addons\dayz_epoch\pictures\equip_deaths_ca.paa";
+		descriptionShort = "Torn Newspaper - ""The recent obituaries are stil legible...""";
+		weight = 0.1;
+	};
 	class ItemDocumentRamp: CA_Magazine
 	{
 		scope = 2;
 		count = 1;
 		type = 256;
-		displayName = "HowTo: Ramp";
+		displayName = "HowTo: Wood Ramp";
 		model = "\z\addons\dayz_epoch\models\doc_ramp.p3d";
 		picture = "\z\addons\dayz_epoch\pictures\equip_doc_ramp_ca.paa";
 		descriptionShort = "Document used in building a wooden ramp.";
