b7d6be0675271c4a1f7effb39848a63746ef6af4
dayz10k <dayz10000@gmail.com>
2013-11-18 10:14:12 -0500

    #709 Add Stringtable localization for Epoch toolbelt items

--- SQF/dayz_code/Configs/CfgWeapons/Item/ItemFishingPole.hpp
+++ SQF/dayz_code/Configs/CfgWeapons/Item/ItemFishingPole.hpp
@@ -1,15 +1,15 @@
 class ItemFishingPole: ItemCore
 {
 	scope=2;
-	displayName="Fishing Pole";
+	displayName="STR_EPOCH_PLAYER_301";
 	model="\z\addons\dayz_epoch\models\fishing_rod_loot_item.p3d";
 	picture="\z\addons\dayz_epoch\pictures\equip_fishingpole_CA.paa";
-	descriptionShort="Fishing Pole: This carbon fiber rod may be old but, she still works like a charm.";
+	descriptionShort="STR_EPOCH_PLAYER_302";
 	class ItemActions
 	{
 		class Toolbelt
 		{
-			text="Remove from Toolbelt";
+			text="STR_EPOCH_PLAYER_299";
 			script="spawn player_addToolbelt;";
 			use[]=
 			{
