7d4f3be4551379a5e21564045f1e653ae5504b37
dayz10k <dayz10000@gmail.com>
2013-11-18 10:44:44 -0500

    #709 Add Stringtable localization for Epoch toolbelt items

--- SQF/dayz_code/Configs/CfgWeapons/Melee/ItemCrowbar.hpp
+++ SQF/dayz_code/Configs/CfgWeapons/Melee/ItemCrowbar.hpp
@@ -1,15 +1,15 @@
 class ItemCrowbar: ItemCore
 	{
 		scope=2;
-		displayName=$STR_EPOCH_PLAYER_303;
+		displayName=$STR_EQUIP_CROWBAR;
 		model="\dayz_equip\models\crowbar.p3d";
 		picture="\z\addons\dayz_communityassets\pictures\equip_crowbar_CA.paa";
-		descriptionShort=$STR_EPOCH_PLAYER_304;
+		descriptionShort=$STR_EQUIP_CROWBAR_DESC;
 		class ItemActions
 		{
 			class Toolbelt
 			{
-				text = "STR_EPOCH_PLAYER_298";
+				text = "STR_EPOCH_PLAYER_295";
 				script="spawn player_addToolbelt;";
 				use[]=
 				{
