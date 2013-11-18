869d7208d881b5b592b86c0cb45d59fcfb526f51
dayz10k <dayz10000@gmail.com>
2013-11-18 10:15:26 -0500

    Update ItemFishingPole.hpp

--- SQF/dayz_code/Configs/CfgWeapons/Item/ItemFishingPole.hpp
+++ SQF/dayz_code/Configs/CfgWeapons/Item/ItemFishingPole.hpp
@@ -1,10 +1,10 @@
 class ItemFishingPole: ItemCore
 {
 	scope=2;
-	displayName="STR_EPOCH_PLAYER_301";
+	displayName=$STR_EPOCH_PLAYER_301;
 	model="\z\addons\dayz_epoch\models\fishing_rod_loot_item.p3d";
 	picture="\z\addons\dayz_epoch\pictures\equip_fishingpole_CA.paa";
-	descriptionShort="STR_EPOCH_PLAYER_302";
+	descriptionShort=$STR_EPOCH_PLAYER_302;
 	class ItemActions
 	{
 		class Toolbelt
