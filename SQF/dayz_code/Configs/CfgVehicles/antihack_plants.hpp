b5f148cece756c96047d998caf6f24ddbd6e8fec
facoptere <facoptere@gmail.com>
2013-11-18 17:43:11 +0100

    Configs: antihack for logic and plants

---
+++ SQF/dayz_code/Configs/CfgVehicles/antihack_plants.hpp
@@ -0,0 +1,90 @@
+
+	// check that plants libs are properly loaded
+	// thanks to Tansien
+	class faplants : House {};
+	class grass: faplants 
+	{
+			model = "\ca\data\cl_grass1.p3d";
+			displayName = "grass";
+			vehicleClass = "grass";
+			class AnimationSources {};
+	};
+
+	class prunus: faplants 
+	{
+			model = "\ca\plants2\bush\b_prunus.p3d";
+			displayName = "prunus";
+			vehicleClass = "prunus";
+			class AnimationSources {};
+	};
+
+	class picea: faplants 
+	{
+			model = "\ca\plants2\clutter\c_picea.p3d";
+			displayName = "picea";
+			vehicleClass = "picea";
+			class AnimationSources {};
+	};
+
+	class fallentree: faplants 
+	{
+			model = "\ca\plants2\misc\misc_fallentree1.p3d";
+			displayName = "fallentree";
+			vehicleClass = "fallentree";
+			class AnimationSources {};
+	};
+
+	class phragmites: faplants 
+	{
+			model = "\ca\plants2\plant\p_phragmites.p3d";
+			displayName = "phragmites";
+			vehicleClass = "phragmites";
+			class AnimationSources {};
+	};
+
+	class acer: faplants 
+	{
+			model = "\ca\plants2\tree\t_acer2s.p3d";
+			displayName = "acer";
+			vehicleClass = "acer";
+			class AnimationSources {};
+	};
+
+	class amygdalusn: faplants 
+	{
+			model = "\ca\plants_e\bush\b_amygdalusn1s_ep1.p3d";
+			displayName = "amygdalusn";
+			vehicleClass = "amygdalusn";
+			class AnimationSources {};
+	};
+
+	class Brush: faplants 
+	{
+			model = "\ca\plants_E\Clutter\c_Brush_Hard_EP1.p3d";
+			displayName = "Brush";
+			vehicleClass = "Brush";
+			class AnimationSources {};
+	};
+
+	class fiberplant: faplants 
+	{
+			model = "\ca\plants_e\plant\p_fiberplant_ep1.p3d";
+			displayName = "fiberplant";
+			vehicleClass = "fiberplant";
+			class AnimationSources {};
+	};
+
+	class amygdalusc: faplants 
+	{
+			model = "\ca\plants_e\tree\t_amygdalusc2s_ep1.p3d";
+			displayName = "amygdalusc";
+			vehicleClass = "amygdalusc";
+			class AnimationSources {};
+	};
+	class boulder: faplants 
+	{
+			model = "\ca\rocks2\R2_Boulder1.p3d";
+			displayName = "boulder";
+			vehicleClass = "boulder";
+			class AnimationSources {};
+	};
