7b67014fba283d7509307d327c9c0f47c389e5e6
dayz10k <dayz10000@gmail.com>
2013-11-18 10:11:42 -0500

    #709 Add Stringtable localization for Epoch toolbelt items

--- SQF/dayz_code/Configs/CfgWeapons/Item/Flashlight.hpp
+++ SQF/dayz_code/Configs/CfgWeapons/Item/Flashlight.hpp
@@ -9,7 +9,7 @@ class MeleeFlashlight : Pistol {
 	class ItemFlashlight : ItemCore {
 		class ItemActions {
 			class Toolbelt {
-				text = "Remove from toolbelt";
+				text = "STR_EPOCH_PLAYER_299";
 			};
 		};
 	}; 
@@ -25,7 +25,7 @@ class MeleeFlashlight : Pistol {
 	class ItemFlashlightRed : ItemCore {
 		class ItemActions {
 			class Toolbelt {
-				text = "Remove from toolbelt";
+				text = "STR_EPOCH_PLAYER_299";
 			};
 		};
 	};
