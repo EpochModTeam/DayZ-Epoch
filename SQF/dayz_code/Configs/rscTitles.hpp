4b7167154060fa8e4d4e6fe5189c5908748c271e
Skaronator <DayZ@Skaronator.com>
2013-11-24 22:17:05 +0100

    Could fix Directly Disconnect, not sure because Private Repo is 404

--- SQF/dayz_code/Configs/rscTitles.hpp
+++ SQF/dayz_code/Configs/rscTitles.hpp
@@ -70,13 +70,21 @@ class RscDisplayDebriefing: RscStandardDisplay
 		delete CA_DebriefingStatsGroup;
 		delete ButtonStatistics;
 		delete ButtonRetry;
-		//delete ButtonContinue;
+		class ButtonContinue: RscIGUIShortcutButton
+		{
+			idc = 2;
+			shortcuts[] = {"0x00050000 + 0",28,57,156};
+			x = 0.4080875;
+			y = 0.863977;
+			text = $STR_UI_CONTINUE;
+		};
 	};
 	class ControlsBackground
 	{
 		delete Mainback;
 	};
 };
+
 class RscDisplayMissionFail: RscStandardDisplay
 {
 	onLoad = "ctrlActivate ((_this select 0) displayCtrl 2)";
@@ -91,7 +99,14 @@ class RscDisplayMissionFail: RscStandardDisplay
 		delete CA_DebriefingObjectivesGroup;
 		delete CA_DebriefingStatsGroup;
 		delete BRetry;
-		//delete BAbort;
+		class BAbort: RscIGUIShortcutButton
+		{
+			idc = 2;
+			shortcuts[] = {"0x00050000 + 0",28,57,156};
+			x = 0.4080875;
+			y = 0.863977;
+			text = $STR_UI_END;
+		};
 	};
 	class ControlsBackground
 	{
