c43caf7ab64f1c63ee189ad995a0bfb62aaf2db7
[VB]AWOL <vbawol@veteranbastards.com>
2013-12-12 14:24:38 -0600

    SMD sara example configs

---
+++ Server Files/Configs/DayZ_Epoch_instance_22_SMDsahrani.bat
@@ -0,0 +1,2 @@
+@echo off
+start "arma2" /min "Expansion\beta\arma2oaserver.exe" -port=2345 "-config=instance_22_SMDsahrani\config.cfg" "-cfg=instance_22_SMDsahrani\basic.cfg" "-profiles=instance_22_SMDsahrani" -name=instance_22_SMDsahrani "-mod=@smd_sahrani_a2;@DayZ_Epoch;@DayZ_Epoch_Server;"
