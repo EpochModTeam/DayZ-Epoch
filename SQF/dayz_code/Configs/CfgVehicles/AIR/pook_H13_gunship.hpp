5c62ac3c6bca3955d0d11aeec5e6d06b866584b9
Zac Surplice <me@zacsurplice.co>
2013-12-23 14:32:17 +1100

    reclass pooks

---
+++ SQF/dayz_code/Configs/CfgVehicles/AIR/pook_H13_gunship.hpp
@@ -0,0 +1,69 @@
+class pook_H13_gunship;
+class pook_H13_gunship_DZE : pook_H13_gunship {
+	scope = 0;
+	side = 1;
+	faction = "USMC";
+	crew = "";
+	typicalCargo[] = {};
+	magazines[] = {};
+};
+
+class pook_H13_gunship_CDF;
+class pook_H13_gunship_CDF_DZE : pook_H13_gunship_CDF {
+	scope = 0;
+	side = 1;
+	faction = "USMC";
+	crew = "";
+	typicalCargo[] = {};
+	magazines[] = {};
+};
+
+class pook_H13_gunship_UNO;
+class pook_H13_gunship_UNO_DZE : pook_H13_gunship_UNO {
+	scope = 0;
+	side = 1;
+	faction = "USMC";
+	crew = "";
+	typicalCargo[] = {};
+	magazines[] = {};
+};
+
+class pook_H13_gunship_PMC;
+class pook_H13_gunship_PMC_DZE : pook_H13_gunship_PMC {
+	scope = 0;
+	side = 1;
+	faction = "USMC";
+	crew = "";
+	typicalCargo[] = {};
+	magazines[] = {};
+};
+
+class pook_H13_gunship_GUE;
+class pook_H13_gunship_GUE_DZE : pook_H13_gunship_GUE {
+	scope = 0;
+	side = 1;
+	faction = "USMC";
+	crew = "";
+	typicalCargo[] = {};
+	magazines[] = {};
+};
+
+class pook_H13_gunship_TAK;
+class pook_H13_gunship_TAK_DZE : pook_H13_gunship_TAK {
+	scope = 0;
+	side = 1;
+	faction = "USMC";
+	crew = "";
+	typicalCargo[] = {};
+	magazines[] = {};
+};
+
+class pook_H13_gunship_INS;
+class pook_H13_gunship_INS_DZE : pook_H13_gunship_INS {
+	scope = 0;
+	side = 1;
+	faction = "USMC";
+	crew = "";
+	typicalCargo[] = {};
+	magazines[] = {};
+};
