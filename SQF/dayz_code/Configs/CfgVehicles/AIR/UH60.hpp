2d5ff63b2eb79e002ddc4814559a625a1c227924
Florian Kinder <Florian.Kinder@mcl.de>
2014-01-13 19:35:23 +0100

    Added MH60S_DZ & MH60S_AL

--- SQF/dayz_code/Configs/CfgVehicles/AIR/UH60.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/AIR/UH60.hpp
@@ -1,4 +1,4 @@
-class UH60_Base: Helicopter 
+class UH60_Base: Helicopter
 {
 	scope = 0;
 	side = 1;
@@ -13,6 +13,303 @@ class UH60_Base: Helicopter
 	lockDetectionSystem = "8 + 4";
 	incommingMisslieDetectionSystem = 0;
 };
+class MH60S_DZ : UH60_Base
+{
+	crew = "";
+	typicalCargo[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	transportMaxWeapons = 10;
+	transportMaxMagazines = 100;
+	transportMaxBackpacks = 5;
+	scope = 2;
+	accuracy = 1.5;	// accuracy needed to recognize type of this target
+	displayName = "MH60S DZ";
+	class Turrets : Turrets
+	{
+		class MainTurret : MainTurret
+		{
+			body = "mainTurret";
+			gun = "mainGun";
+			minElev = -80;
+			maxElev = 25;
+			initElev = -80;
+			minTurn = 30;
+			maxTurn = 150;
+			initTurn = 90;
+			soundServo[] = {"",0.01,1};
+			stabilizedInAxes = "StabilizedInAxesNone";
+			gunBeg = "muzzle_1";	// endpoint of the gun
+			gunEnd = "chamber_1";	// chamber of the gun
+			weapons[] = {"M240_veh"};
+			magazines[] = {"100Rnd_762x51_M240", "100Rnd_762x51_M240", "100Rnd_762x51_M240"};
+			gunnerName = $STR_POSITION_CREWCHIEF;
+			gunnerOpticsModel = "\ca\weapons\optika_empty";
+			gunnerOutOpticsShowCursor = 1;
+			gunnerOpticsShowCursor = 1;
+			gunnerAction = "MH60_Gunner";
+			gunnerInAction = "MH60_Gunner";
+			primaryGunner = 1;
+			class ViewOptics
+			{
+				initAngleX = 0;
+				minAngleX = -30;
+				maxAngleX = 30;
+				initAngleY = 0;
+				minAngleY = -100;
+				maxAngleY = 100;
+				initFov = 0.7;
+				minFov = 0.25;
+				maxFov = 1.1;
+			};
+			gunnerCompartments = "Compartment2";
+		};
+
+		class RightDoorGun : MainTurret {
+			body = "Turret_2";
+			gun = "Gun_2";
+			animationSourceBody = "Turret_2";
+			animationSourceGun = "Gun_2";
+			weapons[] = {"M240_veh_2"};
+			animationSourceHatch = "";
+			selectionFireAnim = "zasleh_1";
+			proxyIndex = 2;
+			gunnerName = $STR_POSITION_DOORGUNNER;
+			commanding = -2;
+			minTurn = -150;
+			maxTurn = -30;
+			initTurn = -90;
+			stabilizedInAxes = "StabilizedInAxesNone";
+			gunBeg = "muzzle_2";	// endpoint of the gun
+			gunEnd = "chamber_2";	// chamber of the gun
+			primaryGunner = 0;
+			gunnerCompartments = "Compartment2";
+			memoryPointGun = "machinegun_2";
+			memoryPointGunnerOptics = "gunnerview_2";
+		};
+	};
+	class AnimationSources : AnimationSources
+	{
+		class ReloadAnim {
+			source = "reload";
+			weapon = "M240_veh";
+		};
+
+		class ReloadMagazine {
+			source = "reloadmagazine";
+			weapon = "M240_veh";
+		};
+
+		class Revolving {
+			source = "revolving";
+			weapon = "M240_veh";
+		};
+
+		class ReloadAnim_2 {
+			source = "reload";
+			weapon = "M240_veh_2";
+		};
+
+		class ReloadMagazine_2 {
+			source = "reloadmagazine";
+			weapon = "M240_veh_2";
+		};
+
+		class Revolving_2 {
+			source = "revolving";
+			weapon = "M240_veh_2";
+		};
+	};
+
+	destrType = "DestructWreck";
+	vehicleClass = "Air";
+	model = "\ca\air\Mh_60mg";
+	mapSize = 17;
+	picture = "\ca\air\data\ico\MH_60mg_CA.paa";
+	Icon = "\ca\air\data\map_ico\icomap_MH60mg_CA.paa";
+	side = TWest;
+	faction = USMC;
+	attendant = true;
+	maxSpeed = 270;	// max speed on level road, km/h
+	armor = 35;
+	damageResistance = 0.00242;
+	cost = 10000000;
+	mainRotorSpeed = -1;
+	soundGetIn[] = {"\ca\Sounds\Air\Noises\heli_door_01", db-10, 1};
+	soundGetOut[] = {"\ca\Sounds\Air\Noises\heli_door_01", db-10, 1, 30};
+	soundEnviron[] = {"", db-30, 1.0};
+	soundDammage[] = {"\ca\Air\Data\Sound\alarm_loop1", 0.001, 1};
+	soundEngineOnInt[] = {"ca\sounds\Air\UH1Y\int\int-start-final", 0.1, 1.0};
+	soundEngineOnExt[] = {"ca\sounds\Air\UH1Y\ext\ext-motor-start", 1.0, 1.0, 800};
+	soundEngineOffInt[] = {"ca\sounds\Air\UH1Y\int\int-stop-final", 0.1, 1.0};
+	soundEngineOffExt[] = {"ca\sounds\Air\UH1Y\ext\ext-motor-stop", 1.0, 1.0, 800};
+
+	class Sounds {
+		class Engine {
+			sound[] = {"ca\sounds\air\uh1y\ext\ext-fly-mode2", 2.51189, 1.0, 900};
+			frequency = "rotorSpeed";
+			volume = "camPos*((rotorSpeed-0.72)*4)";
+		};
+
+		class RotorLowOut {
+			sound[] = {"ca\sounds\air\uh1y\int\int-rotor-single5b", 2.51189, 1.0, 1400};
+			frequency = "rotorSpeed";
+			volume = "camPos*(0 max (rotorSpeed-0.1))";
+			cone[] = {1.6, 3.14, 2.0, 0.5};
+		};
+
+		class RotorHighOut {
+			sound[] = {"ca\sounds\air\uh1y\int\int-rotor-single5a", 2.51189, 1.0, 1600};
+			frequency = "rotorSpeed";
+			volume = "camPos*10*(0 max (rotorThrust-0.9))";
+			cone[] = {1.6, 3.14, 2.0, 0.5};
+		};
+
+		class EngineIn {
+			sound[] = {"ca\sounds\air\uh1y\int\int-fly-mode7", db10, 1.0};
+			frequency = "rotorSpeed";
+			volume = "(1-camPos)*((rotorSpeed-0.75)*4)";
+		};
+
+		class RotorLowIn {
+			sound[] = {"ca\sounds\air\uh1y\int\int-rotor-single5b", db10, 1.0};
+			frequency = "rotorSpeed";
+			volume = "2*(1-camPos)*((rotorSpeed factor[0.3, 1.1]) min (rotorSpeed factor[1.1, 0.3]))";
+		};
+
+		class RotorHighIn {
+			sound[] = {"ca\sounds\air\uh1y\int\int-rotor-single5a", db10, 1.0};
+			frequency = "rotorSpeed";
+			volume = "(1-camPos)*3*(rotorThrust-0.9)";
+		};
+	};
+	driverInAction = "UH60_Pilot";
+	driverAction = "UH60_Pilot";
+	cargoAction[] = {"UH60_Cargo01", "UH1Y_Cargo02", "UH1Y_Cargo01", "UH1Y_Cargo01", "UH1Y_Cargo01", "UH60_Cargo02"};
+	transportSoldier = 13;
+	driverCompartments = "Compartment1";
+	cargoCompartments[] = {"Compartment2"};
+	enableSweep = false;
+	transportAmmo = 0;
+	supplyRadius = 2.5;
+	initCargoAngleY = 10;
+	minCargoAngleY = -60;
+	maxCargoAngleY = 120;
+	type = VAir;
+
+	// threat (VSoft, VArmor, VAir), how threatening vehicle is to unit types
+	threat[] = {0.3, 1, 0.4};
+	cargoIsCoDriver[] = {1, 0, 0};
+	weapons[] = {};
+	magazines[] = {};
+	forceHideDriver = true;
+	castDriverShadow = true;
+	castCargoShadow = true;
+
+	class Library {
+		libTextDesc = $STR_LIB_MH60S;
+	};
+
+	class Damage {
+		tex[] = {};
+		mat[] = {"ca\air\Data\uh60_skla.rvmat", "ca\air\Data\uh60_skla_damage.rvmat", "ca\air\Data\uh60_skla_damage.rvmat", "ca\air\Data\uh60_skla_in.rvmat", "ca\air\Data\uh60_skla_in_damage.rvmat", "ca\air\Data\uh60_skla_in_damage.rvmat", "ca\air\Data\materialy\uh60_01.rvmat", "ca\air\Data\materialy\uh60_01_damage.rvmat", "ca\air\Data\materialy\uh60_01_destruct.rvmat"};
+	};
+	enableManualFire = 0;
+
+	class Reflectors {
+		class Left {
+			color[] = {0.8, 0.8, 1.0, 1.0};
+			ambient[] = {0.07, 0.07, 0.07, 1.0};
+			position = "L svetlo";
+			direction = "konec L svetla";
+			hitpoint = "L svetlo";
+			selection = "L svetlo";
+			size = 1;
+			brightness = 1.0;
+		};
+
+		class Right {
+			color[] = {0.8, 0.8, 1.0, 1.0};
+			ambient[] = {0.07, 0.07, 0.07, 1.0};
+			position = "P svetlo";
+			direction = "konec P svetla";
+			hitpoint = "P svetlo";
+			selection = "P svetlo";
+			size = 1;
+			brightness = 1.0;
+		};
+	};
+};
+class MH60S_DZE : UH60_Base
+{
+	displayName = "MH60S AL";
+	class Turrets : Turrets
+	{
+		class MainTurret : MainTurret
+		{
+			body = "mainTurret";
+			gun = "mainGun";
+			minElev = -80;
+			maxElev = 25;
+			initElev = -80;
+			minTurn = 30;
+			maxTurn = 150;
+			initTurn = 90;
+			soundServo[] = {"",0.01,1};
+			stabilizedInAxes = "StabilizedInAxesNone";
+			gunBeg = "muzzle_1";	// endpoint of the gun
+			gunEnd = "chamber_1";	// chamber of the gun
+			weapons[] = {"M240_veh"};
+			magazines[] = {};
+			gunnerName = $STR_POSITION_CREWCHIEF;
+			gunnerOpticsModel = "\ca\weapons\optika_empty";
+			gunnerOutOpticsShowCursor = 1;
+			gunnerOpticsShowCursor = 1;
+			gunnerAction = "MH60_Gunner";
+			gunnerInAction = "MH60_Gunner";
+			primaryGunner = 1;
+			class ViewOptics
+			{
+				initAngleX = 0;
+				minAngleX = -30;
+				maxAngleX = 30;
+				initAngleY = 0;
+				minAngleY = -100;
+				maxAngleY = 100;
+				initFov = 0.7;
+				minFov = 0.25;
+				maxFov = 1.1;
+			};
+			gunnerCompartments = "Compartment2";
+		};
+
+		class RightDoorGun : MainTurret {
+			body = "Turret_2";
+			gun = "Gun_2";
+			animationSourceBody = "Turret_2";
+			animationSourceGun = "Gun_2";
+			weapons[] = {"M240_veh_2"};
+			animationSourceHatch = "";
+			selectionFireAnim = "zasleh_1";
+			proxyIndex = 2;
+			gunnerName = $STR_POSITION_DOORGUNNER;
+			commanding = -2;
+			minTurn = -150;
+			maxTurn = -30;
+			initTurn = -90;
+			stabilizedInAxes = "StabilizedInAxesNone";
+			gunBeg = "muzzle_2";	// endpoint of the gun
+			gunEnd = "chamber_2";	// chamber of the gun
+			primaryGunner = 0;
+			gunnerCompartments = "Compartment2";
+			memoryPointGun = "machinegun_2";
+			memoryPointGunnerOptics = "gunnerview_2";
+		};
+	};
+}
 class UH60M_base_EP1: UH60_Base
 {
 	expansion = 1;
@@ -164,7 +461,7 @@ class UH60M_US_base_EP1: UH60M_base_EP1
 };
 class UH60M_EP1_DZ: UH60M_US_base_EP1
 {
-	crew = ""; 
+	crew = "";
 	typicalCargo[] = {};
 	class TransportMagazines{};
 	class TransportWeapons{};
@@ -261,7 +558,7 @@ class UH60M_EP1_DZ: UH60M_US_base_EP1
 // AMMO LESS
 class UH60M_EP1_DZE: UH60M_US_base_EP1
 {
-	crew = ""; 
+	crew = "";
 	typicalCargo[] = {};
 	class TransportMagazines{};
 	class TransportWeapons{};
