8cfe02c8a401cd6bc9e9b6125bf42242438ec6de
Florian Kinder <Florian.Kinder@mcl.de>
2014-01-16 13:06:02 +0100

    Revert "Added M249_EP1_DZE"
    This reverts commit d751737de3b3db5dd8744d623ec20985373288cf.

--- SQF/dayz_code/Configs/CfgWeapons.hpp
+++ SQF/dayz_code/Configs/CfgWeapons.hpp
@@ -275,7 +275,6 @@ class CfgWeapons {
 //	#include "CfgWeapons\Weapon\Sniper\DMR_DZ3.hpp"
 	#include "CfgWeapons\Weapon\Pistol\MakerovSD.hpp"
 	#include "CfgWeapons\Weapon\LMG\m240_scoped.hpp"
-	#include "CfgWeapons\Weapon\LMG\M249_EP1.hpp"
 	#include "CfgWeapons\Weapon\LMG\M249_m145_EP1.hpp"
 	#include "CfgWeapons\Weapon\LMG\M60A4.hpp"
 //	#include "CfgWeapons\Weapon\Rifle\FN_FAL_ANPVS4_DZE.hpp"
--- SQF/dayz_code/Configs/CfgWeapons/Weapon/LMG/M249_EP1.hpp
+++
@@ -1,4 +0,0 @@
-class M249_EP1;
-class M249_EP1_DZE : M249_EP1 {
-    type = "1";
-};
