6a983d8684cec248ce691e88a38e3503f0354d92
Florian Kinder <Florian.Kinder@mcl.de>
2014-01-16 13:14:51 +0100

    Fixed M249_DZ for 100Rnd M249 ammo

---
+++ SQF/dayz_code/Configs/CfgWeapons/Weapon/LMG/M249_DZ.hpp
@@ -0,0 +1,4 @@
+class M249_DZ;
+class M249_DZ:M249_DZ {
+	magazines[] = {"100Rnd_556x45_M249", "200Rnd_556x45_M249", "30Rnd_556x45_Stanag", "20Rnd_556x45_Stanag", "30Rnd_556x45_StanagSD", "100Rnd_556x45_BetaCMag", "30Rnd_556x45_G36", "30Rnd_556x45_G36SD"};
+};
