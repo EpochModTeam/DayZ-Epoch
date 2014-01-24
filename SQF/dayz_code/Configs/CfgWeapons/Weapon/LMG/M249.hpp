aca82618360a3d983ae0428585db610fd7349eac
Florian Kinder <Florian.Kinder@mcl.de>
2014-01-24 09:09:12 +0100

    rename M249_DZ to M249_EP1_DZ
    because it is based on M249_EP1

--- SQF/dayz_code/Configs/CfgWeapons/Weapon/LMG/M249.hpp
+++
@@ -1,5 +0,0 @@
-class M249;
-class M249_DZ:M249 {
-	type = "1";
-	magazines[] = {"100Rnd_556x45_M249", "200Rnd_556x45_M249", "30Rnd_556x45_Stanag", "20Rnd_556x45_Stanag", "30Rnd_556x45_StanagSD", "100Rnd_556x45_BetaCMag", "30Rnd_556x45_G36", "30Rnd_556x45_G36SD"};
-};
