8dcc979c21c470b463b535e89d986cc450607fdd
[VB]AWOL <vbawol@veteranbastards.com>
2014-01-26 20:11:45 -0600

    add worth

--- SQF/dayz_code/Configs/CfgMagazines.hpp
+++ SQF/dayz_code/Configs/CfgMagazines.hpp
@@ -682,6 +682,7 @@ class CfgMagazines {
 		picture = "\z\addons\dayz_epoch\pictures\equip_briefcase_10oz_CA.paa";
 		descriptionShort = "Briefcase (Contains: 1 x 10oz Gold Bar)";
 		weight = 1.2;
+		worth = 1000;
 		class ItemActions
 		{
 			class Crafting
@@ -719,6 +720,7 @@ class CfgMagazines {
 		picture = "\z\addons\dayz_epoch\pictures\equip_briefcase_20oz_CA.paa";
 		descriptionShort = "Briefcase (Contains: 2 x 10oz Gold Bar)";
 		weight = 1.5;
+		worth = 2000;
 		class ItemActions
 		{
 			class Crafting
@@ -764,6 +766,7 @@ class CfgMagazines {
 		picture = "\z\addons\dayz_epoch\pictures\equip_briefcase_30oz_CA.paa";
 		descriptionShort = "Briefcase (Contains: 3 x 10oz Gold Bar)";
 		weight = 1.8;
+		worth = 3000;
 		class ItemActions
 		{
 			class Crafting
@@ -809,6 +812,7 @@ class CfgMagazines {
 		picture = "\z\addons\dayz_epoch\pictures\equip_briefcase_40oz_CA.paa";
 		descriptionShort = "Briefcase (Contains: 4 x 10oz Gold Bar)";
 		weight = 2.1;
+		worth = 4000;
 		class ItemActions
 		{
 			class Crafting
@@ -854,6 +858,7 @@ class CfgMagazines {
 		picture = "\z\addons\dayz_epoch\pictures\equip_briefcase_50oz_CA.paa";
 		descriptionShort = "Briefcase (Contains: 5 x 10oz Gold Bar)";
 		weight = 2.4;
+		worth = 5000;
 		class ItemActions
 		{
 			class Crafting
@@ -899,6 +904,7 @@ class CfgMagazines {
 		picture = "\z\addons\dayz_epoch\pictures\equip_briefcase_60oz_CA.paa";
 		descriptionShort = "Briefcase (Contains: 6 x 10oz Gold Bar)";
 		weight = 2.7;
+		worth = 6000;
 		class ItemActions
 		{
 			class Crafting
@@ -944,6 +950,7 @@ class CfgMagazines {
 		picture = "\z\addons\dayz_epoch\pictures\equip_briefcase_70oz_CA.paa";
 		descriptionShort = "Briefcase (Contains: 7 x 10oz Gold Bar)";
 		weight = 2.9;
+		worth = 7000;
 		class ItemActions
 		{
 			class Crafting
@@ -989,6 +996,7 @@ class CfgMagazines {
 		picture = "\z\addons\dayz_epoch\pictures\equip_briefcase_80oz_CA.paa";
 		descriptionShort = "Briefcase (Contains: 8 x 10oz Gold Bar)";
 		weight = 3.2;
+		worth = 8000;
 		class ItemActions
 		{
 			class Crafting
@@ -1034,6 +1042,7 @@ class CfgMagazines {
 		picture = "\z\addons\dayz_epoch\pictures\equip_briefcase_90oz_CA.paa";
 		descriptionShort = "Briefcase (Contains: 9 x 10oz Gold Bar)";
 		weight = 3.5;
+		worth = 9000;
 		class ItemActions
 		{
 			class Crafting
@@ -1070,6 +1079,7 @@ class CfgMagazines {
 		picture = "\z\addons\dayz_epoch\pictures\equip_briefcase_100oz_CA.paa";
 		descriptionShort = "Briefcase (Contains: 10 x 10oz Gold Bar)";
 		weight = 3.8;
+		worth = 10000;
 		class ItemActions
 		{
 			class Crafting
@@ -5977,11 +5987,169 @@ class CfgMagazines {
 		ammo = "TinCan";
 	};
 	
-	class ItemWaterbottle;
+	class FoodDrink;
+	class ItemWaterbottle: FoodDrink
+	{
+		scope = 2;
+		refillable = 1;
+		sfx = "drink";
+		wateroz = 10; 
+		displayName = "$STR_EQUIP_NAME_12";
+		model = "\dayz_equip\models\waterbottle_gear.p3d";
+		picture = "\z\addons\dayz_epoch\pictures\equip_waterbottle10oz_ca.paa";
+		descriptionShort = "$STR_EQUIP_DESC_12";
+	};
+	class ItemWaterbottleUnfilled: CA_Magazine
+	{
+		scope = 2;
+		count = 1;
+		type = 256;
+		displayName = "$STR_EQUIP_NAME_13";
+		model = "\dayz_equip\models\waterbottle_gear.p3d";
+		picture = "\dayz_equip\textures\equip_waterbottle_empty_ca.paa";
+		descriptionShort = "$STR_EQUIP_DESC_13";
+		class ItemActions
+		{
+			class Fill
+			{
+				text = "$STR_ACTIONS_FILL_W";
+				script = "spawn player_fillWater;";
+			};
+		};
+	};
+	
 	class ItemWaterbottleBoiled : ItemWaterbottle {
 		displayName = $STR_ITEMWATERBOTTLEBOILED_CODE_NAME;
 		descriptionShort = $STR_ITEMWATERBOTTLEBOILED_CODE_DESC;
-	 };
+	};
+	
+	class ItemWaterbottle1oz : ItemWaterbottle {
+		displayName = "Water Bottle (1oz)";
+		descriptionShort = "Watter Bottle with 1oz of water";
+		picture = "\z\addons\dayz_epoch\pictures\equip_waterbottle1oz_ca.paa";
+		wateroz = 1;
+		class ItemActions
+		{
+			class Fill
+			{
+				text = "$STR_ACTIONS_FILL_W";
+				script = "spawn player_fillWater;";
+			};
+		};
+	};
+	class ItemWaterbottle2oz : ItemWaterbottle {
+		displayName = "Water Bottle (2oz)";
+		descriptionShort = "Watter Bottle with 2oz of water";
+		picture = "\z\addons\dayz_epoch\pictures\equip_waterbottle2oz_ca.paa";
+		wateroz = 2;
+		class ItemActions
+		{
+			class Fill
+			{
+				text = "$STR_ACTIONS_FILL_W";
+				script = "spawn player_fillWater;";
+			};
+		};
+	};
+	class ItemWaterbottle3oz : ItemWaterbottle {
+		displayName = "Water Bottle (3oz)";
+		descriptionShort = "Watter Bottle with 3oz of water";
+		picture = "\z\addons\dayz_epoch\pictures\equip_waterbottle3oz_ca.paa";
+		wateroz = 3;
+		class ItemActions
+		{
+			class Fill
+			{
+				text = "$STR_ACTIONS_FILL_W";
+				script = "spawn player_fillWater;";
+			};
+		};
+	};
+	class ItemWaterbottle4oz : ItemWaterbottle {
+		displayName = "Water Bottle (4oz)";
+		descriptionShort = "Watter Bottle with 4oz of water";
+		picture = "\z\addons\dayz_epoch\pictures\equip_waterbottle4oz_ca.paa";
+		wateroz = 4;
+		class ItemActions
+		{
+			class Fill
+			{
+				text = "$STR_ACTIONS_FILL_W";
+				script = "spawn player_fillWater;";
+			};
+		};
+	};
+	class ItemWaterbottle5oz : ItemWaterbottle {
+		displayName = "Water Bottle (5oz)";
+		descriptionShort = "Watter Bottle with 5oz of water";
+		picture = "\z\addons\dayz_epoch\pictures\equip_waterbottle5oz_ca.paa";
+		wateroz = 5;
+		class ItemActions
+		{
+			class Fill
+			{
+				text = "$STR_ACTIONS_FILL_W";
+				script = "spawn player_fillWater;";
+			};
+		};
+	};
+	class ItemWaterbottle6oz : ItemWaterbottle {
+		displayName = "Water Bottle (6oz)";
+		descriptionShort = "Watter Bottle with 6oz of water";
+		picture = "\z\addons\dayz_epoch\pictures\equip_waterbottle6oz_ca.paa";
+		wateroz = 6;
+		class ItemActions
+		{
+			class Fill
+			{
+				text = "$STR_ACTIONS_FILL_W";
+				script = "spawn player_fillWater;";
+			};
+		};
+	};
+	class ItemWaterbottle7oz : ItemWaterbottle {
+		displayName = "Water Bottle (7oz)";
+		descriptionShort = "Watter Bottle with 7oz of water";
+		picture = "\z\addons\dayz_epoch\pictures\equip_waterbottle7oz_ca.paa";
+		wateroz = 7;
+		class ItemActions
+		{
+			class Fill
+			{
+				text = "$STR_ACTIONS_FILL_W";
+				script = "spawn player_fillWater;";
+			};
+		};
+	};
+	class ItemWaterbottle8oz : ItemWaterbottle {
+		displayName = "Water Bottle (8oz)";
+		descriptionShort = "Watter Bottle with 8oz of water";
+		picture = "\z\addons\dayz_epoch\pictures\equip_waterbottle8oz_ca.paa";
+		wateroz = 8;
+		class ItemActions
+		{
+			class Fill
+			{
+				text = "$STR_ACTIONS_FILL_W";
+				script = "spawn player_fillWater;";
+			};
+		};
+	};
+	class ItemWaterbottle9oz : ItemWaterbottle {
+		displayName = "Water Bottle (9oz)";
+		descriptionShort = "Watter Bottle with 9oz of water";
+		picture = "\z\addons\dayz_epoch\pictures\equip_waterbottle9oz_ca.paa";
+		wateroz = 9;
+		class ItemActions
+		{
+			class Fill
+			{
+				text = "$STR_ACTIONS_FILL_W";
+				script = "spawn player_fillWater;";
+			};
+		};
+	};
+
 
 	#include "CfgMagazines\MeleeSwing.hpp"
 
