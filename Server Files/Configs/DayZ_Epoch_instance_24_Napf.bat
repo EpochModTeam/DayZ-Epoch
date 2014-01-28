bf88ad4337df2085cc2f0bf66f3a035455ee970e
[VB]AWOL <vbawol@veteranbastards.com>
2014-01-27 22:35:34 -0600

    lets do this

---
+++ Server Files/Configs/DayZ_Epoch_instance_24_Napf.bat
@@ -0,0 +1,2 @@
+@echo off
+start "arma2" /min "Expansion\beta\arma2oaserver.exe" -port=2302 "-config=instance_24_Napf\config.cfg" "-cfg=instance_24_Napf\basic.cfg" "-profiles=instance_24_Napf" -name=instance_24_Napf "-mod=@DayZ_Epoch;@DayZ_Epoch_Server;"
