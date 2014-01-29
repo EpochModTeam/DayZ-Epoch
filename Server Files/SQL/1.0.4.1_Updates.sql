df005abfbfbeb14c5c73ea8567b2a9129d34cd6a
[VB]AWOL <vbawol@veteranbastards.com>
2014-01-29 13:57:26 -0600

    price review update

--- Server Files/SQL/1.0.4.1_Updates.sql
+++ Server Files/SQL/1.0.4.1_Updates.sql
@@ -1,8 +1,10 @@
-UPDATE `traders_data` SET `buy` = '[4,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%pipebomb%';
-UPDATE `traders_data` SET `buy` = '[4,"ItemBriefcase100oz",1]', `sell` = '[1,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%HMMWV_M1151_M2_CZ_DES_EP1%';
-UPDATE `traders_data` SET `buy` = '[1,"ItemBriefcase100oz",1]', `sell` = '[5,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%HMMWV_M998A2_SOV_DES_EP1%';
-UPDATE `traders_data` SET `buy` = '[1,"ItemBriefcase100oz",1]', `sell` = '[5,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%LandRover_Special_CZ_EP1%';
-UPDATE `traders_data` SET `buy` = '[6,"ItemGoldBar10oz",1]', `sell` = '[3,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%LandRover_MG_TK_EP1%';
-UPDATE `traders_data` SET `buy` = '[6,"ItemGoldBar10oz",1]', `sell` = '[3,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%UAZ_MG_TK_EP1%';
-UPDATE `traders_data` SET `buy` = '[4,"ItemBriefcase100oz",1]', `sell` = '[1,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%CH_47F_EP1%';
-UPDATE `traders_data` SET `buy` = '[2,"ItemBriefcase100oz",1]', `sell` = '[1,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%Mi17%';
+UPDATE `Traders_DATA` SET `buy` = '[4,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%pipebomb%';
+UPDATE `Traders_DATA` SET `buy` = '[4,"ItemBriefcase100oz",1]', `sell` = '[1,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%HMMWV_M1151_M2_CZ_DES_EP1%';
+UPDATE `Traders_DATA` SET `buy` = '[1,"ItemBriefcase100oz",1]', `sell` = '[5,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%HMMWV_M998A2_SOV_DES_EP1%';
+UPDATE `Traders_DATA` SET `buy` = '[1,"ItemBriefcase100oz",1]', `sell` = '[5,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%LandRover_Special_CZ_EP1%';
+UPDATE `Traders_DATA` SET `buy` = '[6,"ItemGoldBar10oz",1]', `sell` = '[3,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%LandRover_MG_TK_EP1%';
+UPDATE `Traders_DATA` SET `buy` = '[6,"ItemGoldBar10oz",1]', `sell` = '[3,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%UAZ_MG_TK_EP1%';
+UPDATE `Traders_DATA` SET `buy` = '[4,"ItemBriefcase100oz",1]', `sell` = '[1,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%CH_47F_EP1%';
+UPDATE `Traders_DATA` SET `buy` = '[2,"ItemBriefcase100oz",1]', `sell` = '[1,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%Mi17%';
+UPDATE `Traders_DATA` SET `buy` = '[2,"ItemBriefcase100oz",1]', `sell` = '[1,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%GAZ_Vodnik_DZE%';
+UPDATE `Traders_DATA` SET `buy` = '[1,"ItemBriefcase100oz",1]', `sell` = '[5,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%GAZ_Vodnik_MedEvac%';
