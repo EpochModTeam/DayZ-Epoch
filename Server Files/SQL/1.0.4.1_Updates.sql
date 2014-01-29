3580d0765d6cef178e16d45fa42e501430e0e991
[VB]AWOL <vbawol@veteranbastards.com>
2014-01-29 11:02:33 -0600

    also increased prices on other armed vehicles

--- Server Files/SQL/1.0.4.1_Updates.sql
+++ Server Files/SQL/1.0.4.1_Updates.sql
@@ -1,2 +1,6 @@
 UPDATE `traders_data` SET `buy` = '[4,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%pipebomb%';
 UPDATE `traders_data` SET `buy` = '[4,"ItemBriefcase100oz",1]', `sell` = '[1,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%HMMWV_M1151_M2_CZ_DES_EP1%';
+UPDATE `traders_data` SET `buy` = '[1,"ItemBriefcase100oz",1]', `sell` = '[5,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%HMMWV_M998A2_SOV_DES_EP1%';
+UPDATE `traders_data` SET `buy` = '[1,"ItemBriefcase100oz",1]', `sell` = '[5,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%LandRover_Special_CZ_EP1%';
+UPDATE `traders_data` SET `buy` = '[6,"ItemGoldBar10oz",1]', `sell` = '[3,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%LandRover_MG_TK_EP1_DZE %';
+UPDATE `traders_data` SET `buy` = '[6,"ItemGoldBar10oz",1]', `sell` = '[3,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%UAZ_MG_TK_EP1_DZE %';
