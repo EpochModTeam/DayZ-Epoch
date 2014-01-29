51b8fdff05f532df58bc15ae2d4a57fa6bc69798
[VB]AWOL <vbawol@veteranbastards.com>
2014-01-29 11:06:05 -0600

    fix typo and increased price on CH_47F_EP1* and Mi17*

--- Server Files/SQL/1.0.4.1_Updates.sql
+++ Server Files/SQL/1.0.4.1_Updates.sql
@@ -2,5 +2,7 @@ UPDATE `traders_data` SET `buy` = '[4,"ItemBriefcase100oz",1]' WHERE `item` LIKE
 UPDATE `traders_data` SET `buy` = '[4,"ItemBriefcase100oz",1]', `sell` = '[1,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%HMMWV_M1151_M2_CZ_DES_EP1%';
 UPDATE `traders_data` SET `buy` = '[1,"ItemBriefcase100oz",1]', `sell` = '[5,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%HMMWV_M998A2_SOV_DES_EP1%';
 UPDATE `traders_data` SET `buy` = '[1,"ItemBriefcase100oz",1]', `sell` = '[5,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%LandRover_Special_CZ_EP1%';
-UPDATE `traders_data` SET `buy` = '[6,"ItemGoldBar10oz",1]', `sell` = '[3,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%LandRover_MG_TK_EP1_DZE %';
-UPDATE `traders_data` SET `buy` = '[6,"ItemGoldBar10oz",1]', `sell` = '[3,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%UAZ_MG_TK_EP1_DZE %';
+UPDATE `traders_data` SET `buy` = '[6,"ItemGoldBar10oz",1]', `sell` = '[3,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%LandRover_MG_TK_EP1%';
+UPDATE `traders_data` SET `buy` = '[6,"ItemGoldBar10oz",1]', `sell` = '[3,"ItemGoldBar10oz",1]' WHERE `item` LIKE  '%UAZ_MG_TK_EP1%';
+UPDATE `traders_data` SET `buy` = '[4,"ItemBriefcase100oz",1]', `sell` = '[1,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%CH_47F_EP1%';
+UPDATE `traders_data` SET `buy` = '[2,"ItemBriefcase100oz",1]', `sell` = '[1,"ItemBriefcase100oz",1]' WHERE `item` LIKE  '%Mi17%';
