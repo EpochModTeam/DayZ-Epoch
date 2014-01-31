7610f79f15bb8e0d2153e8ab1a0580f0f8dcf5c3
Florian Kinder <Florian.Kinder@fankserver.com>
2014-02-01 00:20:54 +0100

    Split Wrecks

---
+++ SQF/dayz_code/Configs/CfgVehicles/Wrecks.hpp
@@ -0,0 +1,15 @@
+class SpawnableWreck : House {};
+class UH1Wreck_DZ: SpawnableWreck {
+	model = "\ca\air2\UH1Y\UH1Y_Crashed.p3d";
+	icon = "\ca\air2\data\UI\icon_UH1Y_CA.paa";
+	mapSize = 15;
+	displayName = "Crashed UH-1Y";
+	vehicleClass = "Wrecks";
+};
+class UH60Wreck_DZ: SpawnableWreck {
+	model = "\Ca\Misc_E\Wreck_UH60_EP1.p3d";
+	icon = "ca\Misc_E\data\Icons\Icon_uh60_wreck_CA";
+	mapSize = 15;
+	displayName = "UH-60 Wreck";
+	vehicleClass = "Wrecks";
+};
