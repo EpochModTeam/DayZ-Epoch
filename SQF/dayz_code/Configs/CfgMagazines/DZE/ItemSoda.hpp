0b283b30b46fbf3cc247e210ab499ef89ff43b41
Florian Kinder <Florian.Kinder@fankserver.com>
2014-01-31 20:23:56 +0100

    Split ItemSoda

---
+++ SQF/dayz_code/Configs/CfgMagazines/DZE/ItemSoda.hpp
@@ -0,0 +1,12 @@
+class ItemSodaRbull : ItemSodaCoke {
+	displayName = $STR_FOOD_NAME_RBULL;
+	descriptionShort = $STR_FOOD_DESC_RBULL;
+	model = "z\addons\dayz_epoch\models\soda_redbull.p3d";
+	picture = "\z\addons\dayz_epoch\pictures\equip_soda_rbull_CA.paa";
+};
+class ItemSodaOrangeSherbet : ItemSodaCoke {
+	displayName = $STR_FOOD_NAME_OSHERBET;
+	descriptionShort = $STR_FOOD_DESC_OSHERBET;
+	model = "z\addons\dayz_epoch\models\soda_sherbet.p3d";
+	picture = "\z\addons\dayz_epoch\pictures\soda_orangesherbet_ca.paa";
+};
