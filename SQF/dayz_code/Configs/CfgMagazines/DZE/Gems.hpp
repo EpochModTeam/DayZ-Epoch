353045eb819a11cd893ef5836fd143d8f1e01b3d
Florian Kinder <Florian.Kinder@mcl.de>
2014-01-31 14:24:55 +0100

    Split Gems

---
+++ SQF/dayz_code/Configs/CfgMagazines/DZE/Gems.hpp
@@ -0,0 +1,63 @@
+class ItemTopaz : CA_Magazine {
+	scope = 2;
+	displayName = "Topaz";
+	model = "\z\addons\dayz_epoch\models\topaz.p3d";
+	picture = "\z\addons\dayz_epoch\pictures\equip_topaz_CA.paa";
+	count = 1;
+	type = "256";
+	descriptionShort = "Topaz";
+};
+class ItemObsidian : CA_Magazine {
+	scope = 2;
+	displayName = "Obsidian";
+	model = "\z\addons\dayz_epoch\models\obsidian.p3d";
+	picture = "\z\addons\dayz_epoch\pictures\equip_obsidian_CA.paa";
+	count = 1;
+	type = "256";
+	descriptionShort = "Obsidian";
+};
+class ItemSapphire : CA_Magazine {
+	scope = 2;
+	displayName = "Sapphire";
+	model = "\z\addons\dayz_epoch\models\sapphire.p3d";
+	picture = "\z\addons\dayz_epoch\pictures\equip_sapphire_CA.paa";
+	count = 1;
+	type = "256";
+	descriptionShort = "Sapphire";
+};
+class ItemAmethyst : CA_Magazine {
+	scope = 2;
+	displayName = "Amethyst";
+	model = "\z\addons\dayz_epoch\models\amethyst.p3d";
+	picture = "\z\addons\dayz_epoch\pictures\equip_amethyst_CA.paa";
+	count = 1;
+	type = "256";
+	descriptionShort = "Amethyst";
+};
+class ItemEmerald : CA_Magazine {
+	scope = 2;
+	displayName = "Emerald";
+	model = "\z\addons\dayz_epoch\models\emerald.p3d";
+	picture = "\z\addons\dayz_epoch\pictures\equip_emerald_CA.paa";
+	count = 1;
+	type = "256";
+	descriptionShort = "Emerald";
+};
+class ItemCitrine : CA_Magazine {
+	scope = 2;
+	displayName = "Citrine";
+	model = "\z\addons\dayz_epoch\models\citrine.p3d";
+	picture = "\z\addons\dayz_epoch\pictures\equip_citrine_CA.paa";
+	count = 1;
+	type = "256";
+	descriptionShort = "Citrine";
+};
+class ItemRuby : CA_Magazine {
+	scope = 2;
+	displayName = "Ruby";
+	model = "\z\addons\dayz_epoch\models\ruby.p3d";
+	picture = "\z\addons\dayz_epoch\pictures\equip_ruby_CA.paa";
+	count = 1;
+	type = "256";
+	descriptionShort = "Ruby";
+};
