e85f9aaad261bcb8a49d3576c7b2609002b94268
Florian Kinder <Florian.Kinder@fankserver.com>
2014-02-01 00:20:24 +0100

    Split AIR vehicles

---
+++ SQF/dayz_code/Configs/CfgVehicles/AIR/Mi17.hpp
@@ -0,0 +1,67 @@
+class Mi17_base: Helicopter  {
+	class Turrets: Turrets {
+		class MainTurret: MainTurret {
+			class ViewOptics: ViewOptics {};
+			class Turrets: Turrets {};
+		};
+		class BackTurret: MainTurret {
+			class Turrets: Turrets {};
+		};
+	};
+};
+
+class Mi17_DZ: Mi17_base	 {
+	displayname = "Mi-17";
+	displaynameshort = "Mi17_DZ";
+	scope = 2;
+	side = 2;
+	crew = "";
+	typicalCargo[] = {};
+	hiddenSelections[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	transportMaxWeapons = 10;
+	transportMaxMagazines = 50;
+	transportmaxbackpacks = 10;
+
+	class Turrets : Turrets  {
+		class MainTurret : MainTurret  {
+			magazines[] = {"100Rnd_762x54_PK"};
+		};
+		class BackTurret : BackTurret {
+			magazines[] = {"100Rnd_762x54_PK"};
+		};
+	};
+};
+class Mi17_DZE: Mi17_DZ	 {
+	displaynameshort = "Mi17_DZE";
+	class Turrets : Turrets  {
+		class MainTurret : MainTurret  {
+			magazines[] = {};
+		};
+		class BackTurret : BackTurret {
+			magazines[] = {};
+		};
+	};
+};
+
+class Mi17_Civilian;
+class Mi17_Civilian_DZ: Mi17_Civilian	 {
+	displayname = "Mi-17 (Civilian)";
+	displaynameshort = "Mi-17 (Civ)";
+	scope = 2;
+	side = 3;
+	crew = "";
+	typicalCargo[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	transportMaxWeapons = 10;
+	transportMaxMagazines = 50;
+	transportmaxbackpacks = 10;
+};
---
+++ SQF/dayz_code/Configs/CfgVehicles/AIR/UH1.hpp
@@ -0,0 +1,94 @@
+class UH1_Base: Helicopter {
+	class Turrets: Turrets {
+		class MainTurret: MainTurret {
+			class ViewOptics: ViewOptics {};
+			class Turrets: Turrets {};
+		};
+		class RightDoorGun: MainTurret {
+			class Turrets: Turrets {};
+		};
+		class CoPilotObs: MainTurret {
+			class Turrets: Turrets {};
+		};
+	};
+};
+class UH1Y_DZ: UH1_Base {
+	scope = 2;
+	side = 2;
+	crew = "";
+	typicalCargo[] = {};
+	hiddenSelections[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	transportMaxWeapons = 5;
+	transportMaxMagazines = 25;
+	transportmaxbackpacks = 4;
+	weapons[] = {};
+	magazines[] = {};
+	class Turrets : Turrets {
+		class MainTurret : MainTurret {
+			magazines[] = {"2000Rnd_762x51_M134"};
+		};
+		class RightDoorGun : RightDoorGun {
+			magazines[] = {"2000Rnd_762x51_M134"};
+		};
+	};
+};
+class UH1Y_DZE: UH1Y_DZ {
+	class Turrets : Turrets {
+		class MainTurret : MainTurret {
+			magazines[] = {};
+		};
+		class RightDoorGun : RightDoorGun {
+			magazines[] = {};
+		};
+	};
+};
+class UH1H_base: Helicopter {
+	class Turrets: Turrets {
+		class MainTurret: MainTurret {
+			class ViewOptics: ViewOptics {};
+			class Turrets: Turrets {};
+		};
+		class LeftDoorGun: MainTurret {
+			class Turrets: Turrets {};
+		};
+	};
+};
+
+class UH1H_DZ: UH1H_base {
+	scope = 2;
+	side = 2;
+	crew = "";
+	typicalCargo[] = {};
+	hiddenSelections[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	transportMaxWeapons = 5;
+	transportMaxMagazines = 25;
+	transportmaxbackpacks = 4;
+	class Turrets : Turrets {
+		class MainTurret : MainTurret {
+			magazines[] = {"100Rnd_762x51_M240"};
+		};
+		class LeftDoorGun : LeftDoorGun {
+			magazines[] = {"100Rnd_762x51_M240"};
+		};
+	};
+};
+class UH1H_DZE: UH1H_DZ {
+	class Turrets : Turrets {
+		class MainTurret : MainTurret {
+			magazines[] = {};
+		};
+		class LeftDoorGun : LeftDoorGun {
+			magazines[] = {};
+		};
+	};
+};
