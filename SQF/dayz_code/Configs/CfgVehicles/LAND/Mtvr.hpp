afc0054e5e09670669d04b344634266c48aa5351
Florian Kinder <Florian.Kinder@fankserver.com>
2014-02-01 00:20:12 +0100

    Split LAND vehicles

---
+++ SQF/dayz_code/Configs/CfgVehicles/LAND/Mtvr.hpp
@@ -0,0 +1,24 @@
+class MtvrRefuel_DES_EP1;
+class MtvrRefuel_DES_EP1_DZ: MtvrRefuel_DES_EP1 {
+	scope = 2;
+	side = 2;
+	crew = "";
+	typicalCargo[] = {};
+	hiddenSelections[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	transportFuel = 0;
+	fuelCapacity = 10000;
+};
+class MtvrRefuel;
+class MtvrRefuel_DZ: MtvrRefuel {
+	scope = 2;
+	side = 2;
+	crew = "";
+	typicalCargo[] = {};
+	hiddenSelections[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	transportFuel = 0;
+	fuelCapacity = 10000;
+};
