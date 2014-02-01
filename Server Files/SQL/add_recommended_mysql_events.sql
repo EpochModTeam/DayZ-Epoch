324caa8196e6218d2872355579e8edc1d3ccca6a
[VB]AWOL <vbawol@veteranbastards.com>
2014-02-01 12:20:03 -0600

    fixed case

--- Server Files/SQL/add_recommended_mysql_events.sql
+++ Server Files/SQL/add_recommended_mysql_events.sql
@@ -23,8 +23,8 @@ BEGIN
         ELSE 'ERROR'
     END);
 
-    SET keysInChar = (SELECT COUNT(*) FROM `character_data` WHERE `Alive` = '1' AND (`Inventory` LIKE CONCAT('%', keyName, '%') OR `Backpack` LIKE CONCAT('%', keyName, '%')));
-    SET keysInObj = (SELECT COUNT(*) FROM `object_data` WHERE `Inventory` LIKE CONCAT('%', keyName, '%'));
+    SET keysInChar = (SELECT COUNT(*) FROM `Character_DATA` WHERE `Alive` = '1' AND (`Inventory` LIKE CONCAT('%', keyName, '%') OR `Backpack` LIKE CONCAT('%', keyName, '%')));
+    SET keysInObj = (SELECT COUNT(*) FROM `Object_DATA` WHERE `Inventory` LIKE CONCAT('%', keyName, '%'));
 
     RETURN (keysInChar + keysInObj);
 END
@@ -36,7 +36,7 @@ DELIMITER ;
 -- ----------------------------
 DROP EVENT IF EXISTS `removeDamagedVehicles`;
 DELIMITER ;;
-CREATE EVENT `removeDamagedVehicles` ON SCHEDULE EVERY 1 DAY COMMENT 'Removes damaged vehicles' DO DELETE FROM `object_data` WHERE Damage = 1
+CREATE EVENT `removeDamagedVehicles` ON SCHEDULE EVERY 1 DAY COMMENT 'Removes damaged vehicles' DO DELETE FROM `Object_DATA` WHERE Damage = 1
 ;;
 DELIMITER ;
 
@@ -45,7 +45,7 @@ DELIMITER ;
 -- ----------------------------
 DROP EVENT IF EXISTS `removeObjectEmpty`;
 DELIMITER ;;
-CREATE EVENT `removeObjectEmpty` ON SCHEDULE EVERY 1 DAY COMMENT 'Removes abandoned storage objects and vehicles' DO DELETE FROM `object_data` WHERE `LastUpdated` < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 14 DAY) AND `Datestamp` < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 24 DAY) AND ( (`Inventory` IS NULL) OR (`Inventory` = '[]') OR (`Inventory` = '[[[],[]],[[],[]],[[],[]]]') )
+CREATE EVENT `removeObjectEmpty` ON SCHEDULE EVERY 1 DAY COMMENT 'Removes abandoned storage objects and vehicles' DO DELETE FROM `Object_DATA` WHERE `LastUpdated` < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 14 DAY) AND `Datestamp` < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 24 DAY) AND ( (`Inventory` IS NULL) OR (`Inventory` = '[]') OR (`Inventory` = '[[[],[]],[[],[]],[[],[]]]') )
 ;;
 DELIMITER ;
 
@@ -54,7 +54,7 @@ DELIMITER ;
 -- ----------------------------
 DROP EVENT IF EXISTS `removeObjectOld`;
 DELIMITER ;;
-CREATE EVENT `removeObjectOld` ON SCHEDULE EVERY 1 DAY COMMENT 'Removes old objects and vehicles' DO DELETE FROM `object_data` WHERE `LastUpdated` < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 24 DAY) AND `Datestamp` < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 42 DAY)
+CREATE EVENT `removeObjectOld` ON SCHEDULE EVERY 1 DAY COMMENT 'Removes old objects and vehicles' DO DELETE FROM `Object_DATA` WHERE `LastUpdated` < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 24 DAY) AND `Datestamp` < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 42 DAY)
 ;;
 DELIMITER ;
 
@@ -63,7 +63,7 @@ DELIMITER ;
 -- ----------------------------
 DROP EVENT IF EXISTS `setDamageOnAge`;
 DELIMITER ;;
-CREATE EVENT `setDamageOnAge` ON SCHEDULE EVERY 1 DAY COMMENT 'This sets damage on a wall so that it can be maintained' DO UPDATE `object_data` SET `Damage`=0.1 WHERE `ObjectUID` <> 0 AND `CharacterID` <> 0 AND `Datestamp` < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 3 DAY) AND ( (`Inventory` IS NULL) OR (`Inventory` = '[]') )
+CREATE EVENT `setDamageOnAge` ON SCHEDULE EVERY 1 DAY COMMENT 'This sets damage on a wall so that it can be maintained' DO UPDATE `Object_DATA` SET `Damage`=0.1 WHERE `ObjectUID` <> 0 AND `CharacterID` <> 0 AND `Datestamp` < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 3 DAY) AND ( (`Inventory` IS NULL) OR (`Inventory` = '[]') )
 ;;
 DELIMITER ;
 
@@ -72,7 +72,7 @@ DELIMITER ;
 -- ----------------------------
 DROP EVENT IF EXISTS `updateStockDaily`;
 DELIMITER ;;
-CREATE EVENT `updateStockDaily` ON SCHEDULE EVERY 1 DAY COMMENT 'Updates out of stock vendors' DO UPDATE `traders_data` SET qty=10 WHERE qty=0 AND afile<>'trade_any_vehicle' AND afile<>'trade_any_boat'
+CREATE EVENT `updateStockDaily` ON SCHEDULE EVERY 1 DAY COMMENT 'Updates out of stock vendors' DO UPDATE `Traders_DATA` SET qty=10 WHERE qty=0 AND afile<>'trade_any_vehicle' AND afile<>'trade_any_boat'
 ;;
 DELIMITER ;
 
@@ -82,19 +82,19 @@ DELIMITER ;
 DROP EVENT IF EXISTS `DeleteNonKeyVehicles`;
 DELIMITER ;;
 CREATE EVENT `DeleteNonKeyVehicles` ON SCHEDULE EVERY 1 DAY DO DELETE FROM
-			`object_data`
+			`Object_DATA`
 		WHERE
-			`object_data`.`CharacterID` <> 0
-			AND `object_data`.`CharacterID` <= 12500
-			AND `object_data`.`Classname` NOT LIKE 'Tent%'
-			AND `object_data`.`Classname` NOT LIKE '%Locked'
-			AND `object_data`.`Classname` NOT LIKE 'Land%'
-			AND `object_data`.`Classname` NOT LIKE 'Cinder%'
-			AND `object_data`.`Classname` NOT LIKE 'Wood%'
-			AND `object_data`.`Classname` NOT LIKE 'Metal%'
-			AND `object_data`.`Classname` NOT LIKE '%Storage%'
-			AND `object_data`.`Classname` NOT IN ('OutHouse_DZ', 'GunRack_DZ', 'WorkBench_DZ', 'Sandbag1_DZ', 'FireBarrel_DZ', 'DesertCamoNet_DZ', 'StickFence_DZ', 'LightPole_DZ', 'DeerStand_DZ', 'ForestLargeCamoNet_DZ', 'Plastic_Pole_EP1_DZ', 'Hedgehog_DZ', 'FuelPump_DZ', 'Fort_RazorWire', 'SandNest_DZ', 'ForestCamoNet_DZ', 'Fence_corrugated_DZ', 'CanvasHut_DZ', 'Generator_DZ')
-			AND FindVehicleKeys(object_data.CharacterID) = 0
+			`Object_DATA`.`CharacterID` <> 0
+			AND `Object_DATA`.`CharacterID` <= 12500
+			AND `Object_DATA`.`Classname` NOT LIKE 'Tent%'
+			AND `Object_DATA`.`Classname` NOT LIKE '%Locked'
+			AND `Object_DATA`.`Classname` NOT LIKE 'Land%'
+			AND `Object_DATA`.`Classname` NOT LIKE 'Cinder%'
+			AND `Object_DATA`.`Classname` NOT LIKE 'Wood%'
+			AND `Object_DATA`.`Classname` NOT LIKE 'Metal%'
+			AND `Object_DATA`.`Classname` NOT LIKE '%Storage%'
+			AND `Object_DATA`.`Classname` NOT IN ('OutHouse_DZ', 'GunRack_DZ', 'WorkBench_DZ', 'Sandbag1_DZ', 'FireBarrel_DZ', 'DesertCamoNet_DZ', 'StickFence_DZ', 'LightPole_DZ', 'DeerStand_DZ', 'ForestLargeCamoNet_DZ', 'Plastic_Pole_EP1_DZ', 'Hedgehog_DZ', 'FuelPump_DZ', 'Fort_RazorWire', 'SandNest_DZ', 'ForestCamoNet_DZ', 'Fence_corrugated_DZ', 'CanvasHut_DZ', 'Generator_DZ')
+			AND FindVehicleKeys(Object_DATA.CharacterID) = 0
 ;;
 DELIMITER ;
 
@@ -104,20 +104,20 @@ DELIMITER ;
 DROP EVENT IF EXISTS `UnlockNonKeyVehicles`;
 DELIMITER ;;
 CREATE EVENT `UnlockNonKeyVehicles` ON SCHEDULE EVERY 1 DAY DO UPDATE
-			`object_data`
+			`Object_DATA`
 		SET
-			`object_data`.`CharacterID` = 0
+			`Object_DATA`.`CharacterID` = 0
 		WHERE
-			`object_data`.`CharacterID` <> 0
-			AND `object_data`.`CharacterID` <= 12500
-			AND `object_data`.`Classname` NOT LIKE 'Tent%'
-			AND `object_data`.`Classname` NOT LIKE '%Locked'
-			AND `object_data`.`Classname` NOT LIKE 'Land%'
-			AND `object_data`.`Classname` NOT LIKE 'Cinder%'
-			AND `object_data`.`Classname` NOT LIKE 'Wood%'
-			AND `object_data`.`Classname` NOT LIKE 'Metal%'
-			AND `object_data`.`Classname` NOT LIKE '%Storage%'
-			AND `object_data`.`Classname` NOT IN ('OutHouse_DZ', 'GunRack_DZ', 'WorkBench_DZ', 'Sandbag1_DZ', 'FireBarrel_DZ', 'DesertCamoNet_DZ', 'StickFence_DZ', 'LightPole_DZ', 'DeerStand_DZ', 'ForestLargeCamoNet_DZ', 'Plastic_Pole_EP1_DZ', 'Hedgehog_DZ', 'FuelPump_DZ', 'Fort_RazorWire', 'SandNest_DZ', 'ForestCamoNet_DZ', 'Fence_corrugated_DZ', 'CanvasHut_DZ', 'Generator_DZ')
-			AND FindVehicleKeys(object_data.CharacterID) = 0
+			`Object_DATA`.`CharacterID` <> 0
+			AND `Object_DATA`.`CharacterID` <= 12500
+			AND `Object_DATA`.`Classname` NOT LIKE 'Tent%'
+			AND `Object_DATA`.`Classname` NOT LIKE '%Locked'
+			AND `Object_DATA`.`Classname` NOT LIKE 'Land%'
+			AND `Object_DATA`.`Classname` NOT LIKE 'Cinder%'
+			AND `Object_DATA`.`Classname` NOT LIKE 'Wood%'
+			AND `Object_DATA`.`Classname` NOT LIKE 'Metal%'
+			AND `Object_DATA`.`Classname` NOT LIKE '%Storage%'
+			AND `Object_DATA`.`Classname` NOT IN ('OutHouse_DZ', 'GunRack_DZ', 'WorkBench_DZ', 'Sandbag1_DZ', 'FireBarrel_DZ', 'DesertCamoNet_DZ', 'StickFence_DZ', 'LightPole_DZ', 'DeerStand_DZ', 'ForestLargeCamoNet_DZ', 'Plastic_Pole_EP1_DZ', 'Hedgehog_DZ', 'FuelPump_DZ', 'Fort_RazorWire', 'SandNest_DZ', 'ForestCamoNet_DZ', 'Fence_corrugated_DZ', 'CanvasHut_DZ', 'Generator_DZ')
+			AND FindVehicleKeys(Object_DATA.CharacterID) = 0
 ;;
 DELIMITER ;
