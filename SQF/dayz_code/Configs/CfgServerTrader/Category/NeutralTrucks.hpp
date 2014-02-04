a7c5ffce4067a2958cf4f4fe76abd85caad40977
Florian Kinder <Florian.Kinder@fankserver.com>
2014-02-04 22:40:41 +0100

    Added Missing Categories

--- SQF/dayz_code/Configs/CfgServerTrader/Category/NeutralTrucks.hpp
+++ SQF/dayz_code/Configs/CfgServerTrader/Category/NeutralTrucks.hpp
@@ -1,3 +1,35 @@
+class Category_659 {
+	class hilux1_civil_3_open_EP1 {
+		type = "trade_any_vehicle";
+		buy[] = {8,"ItemGoldBar"};
+		sell[] = {4,"ItemGoldBar"};
+	};
+	class datsun1_civil_3_open {
+		type = "trade_any_vehicle";
+		buy[] = {8,"ItemGoldBar"};
+		sell[] = {4,"ItemGoldBar"};
+	};
+	class hilux1_civil_1_open {
+		type = "trade_any_vehicle";
+		buy[] = {8,"ItemGoldBar"};
+		sell[] = {4,"ItemGoldBar"};
+	};
+	class datsun1_civil_2_covered {
+		type = "trade_any_vehicle";
+		buy[] = {8,"ItemGoldBar"};
+		sell[] = {4,"ItemGoldBar"};
+	};
+	class datsun1_civil_1_open {
+		type = "trade_any_vehicle";
+		buy[] = {8,"ItemGoldBar"};
+		sell[] = {4,"ItemGoldBar"};
+	};
+	class hilux1_civil_2_covered {
+		type = "trade_any_vehicle";
+		buy[] = {8,"ItemGoldBar"};
+		sell[] = {4,"ItemGoldBar"};
+	};
+};
 class Category_590 {
 	class hilux1_civil_3_open_EP1 {
 		type = "trade_any_vehicle";
--- Tools/TraderMigration/serverTraders2cfg.pl
+++ Tools/TraderMigration/serverTraders2cfg.pl
@@ -86,7 +86,11 @@ foreach my $traderhuman (keys $traderHumanity) {
 			$catName =~ s~ |-|/~~g;
 
 			$cfg .= "\t\t{\"".$cat->[0]."\",\"".$catName."\"}\n";
-			$traderCategories->{$catName} = $cat->[1];
+			if (!defined($traderCategories->{$catName})) {
+				$traderCategories->{$catName} = [$cat->[1]]
+			} else {
+				push(@{$traderCategories->{$catName}}, $cat->[1]);
+			}
 		}
 		$cfg .= "\t};\n";
 		$cfg .= "};\n"
@@ -100,39 +104,40 @@ foreach my $traderhuman (keys $traderHumanity) {
 foreach my $traderCategory (keys $traderCategories) {
 	my $cfg = '';
 
-	$cfg .= "class Category_".$traderCategories->{$traderCategory}." {\n";
-	#$cfg .= "\ttid = ".$traderCategories->{$traderCategory}.";\n";
-	my $sth = $MySQL->prepare(q~
-		SELECT
-			item	AS Item
-			,buy	AS Buy
-			,sell	AS Sell
-			,afile	AS aFile
-		FROM
-			traders_data
-		WHERE
-			tid = ?
-	~);
-	$sth->execute($traderCategories->{$traderCategory});
-	while (my $row = $sth->fetchrow_hashref()) {
-		if ($row->{Item} =~ /^\["([^"]+)",\d+\]$/) {
-			$row->{Item} = $1;
+	foreach my $categoryId (@{$traderCategories->{$traderCategory}}) {
+		$cfg .= "class Category_".$categoryId." {\n";
+		#$cfg .= "\ttid = ".$traderCategories->{$traderCategory}.";\n";
+		my $sth = $MySQL->prepare(q~
+			SELECT
+				item	AS Item
+				,buy	AS Buy
+				,sell	AS Sell
+				,afile	AS aFile
+			FROM
+				traders_data
+			WHERE
+				tid = ?
+		~);
+		$sth->execute($categoryId);
+		while (my $row = $sth->fetchrow_hashref()) {
+			if ($row->{Item} =~ /^\["([^"]+)",\d+\]$/) {
+				$row->{Item} = $1;
+			}
+			else {next;}
+			if ($row->{Buy} =~ s/^\[(\d+),"([^"]+)",(\d+)\]$/{$1,"$2"}/) {}
+			else {next;}
+			if ($row->{Sell} =~ s/^\[(\d+),"([^"]+)",(\d+)\]$/{$1,"$2"}/) {}
+			else {next;}
+
+			$cfg .= "\tclass ".$row->{Item}." {\n";
+			$cfg .= "\t\ttype = \"".$row->{aFile}."\";\n";
+			$cfg .= "\t\tbuy[] = ".$row->{Buy}.";\n";
+			$cfg .= "\t\tsell[] = ".$row->{Sell}.";\n";
+			$cfg .= "\t};\n";
 		}
-		else {next;}
-		if ($row->{Buy} =~ s/^\[(\d+),"([^"]+)",(\d+)\]$/{$1,"$2"}/) {}
-		else {next;}
-		if ($row->{Sell} =~ s/^\[(\d+),"([^"]+)",(\d+)\]$/{$1,"$2"}/) {}
-		else {next;}
-
-		$cfg .= "\tclass ".$row->{Item}." {\n";
-		$cfg .= "\t\ttype = \"".$row->{aFile}."\";\n";
-		$cfg .= "\t\tbuy[] = ".$row->{Buy}.";\n";
-		$cfg .= "\t\tsell[] = ".$row->{Sell}.";\n";
-		$cfg .= "\t};\n";
+		$sth->finish();
+		$cfg .= "};\n";
 	}
-	$sth->finish();
-	$cfg .= "};\n";
-
 	open(CFG, '>', $pathServerTraderCategoriesCfg.$traderCategory.'.hpp') or die $!;
 	print CFG $cfg;
 	close(CFG);
