78a1da77b8efa9db3dc6f09037705b3cad18ef77
Florian Kinder <Florian.Kinder@fankserver.com>
2014-02-11 19:55:40 +0100

    Added Converter for cfgLoot

---
+++ Tools/Loot Position/ConvertLoottables2.pl
@@ -0,0 +1,48 @@
+use strict;
+use warnings;
+use Data::Dumper;
+
+# Customize
+my $configFile = '../../SQF/dayz_code/Configs/CfgBuildingLoot/cfgLoot.hpp';
+
+# do not touch
+my $cfg = '';
+my $collect = 0;
+my $small = 0;
+my @itemType;
+my @itemChance;
+open(CFG, '<', $configFile) or die $!;
+while (my $line = <CFG>) {
+	if ($line =~ /^\s*(\w+)\[\]\s*=\s*\{/i) {
+		$collect = 1;
+		$cfg .= "\t".$1."[] = {\n";
+	}
+	elsif ($collect == 1 && $line =~ /^\s*"([^"]+)"/) {
+		push(@itemType, $1);
+	}
+	elsif ($collect == 1 && $line =~ /^\s*\},/) {
+		$collect = 2;
+	}
+	elsif ($collect == 2 && $line =~ /^\s*(\d+\.\d+),?/) {
+		push(@itemChance, $1);
+	}
+	elsif ($collect == 2 && $line =~ /^\t\}/) {
+		$collect = 0;
+
+		for (my $i=0; $i<scalar(@itemType); $i++) {
+			$cfg .= sprintf("\t\t%s{\"%s\",%.3f}\n", ($i > 0 ? ',' : ''), $itemType[$i], $itemChance[$i]);
+		}
+
+		$cfg .= "\t};\n";
+	}
+	elsif ($collect == 0 && $line !~ /^\s*\/\// && $line !~ /^\s*$/) {
+		$cfg .= $line;
+	}
+}
+close(CFG);
+
+open(NEWCFG,'>', $configFile) or die $!;
+print NEWCFG $cfg;
+close(NEWCFG);
+
+exit(0);
