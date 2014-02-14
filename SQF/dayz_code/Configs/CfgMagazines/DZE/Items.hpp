34444e3494bc348610ec7d517a223855b8044985
Florian Kinder <Florian.Kinder@fankserver.com>
2014-02-15 00:23:57 +0100

    Added ItemARM

--- SQF/dayz_code/Configs/CfgMagazines/DZE/Items.hpp
+++ SQF/dayz_code/Configs/CfgMagazines/DZE/Items.hpp
@@ -176,10 +176,9 @@ class ItemORP: CA_Magazine {
 	class ItemActions {
 		class Upgrades {
 			text = $STR_EPOCH_PLAYER_UPGRADEV;
-			script = "['ORP'] spawn player_upgradeVehicle;";
+			script = "spawn player_upgradeVehicle;";
 		};
 	};
-
 };
 class ItemAVE: CA_Magazine {
 	scope = 2;
@@ -194,10 +193,9 @@ class ItemAVE: CA_Magazine {
 	class ItemActions {
 		class Upgrades {
 			text = $STR_EPOCH_PLAYER_UPGRADEV;
-			script = "['AVE'] spawn player_upgradeVehicle;";
+			script = "spawn player_upgradeVehicle;";
 		};
 	};
-
 };
 class ItemLRK: CA_Magazine {
 	scope = 2;
@@ -212,10 +210,9 @@ class ItemLRK: CA_Magazine {
 	class ItemActions {
 		class Upgrades {
 			text = $STR_EPOCH_PLAYER_UPGRADEV;
-			script = "['LRK'] spawn player_upgradeVehicle;";
+			script = "spawn player_upgradeVehicle;";
 		};
 	};
-
 };
 class ItemTNK: CA_Magazine {
 	scope = 2;
@@ -230,10 +227,26 @@ class ItemTNK: CA_Magazine {
 	class ItemActions {
 		class Upgrades {
 			text = $STR_EPOCH_PLAYER_UPGRADEV;
-			script = "['TNK'] spawn player_upgradeVehicle;";
+			script = "spawn player_upgradeVehicle;";
 		};
 	};
-
+};
+class ItemARM: CA_Magazine {
+	scope = 2;
+	count = 1;
+	type = 256;
+	displayName = $STR_EPOCH_VEHUP_ARM;
+	model = "\z\addons\dayz_epoch\models\doc_Up4.p3d";
+	picture = "\z\addons\dayz_epoch\pictures\equip_docup4_ca.paa";
+	descriptionShort = $STR_EPOCH_VEHUP_ARM_DESC;
+	weight = 0.1;
+	sfx = "document";
+	class ItemActions {
+		class Upgrades {
+			text = $STR_EPOCH_PLAYER_UPGRADEV;
+			script = "spawn player_upgradeVehicle;";
+		};
+	};
 };
 
 // Documents
