63a56b313064dbe1daa05a280e226645cf71cb2f
Josh <raginruffalo@users.noreply.github.com>
2014-05-01 14:16:49 -0400

    Create CH53.hpp

---
+++ SQF/dayz_code/Configs/CfgVehicles/AIR/CH53.hpp
@@ -0,0 +1,43 @@
+class CH53_base: Helicopter  {
+	class Turrets: Turrets {
+		class MainTurret: MainTurret {
+			class ViewOptics: ViewOptics {};
+			class Turrets: Turrets {};
+		};
+	};
+};
+
+class CH53_DZ: CH53_base	 {
+	displayname = "USEC CH53";
+	displaynameshort = "CH53_DZ";
+	scope = 2;
+	side = 2;
+	crew = "";
+	typicalCargo[] = {};
+	hiddenSelections[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	transportMaxWeapons = 25;
+	transportMaxMagazines = 80;
+	transportmaxbackpacks = 15;
+
+	class Turrets : Turrets  {
+		class MainTurret : MainTurret  {
+			magazines[] = {"2000Rnd_762x51_M134"};
+		};
+	};
+};
+class CH53_DZE: CH53_DZ	 {
+	displaynameshort = "Mi17_DZE";
+	class Turrets : Turrets  {
+		class MainTurret : MainTurret  {
+			magazines[] = {};
+		};
+		class BackTurret : BackTurret {
+			magazines[] = {};
+		};
+	};
+};
