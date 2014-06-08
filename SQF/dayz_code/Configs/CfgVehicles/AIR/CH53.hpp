899f8c93e1d07f51435cc9560270f3e3243a7c88
[VB]AWOL <vbawol@veteranbastards.com>
2014-06-08 13:57:22 -0500

    fixed us53

--- SQF/dayz_code/Configs/CfgVehicles/AIR/CH53.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/AIR/CH53.hpp
@@ -1,16 +1,6 @@
-#include "\usec_ch53\config.cpp"
-
-class CH53_base: USEC_ch53_E {
-	class Turrets: Turrets {
-		class MainTurret: MainTurret {
-			class ViewOptics: ViewOptics {};
-			class Turrets: Turrets {};
-		};
-	};
-};
-
-class CH53_DZ: CH53_base {
-	displayname = "USEC CH53E";
+class USEC_ch53_E;
+class CH53_DZ : USEC_ch53_E {
+	displayname = "USEC CH53E DZ";
 	displaynameshort = "CH53_DZ";
 	enablemanualfire = 0;
 	scope = 2;
@@ -27,18 +17,4 @@ class CH53_DZ: CH53_base {
 	transportMaxWeapons = 25;
 	transportMaxMagazines = 80;
 	transportmaxbackpacks = 15;
-
-	class Turrets : Turrets  {
-		class MainTurret : MainTurret  {
-			magazines[] = {"2000Rnd_762x51_M134"};
-		};
-	};
-};
-class CH53_DZE: CH53_DZ	 {
-	displaynameshort = "CH53_DZE";
-	class Turrets : Turrets  {
-		class MainTurret : MainTurret  {
-			magazines[] = {};
-		};
-	};
 };
