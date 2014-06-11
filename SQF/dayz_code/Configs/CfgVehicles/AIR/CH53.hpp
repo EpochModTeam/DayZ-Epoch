a4577e86ce5249ef5ad9e64d896445dc34b50d20
CommuneCode <raginruffalo@users.noreply.github.com>
2014-06-10 23:12:09 -0400

    This just might have fixed it

--- SQF/dayz_code/Configs/CfgVehicles/AIR/CH53.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/AIR/CH53.hpp
@@ -1,20 +1,22 @@
 class USEC_ch53_E;
-class CH53_DZE : USEC_ch53_E {
-	displayname = "USEC CH53E DZ";
-	displaynameshort = "CH53_DZ";
-	enablemanualfire = 0;
-	scope = 2;
-	side = 2;
-	crew = "";
-	typicalCargo[] = {};
-	hiddenSelections[] = {};
-	radartype = 0;
-	class TransportMagazines{};
-	class TransportWeapons{};
-	commanderCanSee = 2+16+32;
-	gunnerCanSee = 2+16+32;
-	driverCanSee = 2+16+32;
-	transportMaxWeapons = 25;
-	transportMaxMagazines = 80;
-	transportmaxbackpacks = 15;
+class CfgVehicles {
+	class CH53_DZE : USEC_ch53_E {
+		displayname = "USEC CH53E DZ";
+		displaynameshort = "CH53_DZ";
+		enablemanualfire = 0;
+		scope = 2;
+		side = 2;
+		crew = "";
+		typicalCargo[] = {};
+		hiddenSelections[] = {};
+		radartype = 0;
+		class TransportMagazines{};
+		class TransportWeapons{};
+		commanderCanSee = 2+16+32;
+		gunnerCanSee = 2+16+32;
+		driverCanSee = 2+16+32;
+		transportMaxWeapons = 25;
+		transportMaxMagazines = 80;
+		transportmaxbackpacks = 15;
+	};
 };
