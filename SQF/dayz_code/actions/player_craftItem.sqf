EpochModTeam/DayZ-Epoch

commit f0e762285678e4c8ab7588b13ea4e616ce605d1f

Fixed Error in expression
Error in expression <_ret = _this select _ret;
};
_ret
>
Error position: <_ret
>
Error Undefined variable in expression: _ret
File z\addons\dayz_code\compile\BIS_fnc\fn_selectRandom.sqf, line 23
Error in expression <BIS_fnc_selectRandom;
_outputWeapons =
[_selectedWeapon];
_selectedMag = _select>
Error position: <_selectedWeapon];
_selectedMag = _select>
Error Undefined variable in expression: _selectedweapon
File z\addons\dayz_code\actions\player_craftItem.sqf, line 193
Error in expression <t_doLoop = false;
};
{
player addWeapon _x;
}
forEach _outputWeapons;
{

_itemOu>
Error position: <_x;
} forEach _outputWeapons;
{

_itemOu>
Error Undefined variable in expression: _x
File z\addons\dayz_code\actions\player_craftItem.sqf, line 200

SQF/dayz_code/actions/player_craftItem.sqf
@@ -189,10 +189,16 @@ if (_canDo) then {
 						} forEach _inputWeapons;
 						if (_num_removed_weapons == (count _inputWeapons)) then {
 							if(_randomOutput == 1) then {
-								_selectedWeapon = _outputWeapons call BIS_fnc_selectRandom;
-								_outputWeapons = [_selectedWeapon];
-								_selectedMag = _selectedRecipeOutput call BIS_fnc_selectRandom;
-								_selectedRecipeOutput = [_selectedMag];
+								_outputWeapons = [];
+								if (!isNil "_outputWeapons" && count _outputWeapons > 0) then {
+									_selectedWeapon = _outputWeapons call BIS_fnc_selectRandom;
+									_outputWeapons = [_selectedWeapon];
+								};
+								_selectedRecipeOutput = [];
+								if (!isNil "_selectedRecipeOutput" && count _selectedRecipeOutput > 0) then {
+									_selectedMag = _selectedRecipeOutput call BIS_fnc_selectRandom;
+									_selectedRecipeOutput = [_selectedMag];
+								};
 								// exit loop
 								_craft_doLoop = false;
 							};

commit 0176e5efbbaedb6fe1cbbb557e6a9333c4816287

fix #1318
Issue caused by f0e762285678e4c8ab7588b13ea4e616ce605d1f

SQF/dayz_code/actions/player_craftItem.sqf
@@ -37,7 +37,8 @@ _abort = false;
 _distance = 3;
 _reason = "";
 _waterLevel = 0;
-
+_outputWeapons = [];
+_selectedRecipeOutput = [];
 _onLadder =	(getNumber (configFile >> "CfgMovesMaleSdr" >> "States" >> (animationState player) >> "onLadder")) == 1;
 _canDo = (!r_drag_sqf && !r_player_unconscious && !_onLadder);
 
@@ -208,12 +209,10 @@ if (_canDo) then {
 						} forEach _inputWeapons;
 						if (_num_removed_weapons == (count _inputWeapons)) then {
 							if(_randomOutput == 1) then {
-								_outputWeapons = [];
 								if (!isNil "_outputWeapons" && count _outputWeapons > 0) then {
 									_selectedWeapon = _outputWeapons call BIS_fnc_selectRandom;
 									_outputWeapons = [_selectedWeapon];
 								};
-								_selectedRecipeOutput = [];
 								if (!isNil "_selectedRecipeOutput" && count _selectedRecipeOutput > 0) then {
 									_selectedMag = _selectedRecipeOutput call BIS_fnc_selectRandom;
 									_selectedRecipeOutput = [_selectedMag];

SQF/dayz_code/compile/BIS_fnc/fn_selectRandom.sqf
@@ -15,9 +15,10 @@ private "_ret";
 
 if(count _this > 0) then
 {
-	_ret = count _this;           //number of elements in the array
+	//_ret = count _this;           //number of elements in the array
 	//_ret = [0, _ret] call BIS_fnc_randomInt; //choose random index
-	_ret = floor(random _ret);
+	_ret = floor(random(count _this));
 	_ret = _this select _ret;           //get the element, return it
 };
-_ret
+
+_ret;