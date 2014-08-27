b3246ec80d57e3fc5618c8bd40400c609f7f8425
Uro1 <scotteh666@gmail.com>
2014-08-27 16:40:33 +0100

    Update CfgServerTraders to match #1508
    Updated the CfgServerTrader to match my changes in dynamic_vehicles &
    SQL changes.

--- SQF/dayz_epoch_b/CfgServerTrader/Category/NeutralTrucks.hpp
+++ SQF/dayz_epoch_b/CfgServerTrader/Category/NeutralTrucks.hpp
@@ -1,62 +1,62 @@
 class Category_659 {
-	class hilux1_civil_3_open_EP1 {
+	class hilux1_civil_3_open_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
 	};
-	class datsun1_civil_3_open {
+	class datsun1_civil_3_open_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
 	};
-	class hilux1_civil_1_open {
+	class hilux1_civil_1_open_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
 	};
-	class datsun1_civil_2_covered {
+	class datsun1_civil_2_covered_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
 	};
-	class datsun1_civil_1_open {
+	class datsun1_civil_1_open_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
 	};
-	class hilux1_civil_2_covered {
+	class hilux1_civil_2_covered_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
 	};
 };
 class Category_590 {
-	class hilux1_civil_3_open_EP1 {
+	class hilux1_civil_3_open_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
 	};
-	class datsun1_civil_3_open {
+	class datsun1_civil_3_open_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
 	};
-	class hilux1_civil_1_open {
+	class hilux1_civil_1_open_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
 	};
-	class datsun1_civil_2_covered {
+	class datsun1_civil_2_covered_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
 	};
-	class datsun1_civil_1_open {
+	class datsun1_civil_1_open_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
 	};
-	class hilux1_civil_2_covered {
+	class hilux1_civil_2_covered_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemGoldBar"};
 		sell[] = {4,"ItemGoldBar"};
