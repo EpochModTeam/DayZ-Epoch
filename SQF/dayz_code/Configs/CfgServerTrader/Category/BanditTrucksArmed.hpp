1e3ee190b18eb38a9d947a64d248e80eef9618db
A Man <r2892@t-online.de>
2016-06-17 17:24:42 +0200

    Update BanditTrucksArmed.hpp

--- SQF/dayz_code/Configs/CfgServerTrader/Category/BanditTrucksArmed.hpp
+++ SQF/dayz_code/Configs/CfgServerTrader/Category/BanditTrucksArmed.hpp
@@ -1,8 +1,8 @@
 class Category_534 {
-	class ArmoredSUV_PMC_DZE {
+	class Pickup_PK_TK_GUE_EP1_DZE {
 		type = "trade_any_vehicle";
-		buy[] = {2,"ItemBriefcase100oz"};
-		sell[] = {1,"ItemBriefcase100oz"};
+		buy[] = {8,"ItemGoldBar"};
+		sell[] = {4,"ItemGoldBar"};
 	};
 	class Pickup_PK_GUE_DZE {
 		type = "trade_any_vehicle";
@@ -14,14 +14,14 @@ class Category_534 {
 		buy[] = {1,"ItemGoldBar10oz"};
 		sell[] = {5,"ItemGoldBar"};
 	};
-	class Pickup_PK_TK_GUE_EP1_DZE {
-		type = "trade_any_vehicle";
-		buy[] = {8,"ItemGoldBar"};
-		sell[] = {4,"ItemGoldBar"};
-	};
 	class Offroad_DSHKM_Gue_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {2,"ItemGoldBar10oz"};
 		sell[] = {1,"ItemGoldBar10oz"};
+	};
+	class ArmoredSUV_PMC_DZE {
+		type = "trade_any_vehicle";
+		buy[] = {2,"ItemBriefcase100oz"};
+		sell[] = {1,"ItemBriefcase100oz"};
 	};
 };
