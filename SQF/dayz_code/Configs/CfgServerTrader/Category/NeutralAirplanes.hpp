2f7a8be32ceb02cdea3460b6b9f79155d33b0dd5
A Man <r2892@t-online.de>
2016-06-17 18:09:35 +0200

    Update NeutralAirplanes.hpp

--- SQF/dayz_code/Configs/CfgServerTrader/Category/NeutralAirplanes.hpp
+++ SQF/dayz_code/Configs/CfgServerTrader/Category/NeutralAirplanes.hpp
@@ -4,16 +4,6 @@ class Category_517 {
 		buy[] = {4,"ItemGoldBar10oz"};
 		sell[] = {2,"ItemGoldBar10oz"};
 	};
-	class C130J_US_EP1_DZ {
-		type = "trade_any_vehicle";
-		buy[] = {4,"ItemBriefcase100oz"};
-		sell[] = {2,"ItemBriefcase100oz"};
-	};
-	class MV22_DZ {
-		type = "trade_any_vehicle";
-		buy[] = {5,"ItemBriefcase100oz"};
-		sell[] = {2,"ItemBriefcase100oz"};
-	};
 	class GNT_C185U {
 		type = "trade_any_vehicle";
 		buy[] = {4,"ItemGoldBar10oz"};
@@ -33,5 +23,15 @@ class Category_517 {
 		type = "trade_any_vehicle";
 		buy[] = {4,"ItemGoldBar10oz"};
 		sell[] = {2,"ItemGoldBar10oz"};
+	};
+	class MV22_DZ {
+		type = "trade_any_vehicle";
+		buy[] = {5,"ItemBriefcase100oz"};
+		sell[] = {2,"ItemBriefcase100oz"};
+	};
+	class C130J_US_EP1_DZ {
+		type = "trade_any_vehicle";
+		buy[] = {4,"ItemBriefcase100oz"};
+		sell[] = {2,"ItemBriefcase100oz"};
 	};
 };
