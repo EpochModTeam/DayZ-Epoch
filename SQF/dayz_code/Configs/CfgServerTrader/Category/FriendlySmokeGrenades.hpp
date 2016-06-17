23570e5565ed6e38783ec28d6396c6419f4742b6
A Man <r2892@t-online.de>
2016-06-17 17:55:17 +0200

    Update FriendlySmokeGrenades.hpp

--- SQF/dayz_code/Configs/CfgServerTrader/Category/FriendlySmokeGrenades.hpp
+++ SQF/dayz_code/Configs/CfgServerTrader/Category/FriendlySmokeGrenades.hpp
@@ -14,4 +14,14 @@ class Category_543 {
 		buy[] = {2,"ItemSilverBar"};
 		sell[] = {1,"ItemSilverBar"};
 	};
+	class 1Rnd_Smoke_M203 {
+		type = "trade_items";
+		buy[] = {4,"ItemSilverBar"};
+		sell[] = {2,"ItemSilverBar"};
+	};
+	class 1Rnd_Smoke_GP25 {
+		type = "trade_items";
+		buy[] = {4,"ItemSilverBar"};
+		sell[] = {2,"ItemSilverBar"};
+	};
 };
