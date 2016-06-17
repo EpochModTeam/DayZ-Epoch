1c769755a3da4c0807308ef2b5ea927392db7905
A Man <r2892@t-online.de>
2016-06-17 17:49:33 +0200

    Update FriendlyPistols.hpp

--- SQF/dayz_code/Configs/CfgServerTrader/Category/FriendlyPistols.hpp
+++ SQF/dayz_code/Configs/CfgServerTrader/Category/FriendlyPistols.hpp
@@ -1,18 +1,13 @@
 class Category_489 {
-	class G17_DZ {
-		type = "trade_weapons";
-		buy[] = {5,"ItemSilverBar10oz"};
-		sell[] = {2,"ItemSilverBar10oz"};
-	};
-	class M1911_DZ {
-		type = "trade_weapons";
-		buy[] = {2,"ItemSilverBar10oz"};
-		sell[] = {1,"ItemSilverBar10oz"};
-	};
 	class M9_DZ {
 		type = "trade_weapons";
 		buy[] = {1,"ItemGoldBar"};
 		sell[] = {5,"ItemSilverBar10oz"};
+	};	
+	class G17_DZ {
+		type = "trade_weapons";
+		buy[] = {5,"ItemSilverBar10oz"};
+		sell[] = {2,"ItemSilverBar10oz"};
 	};
 	class Makarov_DZ {
 		type = "trade_weapons";
@@ -28,6 +23,11 @@ class Category_489 {
 		type = "trade_weapons";
 		buy[] = {3,"ItemGoldBar"};
 		sell[] = {2,"ItemGoldBar"};
+	};
+	class M1911_DZ {
+		type = "trade_weapons";
+		buy[] = {2,"ItemSilverBar10oz"};
+		sell[] = {1,"ItemSilverBar10oz"};
 	};
 };
 
