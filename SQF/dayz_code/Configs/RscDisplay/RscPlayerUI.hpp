67f6d864409e2b578ef20f07fdd3fe3be84fb067
ebaydayz <ebaydayz@openmailbox.org>
2016-07-21 14:58:03 -0400

    Use smaller sound muted UI icon and better alignment
    Vanilla development commit:
    
    https://github.com/DayZMod/DayZ/commit/6aba95d2517ad72b9b3f04b9316f0f0f0ac6e537

--- SQF/dayz_code/Configs/RscDisplay/RscPlayerUI.hpp
+++ SQF/dayz_code/Configs/RscDisplay/RscPlayerUI.hpp
@@ -221,7 +221,7 @@ class RscTitles
 				idc = 1904;
 				text = "\z\addons\dayz_code\gui\status\status_bg.paa";
 				x = 0.955313 * safezoneW + safezoneX;
-				y = 0.51 * safezoneH + safezoneY;
+				y = 0.44 * safezoneH + safezoneY;
 				w = 0.075;
 				h = 0.10;
 			};
@@ -230,7 +230,7 @@ class RscTitles
 				idc = 1204;
 				text = "\z\addons\dayz_code\gui\status\status_sound_muted.paa"; // previously status_connection_ca which is not used
 				x = 0.955313 * safezoneW + safezoneX;
-				y = 0.51 * safezoneH + safezoneY;
+				y = 0.44 * safezoneH + safezoneY;
 				w = 0.075;
 				h = 0.10;
 				colorText[] = {1,1,1,1};
