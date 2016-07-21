eeed1febadabd7d58f3a82a96ca8e4692436cdc5
icomrade <icomrade@live.com>
2016-07-21 19:48:16 -0400

    str_player_31_drink doesn't work for some reason...

--- SQF/dayz_code/Configs/CfgMagazines/Trash/Trash.hpp
+++ SQF/dayz_code/Configs/CfgMagazines/Trash/Trash.hpp
@@ -13,7 +13,7 @@ class ItemSodaEmpty : HandGrenade
 	{
 		class Drink
 		{
-			text = $str_player_31_drink;
+			text = $STR_ACTIONS_DRINK2;
 			script = "spawn player_drinkWater;";
 		};
 		class Crafting {
@@ -42,7 +42,7 @@ class TrashTinCan : HandGrenade
 	{
 		class Drink
 		{
-			text = $str_player_31_drink;
+			text = $STR_ACTIONS_DRINK2;
 			script = "spawn player_drinkWater;";
 		};
 		class Crafting {
