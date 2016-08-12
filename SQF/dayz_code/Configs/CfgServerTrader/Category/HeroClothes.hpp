d7410b78e5a5df886824a172e0c59d185b2707ec
A Man <r2892@t-online.de>
2016-08-12 19:29:44 +0200

    New skins for trading

--- SQF/dayz_code/Configs/CfgServerTrader/Category/HeroClothes.hpp
+++ SQF/dayz_code/Configs/CfgServerTrader/Category/HeroClothes.hpp
@@ -64,4 +64,75 @@ class Category_476 {
 		buy[] = {2,"ItemGoldBar"};
 		sell[] = {1,"ItemGoldBar"};
 	};
+	//new Epoch 1.06
+	class Skin_USMC_Soldier_Officer_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_USMC_Soldier_MG_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_USMC_Soldier_Pilot_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_FR_TL_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_FR_R_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_FR_Marksman_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_FR_Assault_R_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_CZ_Soldier_SL_DES_EP1_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_US_Soldier_EP1_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_UN_CDF_Soldier_Guard_EP1_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_UN_CDF_Soldier_EP1_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_GER_Soldier_TL_EP1_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_BAF_Soldier_Officer_MTP_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_BAF_Soldier_N_MTP_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
 };
