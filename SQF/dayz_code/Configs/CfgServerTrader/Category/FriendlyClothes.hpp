8a805d3c53175d35edbef4464a0dcc7f088a63d0
A Man <r2892@t-online.de>
2016-08-12 19:29:11 +0200

    New skins for trading

--- SQF/dayz_code/Configs/CfgServerTrader/Category/FriendlyClothes.hpp
+++ SQF/dayz_code/Configs/CfgServerTrader/Category/FriendlyClothes.hpp
@@ -79,6 +79,57 @@ class Category_628 {
 		buy[] = {2,"ItemGoldBar"};
 		sell[] = {1,"ItemGoldBar"};
 	};
+	class Skin_INS_Lopotev_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	//new Epoch 1.06
+	class Skin_Doctor_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_Assistant_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_Worker1_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_Worker3_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_Worker4_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_TK_CIV_Takistani01_EP1_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_TK_CIV_Takistani03_EP1_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_TK_CIV_Takistani04_EP1_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
+	class Skin_TK_CIV_Takistani06_EP1_DZ {
+		type = "trade_items";
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
+	};
 };
 
 class Category_689 {
