7d95dbb1701c9b3ca0f301edf07301edea447204
A Man <r2892@t-online.de>
2016-08-15 16:59:24 +0200

    Update Industrial.hpp
    Removing  Sandbags, Wire and Tank Traps from normal Industrial spawn and moved them to Military Industrial- Lowering spawn chance of Planks, Consumable and Generic. Ive tested the new loot chances and they working fine.

--- SQF/dayz_code/Configs/CfgLoot/Groups/Buildings/Industrial.hpp
+++ SQF/dayz_code/Configs/CfgLoot/Groups/Buildings/Industrial.hpp
@@ -3,7 +3,6 @@ Industrial[] =
 	//Tools
 	{Loot_WEAPON,		3,		ItemKnife},
 	{Loot_WEAPON,		6,		ItemToolbox},
-//	{Loot_WEAPON,		2,		ItemFlashlight},
 //	{Loot_WEAPON,		8,		ItemCrowbar},
 	{Loot_VEHICLE,		8,		WeaponHolder_ItemCrowbar},
 //	{Loot_WEAPON,		5,		ItemHatchet},
@@ -16,10 +15,10 @@ Industrial[] =
 	{Loot_WEAPON,		1,		ItemKeyKit},
 	
 	//Items
-	{Loot_MAGAZINE,		6,		ItemSandbag},
+//	{Loot_MAGAZINE,		6,		ItemSandbag},
 	{Loot_MAGAZINE,		4,		ItemJerryCan},
-	{Loot_MAGAZINE,		1,		ItemWire},
-	{Loot_MAGAZINE,		4,		ItemTankTrap},
+//	{Loot_MAGAZINE,		1,		ItemWire},
+//	{Loot_MAGAZINE,		4,		ItemTankTrap},
 	{Loot_MAGAZINE,		1,		equip_brick},
 	{Loot_MAGAZINE,		3,		equip_duct_tape},
 	{Loot_MAGAZINE,		2,		equip_hose},
@@ -29,7 +28,7 @@ Industrial[] =
 	{Loot_MAGAZINE,		2,		equip_metal_sheet},
 	{Loot_MAGAZINE,		2,		equip_1inch_metal_pipe},
 	{Loot_MAGAZINE,		2,		equip_2inch_metal_pipe},
-	{Loot_MAGAZINE,		12,		ItemPlank},
+	{Loot_MAGAZINE,		8,		ItemPlank},
 	{Loot_MAGAZINE,		2,		MortarBucket},
 	{Loot_MAGAZINE,		3,		CinderBlocks},
 	{Loot_MAGAZINE,		3,		ItemFuelBarrelEmpty},
@@ -38,10 +37,9 @@ Industrial[] =
 	
 	//Other
 	{Loot_GROUP,		15,		Parts},
-//	{Loot_GROUP,		7,		Military},
 	{Loot_PILE,			10,		Trash, 1, 3},
-	{Loot_PILE,			4,		Consumable, 1, 2},
-	{Loot_GROUP,		8,		Generic}
+	{Loot_PILE,			3,		Consumable, 1, 2},
+	{Loot_GROUP,		5,		Generic}
 };
 
 IndustrialSmall[] =
@@ -51,6 +49,6 @@ IndustrialSmall[] =
 	{Loot_MAGAZINE,		2,		equip_duct_tape},
 	{Loot_MAGAZINE,		6,		equip_nails},
 	{Loot_GROUP,		10,		Trash},
-	{Loot_GROUP,		4,		Consumable},
-	{Loot_GROUP,		8,		GenericSmall}
+	{Loot_GROUP,		3,		Consumable},
+	{Loot_GROUP,		5,		GenericSmall}
 };
