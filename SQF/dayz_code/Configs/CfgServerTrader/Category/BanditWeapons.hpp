22c071126fa6e6afb1c525f70445d0cfff666440
A Man <r2892@t-online.de>
2016-08-17 19:06:46 +0200

    Update BanditWeapons.hpp
    Adding Mk17 Sniper and M110 NVG.

--- SQF/dayz_code/Configs/CfgServerTrader/Category/BanditWeapons.hpp
+++ SQF/dayz_code/Configs/CfgServerTrader/Category/BanditWeapons.hpp
@@ -19,10 +19,20 @@ class Category_627 {
 		buy[] = {2,"ItemGoldBar10oz"};
 		sell[] = {1,"ItemGoldBar10oz"};
 	};
+	class SCAR_H_LNG_Sniper {
+		type = "trade_weapons";
+		buy[] = {6,"ItemGoldBar10oz"};
+		sell[] = {3,"ItemGoldBar10oz"};
+	};
 	class SCAR_H_LNG_Sniper_SD {
 		type = "trade_weapons";
 		buy[] = {2,"ItemGoldBar10oz"};
 		sell[] = {1,"ItemGoldBar10oz"};
+	};
+	class M110_NVG_EP1 {
+		type = "trade_weapons";
+		buy[] = {7,"ItemGoldBar10oz"};
+		sell[] = {4,"ItemGoldBar10oz"};
 	};
 	class DMR_DZ {
 		type = "trade_weapons";
