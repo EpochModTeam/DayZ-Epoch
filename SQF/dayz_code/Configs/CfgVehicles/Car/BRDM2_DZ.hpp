357ede4d6c801c67102e60de3f6836516c15ec19
A Man <r2892@t-online.de>
2016-08-19 19:08:28 +0200

    Adding BRDM2 to configs

---
+++ SQF/dayz_code/Configs/CfgVehicles/Car/BRDM2_DZ.hpp
@@ -0,0 +1,12 @@
+class BRDM2_HQ_TK_GUE_EP1;
+class BRDM2_HQ_TK_GUE_EP1_DZ: BRDM2_HQ_TK_GUE_EP1 {
+	scope = public;
+	displayname = "BRDM2 (PKT)";
+	displaynameshort = "BRDM2 (PKT)";
+	armor = 85;
+	damageResistance = 0.032;
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	fuelCapacity = 220;
+};
