9a4e2b3e3a890829ea26d621bc31a9a7fa70bffc
A Man <r2892@t-online.de>
2016-08-19 17:56:02 +0200

    Adding Cessna to the configs
    That removes the radar function from the cessans.

---
+++ SQF/dayz_code/Configs/CfgVehicles/Plane/Cessna_DZ.hpp
@@ -0,0 +1,84 @@
+class GNT_C185C;
+class GNT_C185C_DZ: GNT_C185C
+{
+	displayname = "GNT_C185C_DZ";
+	displaynameshort = "GNT_C185C_DZ";
+	scope = public;
+	side = 2;
+	crew = "";
+	typicalCargo[] = {};
+	hiddenSelections[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	weapons[] = {};
+	magazines[] = {};
+	gunnerHasFlares = false;
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	fuelCapacity = 1000;
+};
+
+class GNT_C185R;
+class GNT_C185R_DZ: GNT_C185R
+{
+	displayname = "GNT_C185R_DZ";
+	displaynameshort = "GNT_C185R_DZ";
+	scope = public;
+	side = 2;
+	crew = "";
+	typicalCargo[] = {};
+	hiddenSelections[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	weapons[] = {};
+	magazines[] = {};
+	gunnerHasFlares = false;
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	fuelCapacity = 1000;
+};
+
+class GNT_C185;
+class GNT_C185_DZ: GNT_C185
+{
+	displayname = "GNT_C185_DZ";
+	displaynameshort = "GNT_C185_DZ";
+	scope = public;
+	side = 2;
+	crew = "";
+	typicalCargo[] = {};
+	hiddenSelections[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	weapons[] = {};
+	magazines[] = {};
+	gunnerHasFlares = false;
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	fuelCapacity = 1000;
+};
+
+class GNT_C185U;
+class GNT_C185U_DZ: GNT_C185U
+{
+	displayname = "GNT_C185U_DZ";
+	displaynameshort = "GNT_C185U_DZ";
+	scope = public;
+	side = 2;
+	crew = "";
+	typicalCargo[] = {};
+	hiddenSelections[] = {};
+	class TransportMagazines{};
+	class TransportWeapons{};
+	weapons[] = {};
+	magazines[] = {};
+	gunnerHasFlares = false;
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	fuelCapacity = 1000;
+};
+
