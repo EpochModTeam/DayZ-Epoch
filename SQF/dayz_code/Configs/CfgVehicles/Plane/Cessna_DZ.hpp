a869bc6c9c63ff538f6abc16889508f8c24e2574
ebaydayz <ebaydayz@users.noreply.github.com>
2016-08-20 12:28:51 -0400

    Update Cessna_DZ.hpp

--- SQF/dayz_code/Configs/CfgVehicles/Plane/Cessna_DZ.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Plane/Cessna_DZ.hpp
@@ -3,20 +3,16 @@ class GNT_C185C_DZ: GNT_C185C
 {
 	displayname = "GNT_C185C_DZ";
 	displaynameshort = "GNT_C185C_DZ";
-	scope = public;
 	side = 2;
 	crew = "";
 	typicalCargo[] = {};
-	hiddenSelections[] = {};
 	class TransportMagazines{};
 	class TransportWeapons{};
-	weapons[] = {};
-	magazines[] = {};
 	gunnerHasFlares = false;
 	commanderCanSee = 2+16+32;
 	gunnerCanSee = 2+16+32;
 	driverCanSee = 2+16+32;
-	fuelCapacity = 1000;
+	fuelCapacity = 500;
 };
 
 class GNT_C185R;
@@ -24,20 +20,16 @@ class GNT_C185R_DZ: GNT_C185R
 {
 	displayname = "GNT_C185R_DZ";
 	displaynameshort = "GNT_C185R_DZ";
-	scope = public;
 	side = 2;
 	crew = "";
 	typicalCargo[] = {};
-	hiddenSelections[] = {};
 	class TransportMagazines{};
 	class TransportWeapons{};
-	weapons[] = {};
-	magazines[] = {};
 	gunnerHasFlares = false;
 	commanderCanSee = 2+16+32;
 	gunnerCanSee = 2+16+32;
 	driverCanSee = 2+16+32;
-	fuelCapacity = 1000;
+	fuelCapacity = 500;
 };
 
 class GNT_C185;
@@ -45,20 +37,16 @@ class GNT_C185_DZ: GNT_C185
 {
 	displayname = "GNT_C185_DZ";
 	displaynameshort = "GNT_C185_DZ";
-	scope = public;
 	side = 2;
 	crew = "";
 	typicalCargo[] = {};
-	hiddenSelections[] = {};
 	class TransportMagazines{};
 	class TransportWeapons{};
-	weapons[] = {};
-	magazines[] = {};
 	gunnerHasFlares = false;
 	commanderCanSee = 2+16+32;
 	gunnerCanSee = 2+16+32;
 	driverCanSee = 2+16+32;
-	fuelCapacity = 1000;
+	fuelCapacity = 500;
 };
 
 class GNT_C185U;
@@ -66,19 +54,15 @@ class GNT_C185U_DZ: GNT_C185U
 {
 	displayname = "GNT_C185U_DZ";
 	displaynameshort = "GNT_C185U_DZ";
-	scope = public;
 	side = 2;
 	crew = "";
 	typicalCargo[] = {};
-	hiddenSelections[] = {};
 	class TransportMagazines{};
 	class TransportWeapons{};
-	weapons[] = {};
-	magazines[] = {};
 	gunnerHasFlares = false;
 	commanderCanSee = 2+16+32;
 	gunnerCanSee = 2+16+32;
 	driverCanSee = 2+16+32;
-	fuelCapacity = 1000;
+	fuelCapacity = 500;
 };
 
