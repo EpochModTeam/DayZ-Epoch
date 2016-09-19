7ea6e9c7a444f0f2b71fe49df145511a0e67a6f8
A Man <r2892@t-online.de>
2016-09-20 01:27:31 +0200

    Zombie loot update
    Adding 3 more options for zombie loot to give admins better possibilities to customize the zombie loot.

---
+++ SQF/dayz_code/Configs/CfgLoot/Groups/Zombies/Pilot.hpp
@@ -0,0 +1,15 @@
+ZombiePilot[] =
+{
+	{Loot_MAGAZINE,		1,		FoodMRE},
+	{Loot_MAGAZINE,		2,		ItemHotwireKit},
+	{Loot_GROUP,		3,		MedicalLow},
+	{Loot_GROUP,		10,		AmmoMilitaryLow},
+	{Loot_GROUP,		2,		AmmoMilitaryHigh},
+	{Loot_GROUP,		2,		Consumable}
+};
+
+ZombiePilotViral[] =
+{
+	{Loot_GROUP,		10,		ZombieMilitary},
+	{Loot_MAGAZINE,		1,		ItemAntibiotic1}
+};
