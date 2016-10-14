01a5bbe6878fa89417420ffb9c51941e7cfc8f43
ebaydayz <ebaydayz@openmailbox.org>
2016-10-14 16:18:52 -0400

    Update Melee bat damage
    Vanilla commit:
    
    https://github.com/DayZMod/DayZ/commit/b3781670b48ae19fe0612a07e12b2861b42a0eff

--- SQF/dayz_code/Configs/cfgAmmo.hpp
+++ SQF/dayz_code/Configs/cfgAmmo.hpp
@@ -217,17 +217,17 @@ class CfgAmmo
 	
 	class Bat_Swing_Ammo : Melee
 	{
-		hit = 1;
+		hit = 2;
 	};
 	
 	class BatBarbed_Swing_Ammo : Melee
 	{
-		hit = 2;
+		hit = 3;
 	};
 	
 	class BatNailed_Swing_Ammo : Melee
 	{
-		hit = 2;
+		hit = 3;
 	};
 	
 	
