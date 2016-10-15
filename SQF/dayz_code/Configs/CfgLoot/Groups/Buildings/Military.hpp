98674fadb3f9d6265e6ffdb1c2a714b6a76a88a5
A Man <r2892@t-online.de>
2016-10-15 17:49:50 +0200

    Lowering ammo box spawn chances again.

--- SQF/dayz_code/Configs/CfgLoot/Groups/Buildings/Military.hpp
+++ SQF/dayz_code/Configs/CfgLoot/Groups/Buildings/Military.hpp
@@ -28,13 +28,13 @@ Military[] =
 	{Loot_GROUP,		1,		specialclothes},
 	
 	//Containers
-	{Loot_CONTAINER,	1.3,		DZ_AmmoBoxRU, AmmoBoxRU1, 10, 20},		//545x39	AK74, RPK74
-	{Loot_CONTAINER,	0.7,		DZ_AmmoBoxRU, AmmoBoxRU2, 10, 20},		//762x39	AK47
-	{Loot_CONTAINER,	0.5,		DZ_AmmoBoxRU, AmmoBoxRU3, 5, 10},		//762x54r	SVD, PKM
-	{Loot_CONTAINER,	1.5,		DZ_AmmoBoxUS, AmmoBoxEU1, 10, 20},		//556x45	G36, M249
-	{Loot_CONTAINER,	1,			DZ_AmmoBoxUS, AmmoBoxEU2, 5, 10},		//762x51	FAL, M240
-	{Loot_CONTAINER,	1.5,		DZ_AmmoBoxUS, AmmoBoxUS1, 10, 20},		//556x45	STANAG, M249
-	{Loot_CONTAINER,	1,			DZ_AmmoBoxUS, AmmoBoxUS2, 5, 10},		//762x51	DMR, M240
+	{Loot_CONTAINER,	1.1,		DZ_AmmoBoxRU, AmmoBoxRU1, 10, 20},		//545x39	AK74, RPK74
+	{Loot_CONTAINER,	0.5,		DZ_AmmoBoxRU, AmmoBoxRU2, 10, 20},		//762x39	AK47
+	{Loot_CONTAINER,	0.2,		DZ_AmmoBoxRU, AmmoBoxRU3, 5, 10},		//762x54r	SVD, PKM
+	{Loot_CONTAINER,	1.2,		DZ_AmmoBoxUS, AmmoBoxEU1, 10, 20},		//556x45	G36, M249
+	{Loot_CONTAINER,	0.8,			DZ_AmmoBoxUS, AmmoBoxEU2, 5, 10},		//762x51	FAL, M240
+	{Loot_CONTAINER,	1.2,		DZ_AmmoBoxUS, AmmoBoxUS1, 10, 20},		//556x45	STANAG, M249
+	{Loot_CONTAINER,	0.8,			DZ_AmmoBoxUS, AmmoBoxUS2, 5, 10},		//762x51	DMR, M240
 //	{Loot_CONTAINER,	2,			DZ_MedBox, MedicalBox, 10, 20},
 	
 	//Other
@@ -97,13 +97,13 @@ MilitarySpecial[] =
 	{Loot_GROUP,		2,		specialclothes},
 	
 	//Containers
-	{Loot_CONTAINER,	2.5,		DZ_AmmoBoxRU, AmmoBoxRU1, 10, 20},	//5.45x39
-	{Loot_CONTAINER,	2,			DZ_AmmoBoxUS, AmmoBoxUS1, 7, 14},	//5.56x45
-	{Loot_CONTAINER,	2,			DZ_AmmoBoxRU, AmmoBoxCZ1, 7, 14},	//7.62x39
-	{Loot_CONTAINER,	1,			DZ_AmmoBoxRU, AmmoBoxCZ2, 2, 5},	//7.62x54R
+	{Loot_CONTAINER,	2.2,		DZ_AmmoBoxRU, AmmoBoxRU1, 10, 20},	//5.45x39
+	{Loot_CONTAINER,	1.8,			DZ_AmmoBoxUS, AmmoBoxUS1, 7, 14},	//5.56x45
+	{Loot_CONTAINER,	1.8,			DZ_AmmoBoxRU, AmmoBoxCZ1, 7, 14},	//7.62x39
+	{Loot_CONTAINER,	0.8,			DZ_AmmoBoxRU, AmmoBoxCZ2, 2, 5},	//7.62x54R
 //	{Loot_CONTAINER,	0.5,		DZ_AmmoBoxRU, AmmoBoxRU3, 3, 7},	//7.62x54R
-	{Loot_CONTAINER,	0.5,		DZ_ExplosivesBoxRU, AmmoBoxRU4, 5, 15},	//GP-25
-	{Loot_CONTAINER,	0.5,		DZ_ExplosivesBoxRU, AmmoBoxRU5, 3, 7},	//Grenades
+	{Loot_CONTAINER,	0.2,		DZ_ExplosivesBoxRU, AmmoBoxRU4, 5, 15},	//GP-25
+	{Loot_CONTAINER,	0.2,		DZ_ExplosivesBoxRU, AmmoBoxRU5, 3, 7},	//Grenades
 
 	//Other
 	{Loot_MAGAZINE,		3,		FoodMRE},
