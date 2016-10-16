EpochModTeam/DayZ-Epoch

commit 6cf740e7d4735cd22120b9d3150867a3e73a3a45

Update _object_damage private array
Vanilla commits:

https://github.com/DayZMod/DayZ/commit/21ea5984c94f83d940ebb23c985202ca40cd3929

https://github.com/DayZMod/DayZ/commit/a13e7240dcc6c3b9b736d3649f68eaf611459f03

SQF/dayz_server/compile/server_updateObject.sqf
@@ -109,11 +109,12 @@ _object_inventory = {
 
 _object_damage = {
 	//Allow dmg process
-	private ["_hitpoints","_array","_hit","_selection","_key","_damage","_allFixed"];
+	private ["_hitpoints","_array","_hit","_selection","_key","_damage","_allFixed","_forced","_recorddmg"];
 	_hitpoints = _object call vehicle_getHitpoints;
 	_damage = damage _object;
 	_array = [];
 	_allFixed = true;
+	
 	{
 		_hit = [_object,_x] call object_getHit;
 		_selection = getText (configFile >> "CfgVehicles" >> (typeOf _object) >> "HitPoints" >> _x >> "name");
@@ -127,6 +128,7 @@ _object_damage = {
 	} forEach _hitpoints;
 	
 	if (_allFixed) then {_object setDamage 0;};
+	
 	if (_forced) then {        
 		if (_object in needUpdate_objects) then {needUpdate_objects = needUpdate_objects - [_object];};
 		_recorddmg = true;	       
@@ -147,6 +149,7 @@ _object_damage = {
 		} else {
 			_key = format["CHILD:306:%1:",_objectID] + str _array + ":" + str _damage + ":";
 		};
+		
 		diag_log ("HIVE: WRITE: "+ str(_key));
 		_key call server_hiveWrite;   
 	};

SQF/dayz_code/compile/keyboard.sqf
@@ -252,6 +252,7 @@ if (isNil "keyboard_keys") then {
             _handled = true;
             r_interrupt = true;
         };
+		
 		if (animationState player in ["bunnyhopunarmed","bunnyhoprifle"]) then {
 			//Fixes invisible weapon switch glitch if double tapping vault with no weapon in hands
 			_handled = true;
@@ -275,6 +276,7 @@ if (isNil "keyboard_keys") then {
     _block = {
         _handled = true;
     };
+	
     _addArray = {
         {
             keyboard_keys set [_x, _this select 1];

commit fd0a4685fe88f9e1716dc99194483029e841595b

Update private array
6cf740e7d4735cd22120b9d3150867a3e73a3a45 caused undefined variable
errors since the variables were private in 2 places. _forced and
_recorddmg should be declared private in the top of the script where
they are defined, not in the compile.

SQF/dayz_server/compile/server_updateObject.sqf
@@ -109,7 +109,7 @@ _object_inventory = {
 
 _object_damage = {
 	//Allow dmg process
-	private ["_hitpoints","_array","_hit","_selection","_key","_damage","_allFixed","_forced","_recorddmg"];
+	private ["_hitpoints","_array","_hit","_selection","_key","_damage","_allFixed"];
 	_hitpoints = _object call vehicle_getHitpoints;
 	_damage = damage _object;
 	_array = [];