644d36d53c58e3019d30e84c5fe822b8b6024f9e
A Man <r2892@t-online.de>
2016-11-01 13:58:30 +0100

    Removing old code
    Old code "remove tank trap" is not longer used since we have the scroll action for that.

--- SQF/dayz_code/Configs/CfgWeapons/Tools/Toolbox.hpp
+++ SQF/dayz_code/Configs/CfgWeapons/Tools/Toolbox.hpp
@@ -12,11 +12,6 @@ class ItemToolbox : ItemCore
 			text= $STR_EQUIP_TOOLBOX_ACTION_1;
 			script=";[['DesertCamoNet_DZ','ForestCamoNet_DZ','DesertLargeCamoNet_DZ','ForestLargeCamoNet_DZ'],5,'str_epoch_player_8'] call player_removeNearby;";
 		};
-		class RemoveTankTrap
-		{
-			text= $STR_EQUIP_TOOLBOX_ACTION_2;
-			script=";[['Hedgehog_DZ'],1,'STR_EPOCH_ACTIONS_14'] call player_removeNearby;";
-		};
 	};
 
 };
