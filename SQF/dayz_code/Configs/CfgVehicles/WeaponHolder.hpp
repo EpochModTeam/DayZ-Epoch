c3bf1d29d609cdab2588404d49097ab2b7c6b7bb
ebaydayz <ebaydayz@openmailbox.org>
2016-12-09 13:28:31 -0500

    Fix arrow and other WeaponHolderBase inherited destrType #1815
    Prevents catching on fire when destroyed.

--- SQF/dayz_code/Configs/CfgVehicles/WeaponHolder.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/WeaponHolder.hpp
@@ -3,6 +3,7 @@ class WoodenArrowF : WeaponHolderBase {
 	scope = public;
 	displayName = $STR_ITEMWOODENARROW_CODE_NAME;
 	model = "\dayz_weapons\models\bolt";
+	destrType = "DestructNo";
 
 	class eventHandlers
 	{
@@ -14,6 +15,8 @@ class WeaponHolder_ItemCrowbar: WeaponHolderBase
 	scope=2;
 	displayName=$STR_EQUIP_NAME_CROWBAR;
 	model="\dayz_equip\models\crowbar.p3d";
+	destrType = "DestructNo";
+	
 	class eventHandlers
 	{
 		init="[(_this select 0),'cfgWeapons','ItemCrowbar'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
@@ -23,6 +26,7 @@ class WeaponHolder_ItemSledge : WeaponHolderBase { // Epoch class. Needed for pl
 	scope = public;
 	displayName = $STR_EQUIP_NAME_SledgeHammer;
 	model="\z\addons\dayz_epoch\models\sledge_mag.p3d";
+	destrType = "DestructNo";
 	
 	class eventHandlers {
 		init = "[(_this select 0),'cfgWeapons','ItemSledge'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
@@ -34,6 +38,8 @@ class WeaponHolder_MeleeBaseBallBat: WeaponHolderBase
 	scope=2;
 	displayName=$STR_EQUIP_NAME_BASEBALLBAT;
 	model="\z\addons\dayz_communityassets\models\baseball_bat.p3d";
+	destrType = "DestructNo";
+	
 	class eventHandlers
 	{
 		init="[(_this select 0),'cfgWeapons','MeleeBaseBallBat'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
@@ -44,6 +50,8 @@ class WeaponHolder_MeleeBatBarbed: WeaponHolderBase
 	scope=2;
 	displayName=$STR_CRAFT_NAME_BaseBallBatBarbed;
 	model="\z\addons\dayz_communityassets\models\baseball_bat.p3d";
+	destrType = "DestructNo";
+	
 	class eventHandlers
 	{
 		init="[(_this select 0),'cfgWeapons','MeleeBaseBallBatBarbed'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
@@ -54,6 +62,8 @@ class WeaponHolder_MeleeBaseBallBatNails: WeaponHolderBase
 	scope=2;
 	displayName=$STR_CRAFT_NAME_NailedBaseballBat;
 	model="\z\addons\dayz_communityassets\models\baseball_bat_nails.p3d";
+	destrType = "DestructNo";
+	
 	class eventHandlers
 	{
 		init="[(_this select 0),'cfgWeapons','MeleeBaseBallBatNails'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
@@ -65,6 +75,8 @@ class WeaponHolder_MeleeMachete: WeaponHolderBase
 	scope=2;
 	displayName=$STR_EQUIP_NAME_MACHETE;
 	model="\z\addons\dayz_communityassets\models\machete.p3d";
+	destrType = "DestructNo";
+	
 	class eventHandlers
 	{
 		init="[(_this select 0),'cfgWeapons','MeleeMachete'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
@@ -74,6 +86,8 @@ class WeaponHolder_ItemJerrycanEmpty : WeaponHolderBase {
 	scope = public;
 	displayName = "$STR_EQUIP_NAME_39";
 	model = "\dayz_equip\proxy\jerrycan_side.p3d";
+	destrType = "DestructNo";
+	
 	class eventHandlers {
 		init = "[(_this select 0),'cfgMagazines','ItemJerrycanEmpty'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
 	};
@@ -82,6 +96,7 @@ class WeaponHolder_ItemFuelcanEmpty : WeaponHolderBase {
 	scope = public;
 	displayName = $STR_ITEM_NAME_FUELCAN_EMPTY;
 	model = "z\addons\dayz_communityassets\models\fuelcan.p3d";
+	destrType = "DestructNo";
 
 	class eventHandlers {
 		init = "[(_this select 0),'cfgMagazines','ItemFuelcanEmpty'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
@@ -91,6 +106,7 @@ class WeaponHolder_ItemFuelcan : WeaponHolderBase {
 	scope = public;
 	displayName = $STR_ITEM_NAME_FUELCAN;
 	model = "z\addons\dayz_communityassets\models\fuelcan.p3d";
+	destrType = "DestructNo";
 
 	class eventHandlers {
 		init = "[(_this select 0),'cfgMagazines','ItemFuelcan'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
@@ -101,6 +117,7 @@ class WeaponHolder_ItemCamoNet : WeaponHolderBase {
 	displayName = $STR_ITEM_NAME_CAMONET;
 //	model = "dayz_equip\proxy\tentbag.p3d"; // was models\tentbag_gear.prd ...
 	model = "z\addons\dayz_communityassets\models\packed_net_green.p3d"; // was models\tentbag_gear.prd ...
+	destrType = "DestructNo";
 
 	class eventHandlers {
 		init = "[(_this select 0),'cfgMagazines','forest_net_kit'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
@@ -111,6 +128,7 @@ class WeaponHolder_ItemDomeTent : WeaponHolderBase {
 	scope = public;
 	displayName = $STR_VEH_NAME_DOME_TENT;
 	model = "\dayz_equip\proxy\tentbag.p3d";
+	destrType = "DestructNo";
 	
 	class eventHandlers {
 		init = "[(_this select 0),'cfgMagazines','ItemDomeTent'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
@@ -121,6 +139,7 @@ class WeaponHolder_ItemDesertTent : WeaponHolderBase {
 	scope = public;
 	displayName = $STR_VEH_NAME_DESERT_TENT;
 	model = "\dayz_equip\proxy\tentbag.p3d";
+	destrType = "DestructNo";
 	
 	class eventHandlers {
 		init = "[(_this select 0),'cfgMagazines','ItemDesertTent'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
@@ -131,6 +150,7 @@ class WeaponHolder_ItemCrate : WeaponHolderBase {
 	scope = public;
 	displayName = $STR_ITEM_NAME_equip_crate;
 	model = "\z\addons\dayz_communityassets\models\crate.p3d";
+	destrType = "DestructNo";
 		
 	class eventHandlers {
 		init = "[(_this select 0),'cfgMagazines','equip_crate'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
