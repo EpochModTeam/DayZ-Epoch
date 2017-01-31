ec8f0ecb6b9efbd8409fee98856c0dc4beebe649
A Man <r2892@t-online.de>
2017-01-31 16:56:01 +0100

    adding new documents

---
+++ SQF/dayz_code/Configs/CfgWeapons/Item/Documents.hpp
@@ -0,0 +1,81 @@
+class Kostey_notebook;
+class Notebook_bio_DZE: Kostey_notebook 
+{
+	displayname = $STR_EQUIP_NOTEBOOK_BIO;
+	descriptionShort= $STR_EQUIP_NOTEBOOK_BIO_DESC;
+};
+
+class Notebook_che_DZE: Kostey_notebook 
+{
+	displayname = $STR_EQUIP_NOTEBOOK_CHE;
+	descriptionShort= $STR_EQUIP_NOTEBOOK_CHE_DESC;
+};
+
+class Notebook_phy_DZE: Kostey_notebook 
+{
+	displayname = $STR_EQUIP_NOTEBOOK_PHY;
+	descriptionShort= $STR_EQUIP_NOTEBOOK_PHY_DESC;
+};
+
+class Notebook_secret_DZE: Kostey_notebook 
+{
+	displayname = $STR_EQUIP_NOTEBOOK_SECRET;
+	descriptionShort= $STR_EQUIP_NOTEBOOK_SECRET_DESC;
+};
+
+class Notebook_herbs_DZE: Kostey_notebook 
+{
+	displayname = $STR_EQUIP_NOTEBOOK_HERBS;
+	descriptionShort= $STR_EQUIP_NOTEBOOK_HERBS_DESC;
+};
+
+class Moscow_Bombing_File;
+class Document_Random_DZE: Moscow_Bombing_File 
+{
+	displayname = $STR_EQUIP_DOCUMENT_RANDOM;
+	descriptionShort= $STR_EQUIP_DOCUMENT_RANDOM_DESC;
+};
+
+class Document_Military_DZE: Moscow_Bombing_File 
+{
+	displayname = $STR_EQUIP_DOCUMENT_MILITARY;
+	descriptionShort= $STR_EQUIP_DOCUMENT_MILITARY_DESC;
+};
+
+class Document_Topsecret_DZE: Moscow_Bombing_File 
+{
+	displayname = $STR_EQUIP_DOCUMENT_TOPSECRET;
+	descriptionShort= $STR_EQUIP_DOCUMENT_TOPSECRET_DESC;
+};
+
+class Cobalt_File;
+class Manual_electronics_DZE: Cobalt_File 
+{
+	displayname = $STR_EQUIP_MANUAL_ELECTRONICS;
+	descriptionShort= $STR_EQUIP_MANUAL_ELECTRONICS_DESC;
+};
+
+class Manual_camera_DZE: Cobalt_File 
+{
+	displayname = $STR_EQUIP_MANUAL_CAMERA;
+	descriptionShort= $STR_EQUIP_MANUAL_CAMERA_DESC;
+};
+
+class Manual_repair_DZE: Cobalt_File 
+{
+	displayname = $STR_EQUIP_MANUAL_REPAIR;
+	descriptionShort= $STR_EQUIP_MANUAL_REPAIR_DESC;
+};
+
+class Kostey_map_case;
+class Map_Military_DZE: Kostey_map_case 
+{
+	displayname = $STR_EQUIP_MAP_Military;
+	descriptionShort= $STR_EQUIP_MAP_Military_DESC;
+};
+
+class Map_Survivor_DZE: Kostey_map_case 
+{
+	displayname = $STR_EQUIP_MAP_SURVIVOR;
+	descriptionShort= $STR_EQUIP_MAP_SURVIVOR_DESC;
+};
