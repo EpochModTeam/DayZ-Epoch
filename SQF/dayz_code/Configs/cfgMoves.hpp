0275c58a8931d7d95ea9b7cd874aab866ffca83b
icomrade <icomrade@live.com>
2017-03-27 18:16:51 -0400

    Fix CFGMoves Server CTD
    Reported to CorePatch maintainer.

--- SQF/dayz_code/Configs/cfgMoves.hpp
+++ SQF/dayz_code/Configs/cfgMoves.hpp
@@ -5,6 +5,29 @@ class CfgMovesAnimalsBase {
 	class BlendAnims;
 };
 
+class CfgMovesBasic; //Fix for server CTD
+class CfgMovesMaleSdr: CfgMovesBasic
+{
+	class States
+	{
+		class TransAnimBase_noIK;
+		class AwopPercMstpSoptWbinDnon_AwopPknlMstpSoptWbinDnon_lnr: TransAnimBase_noIK
+		{
+			rightHandIKCurve[] = {};
+			leftHandIKCurve[] = {};
+			ConnectTo[] = {"AwopPknlMstpSoptWbinDnon_lnr",0.02};
+			InterpolateTo[] = {"AwopPknlMstpSoptWbinDnon_AwopPercMstpSoptWbinDnon_lnr",0.02};
+			actions = "BinocKneelLnrActions";
+			rightHandIKEnd = 0;
+			rightHandIKBeg = 0;
+			leftHandIKEnd = 0;
+			leftHandIKBeg = 0;
+			canPullTrigger = 0;
+			file = "\ca\Anims\Characters\data\Anim\Sdr\wop\erc\stp\opt\bin\AwopPercMstpSoptWbinDnon_AwopPknlMstpSoptWbinDnon_una";
+		};
+	};
+};
+
 class CfgMovesDogBase
 {
 	class Default
