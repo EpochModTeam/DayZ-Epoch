38de4b08593d9e2e333c77ee294b5fc53b8fae4f
icomrade <icomrade@live.com>
2017-06-19 14:57:16 -0400

    no longer needed

--- SQF/dayz_code/Configs/CfgWorlds.hpp
+++ SQF/dayz_code/Configs/CfgWorlds.hpp
@@ -1,7 +1,3 @@
-class DayLightingBrightAlmost;	// External class reference
-class DefaultClutter;	// External class reference
-class Grid;	// External class reference
-
 class CfgWorlds {
 	class Chernarus;
 	class DayZMod : Chernarus {
