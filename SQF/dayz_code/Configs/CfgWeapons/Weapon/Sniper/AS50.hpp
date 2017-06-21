fa8ca6371782e72456157b09263c59cb5039ae73
A Man <AirwavesMan@users.noreply.github.com>
2017-06-21 19:13:34 +0200

    Fix Corepatch AS50 magazine spawn
    The Corepatch broke some of the AS50 magazine spawns. Im not sure why it happend sometimes and sometimes not.
    
    This message showed up sometimes: No entry 'bin\config.bin/CfgMagazines-5Rnd_127x99_as50_CP'.
    
    The CorePatch use that: 
    
    https://github.com/Goliath86/CorePatch/blob/e0d06f7086fb0e3b3e6b7dc22beeca3b087967be/CorePatch_CIT_14888/config.cpp
    
    This commit should fix that error message and the AS50 name as well.

--- SQF/dayz_code/Configs/CfgWeapons/Weapon/Sniper/AS50.hpp
+++ SQF/dayz_code/Configs/CfgWeapons/Weapon/Sniper/AS50.hpp
@@ -1,8 +1,9 @@
 class BAF_AS50_scoped;
 class BAF_AS50_scoped_DZ : BAF_AS50_scoped {
-	displayname = "AS50_DZ";
+	displayname = "AS50";
 	type = WeaponSlotPrimary;
 	canlock = 0;
+	magazines[] = {"5Rnd_127x99_as50","10Rnd_127x99_m107","5Rnd_127x99_as50_CP"};
 	//cursor = "RifleCursor";
 	//cursoraim = "Foresight";
 };
