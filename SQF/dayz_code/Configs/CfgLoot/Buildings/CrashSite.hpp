9670426506001b7f99980dbfd98ed089fed654ee
A Man <AirwavesMan@users.noreply.github.com>
2018-10-08 16:09:50 +0200

    Adding some new zombies to crash sites

--- SQF/dayz_code/Configs/CfgLoot/Buildings/CrashSite.hpp
+++ SQF/dayz_code/Configs/CfgLoot/Buildings/CrashSite.hpp
@@ -6,7 +6,15 @@ class CrashSite : Default
 	zombieClass[] = {"z_soldier_pilot","z_soldier_heavy"};
 };
 
-class CrashSite_RU : CrashSite {};
-class CrashSite_US : CrashSite {};
-class CrashSite_EU : CrashSite {};
-class CrashSite_UN : CrashSite {};
+class CrashSite_RU : CrashSite {
+	zombieClass[] = {"z_soldier_ru_pilot","z_soldier_ru_crewman","z_soldier_ru_pilot","z_soldier_ru_crewman","z_soldier_ru_specialforces","z_soldier_ru_soldier","z_soldier_ru_soldier_heavy"};
+};
+class CrashSite_US : CrashSite {
+	zombieClass[] = {"z_soldier_usmc_pilot","z_soldier_usmc_crew","z_soldier_usmc_mg","z_soldier_usmc_soldier_heavy","z_soldier_usmc_soldier"};
+};
+class CrashSite_EU : CrashSite {
+	zombieClass[] = {"z_soldier_cz_pilot","z_soldier_cz_soldier_heavy","z_soldier_cz_spec"};
+};
+class CrashSite_UN : CrashSite {
+	zombieClass[] = {"z_soldier_cdf_crew","z_soldier_pilot","z_soldier_cdf_pilot","z_soldier_un_soldier_heavy","z_soldier_heavy"};
+};
