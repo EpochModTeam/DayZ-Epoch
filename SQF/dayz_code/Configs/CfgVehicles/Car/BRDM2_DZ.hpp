9ab576facc00c0e46a43db8188be80df7d9e0b9f
A Man <AirwavesMan@users.noreply.github.com>
2019-11-18 11:50:01 +0100

    Add missing BRDM2_HQ_TK_GUE_EP1_DZE
    This also extend the gunner optics angle so the PKT can be better used now.

--- SQF/dayz_code/Configs/CfgVehicles/Car/BRDM2_DZ.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Car/BRDM2_DZ.hpp
@@ -1,8 +1,13 @@
-class BRDM2_HQ_TK_GUE_EP1;
+class BRDM2_HQ_Base;
+class BRDM2_HQ_TK_GUE_EP1: BRDM2_HQ_Base {
+	class Turrets; // External class reference
+	class MainTurret; // External class reference	
+};
+
 class BRDM2_HQ_TK_GUE_EP1_DZ: BRDM2_HQ_TK_GUE_EP1 {
 	scope = public;
-	displayname = "BRDM2 (PKT)";
-	displaynameshort = "BRDM2 (PKT)";
+	displayname = "BRDM2 HQ (PKT)";
+	displaynameshort = "BRDM2 HQ (PKT)";
 	crew = "";
 	typicalCargo[] = {};
 	class TransportMagazines {};
@@ -13,4 +18,31 @@ class BRDM2_HQ_TK_GUE_EP1_DZ: BRDM2_HQ_TK_GUE_EP1 {
 	gunnerCanSee = 2+16+32;
 	driverCanSee = 2+16+32;
 	fuelCapacity = 220;
+
+	class Turrets: Turrets {
+		class MainTurret: MainTurret {
+			class ViewOptics {
+				initAngleX = 0;
+				minAngleX = -90;
+				maxAngleX = 90;
+				initAngleY = 0;
+				minAngleY = -100;
+				maxAngleY = 100;
+				initFov = 0.2;
+				minFov = 0.05;
+				maxFov = 0.2;
+				gunnerOpticsEffect[] = {"TankGunnerOptics1","OpticsBlur2","OpticsCHAbera2"};
+				visionMode[] = {"Normal","NVG"};
+			};
+			magazines[] = {"100Rnd_762x54_PK","100Rnd_762x54_PK","100Rnd_762x54_PK","100Rnd_762x54_PK"};
+		};
+	};		
+};
+
+class BRDM2_HQ_TK_GUE_EP1_DZE: BRDM2_HQ_TK_GUE_EP1_DZ {
+	class Turrets: Turrets {
+		class MainTurret: MainTurret {
+			magazines[] = {};
+		};
+	};		
 };
