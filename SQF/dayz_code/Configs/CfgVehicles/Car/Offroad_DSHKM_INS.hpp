8030ad69f90521b7b66cc0519b6f5a9d07e37ab0
A Man <AirwavesMan@users.noreply.github.com>
2019-12-08 12:29:50 +0100

    Clean up offroad DSHKM configs

--- SQF/dayz_code/Configs/CfgVehicles/Car/Offroad_DSHKM_INS.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Car/Offroad_DSHKM_INS.hpp
@@ -50,47 +50,13 @@ class Offroad_DSHKM_Gue_DZ: Offroad_DSHKM_base
 	};
 };
 
-class Offroad_DSHKM_Gue_DZE: Offroad_DSHKM_base
+class Offroad_DSHKM_Gue_DZE: Offroad_DSHKM_Gue_DZ
 {
-	scope = public;
-	side = 2;
-	faction = "GUE";
-	crew = "";
-	typicalCargo[] = {};
-	class TransportMagazines {};
-	class TransportWeapons {};
-	displayName = $STR_VEH_NAME_PICKUP_DSHKM;
-	hiddenSelectionsTextures[] = {"\ca\wheeled\hilux_armed\data\coyota_trup4_CO.paa"};
-	class Damage
-	{
-		tex[] = {};
-		mat[] = {"ca\wheeled\hilux_armed\data\detailmapy\coyota_armed.rvmat","ca\wheeled\hilux_armed\data\detailmapy\coyota_armed.rvmat","ca\wheeled\hilux_armed\data\detailmapy\coyota_armed_destruct.rvmat","ca\weapons\data\dshk.rvmat","ca\weapons\data\dshk.rvmat","ca\weapons\data\dshk_destruct.rvmat","ca\weapons\data\tripod_dshk.rvmat","ca\weapons\data\tripod_dshk.rvmat","ca\wheeled\hilux_armed\data\detailmapy\tripod_dshk_destruct.rvmat","ca\weapons\data\pkm.rvmat","ca\weapons\data\pkm.rvmat","ca\weapons\data\pkm_destruct.rvmat","ca\wheeled\hilux_armed\data\detailmapy\coyota_trup3.rvmat","ca\wheeled\hilux_armed\data\detailmapy\coyota_trup3.rvmat","ca\wheeled\hilux_armed\data\detailmapy\coyota_trup3_destruct.rvmat","ca\wheeled\hilux_armed\data\detailmapy\drziaky.rvmat","ca\wheeled\hilux_armed\data\detailmapy\drziaky.rvmat","ca\wheeled\hilux_armed\data\detailmapy\drziaky_destruct.rvmat","ca\wheeled\data\detailmapy\coyota_kola.rvmat","ca\wheeled\data\detailmapy\coyota_kola.rvmat","ca\wheeled\data\detailmapy\coyota_kola_destruct.rvmat","ca\wheeled\data\detailmapy\coyota_interier.rvmat","ca\wheeled\data\detailmapy\coyota_interier.rvmat","ca\wheeled\data\detailmapy\coyota_interier_destruct.rvmat","ca\wheeled\data\detailmapy\auta_skla.rvmat","ca\wheeled\data\detailmapy\auta_skla_damage.rvmat","ca\wheeled\data\detailmapy\auta_skla_damage.rvmat","ca\wheeled\data\detailmapy\auta_skla_in.rvmat","ca\wheeled\data\detailmapy\auta_skla_in_damage.rvmat","ca\wheeled\data\detailmapy\auta_skla_in_damage.rvmat"};
-	};
 	class Turrets: Turrets
 	{
 		class MainTurret: MainTurret
 		{
-			body = "mainTurret";
-			gun = "mainGun";
-			outGunnerMayFire = 1;
-			forceHideGunner = 1;
-			castGunnerShadow = 1;
-			viewGunnerInExternal = 1;
-			gunnerOpticsModel = "\ca\Weapons\optika_empty";
-			gunnerForceOptics = 0;
-			weapons[] = {"DShKM"};
-			soundServo[] = {};
-			stabilizedInAxes = 0;
 			magazines[] = {};
-			gunnerAction = "Hilux_Gunner";
-			gunnerInAction = "Hilux_Gunner";
-			ejectDeadGunner = 1;
-			minElev = -18;
-			maxElev = 60;
-			minTurn = -360;
-			maxTurn = 360;
-			initTurn = 0;
-			gunnerCompartments = "Compartment2";
 		};
 	};
 	class Upgrades
