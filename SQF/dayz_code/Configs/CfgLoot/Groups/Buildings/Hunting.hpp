7fb4a19faedfa6ce0e88fc4d3b4551ad01c315ad
worldwidesorrow <jsnmerrill@yahoo.com>
2020-01-09 18:05:40 -0600

    Update Hunting.hpp
    File reorganized.

--- SQF/dayz_code/Configs/CfgLoot/Groups/Buildings/Hunting.hpp
+++ SQF/dayz_code/Configs/CfgLoot/Groups/Buildings/Hunting.hpp
@@ -1,26 +1,19 @@
 Hunting[] =
 {
 	//Weapons
-	{Loot_GROUP,		5,		farmweapons},
-	{Loot_GROUP,		3,		Military},
-	{Loot_WEAPON,		2,		Mosin_DZ},
+	{Loot_GROUP,		10,		shotgunsingleshot},
+	{Loot_GROUP,		1,		Chainsaws},
+	{Loot_WEAPON,		1,		DMR_Gh_DZ},
+	{Loot_WEAPON,		1,		RedRyder},
 	
 	//Tools
-	{Loot_WEAPON,		4,		ItemMachete},
-	{Loot_WEAPON,		6,		ItemMap},
-	{Loot_WEAPON,		6,		ItemFlashlight},
-	{Loot_WEAPON,		7,		ItemKnife},
-	{Loot_WEAPON,		2,		ItemCompass},
-	{Loot_WEAPON,		4,		ItemHatchet},
-	{Loot_MAGAZINE,		1,		5Rnd_17HMR},
-	{Loot_MAGAZINE,		1,		10Rnd_303British},
+	{Loot_GROUP,		5,		ToolsSurvival},
 	{Loot_MAGAZINE,		2,		ItemWaterbottleUnfilled},
 	{Loot_MAGAZINE,		2,		ItemHeatPack},
 	{Loot_MAGAZINE,		2,		FoodMRE},
 	{Loot_MAGAZINE,		2,		FoodNutmix},
 	{Loot_MAGAZINE,		1,		ItemDocument},
 	{Loot_MAGAZINE,		1,		ItemMixOil},
-	{Loot_WEAPON,		1,		RedRyder},
 	{Loot_MAGAZINE,		1,		350Rnd_BB_Magazine},
 	
 	//Backpacks
@@ -28,17 +21,16 @@ Hunting[] =
 	{Loot_GROUP,		1,		militarybackpacks},
 	
 	//Other
-	{Loot_MAGAZINE,		2,		ItemTent},
+	{Loot_GROUP,		3,		tents},
 	{Loot_MAGAZINE,		1,		equip_tent_poles},
 	{Loot_MAGAZINE,		2,		ItemBandage},
-	{Loot_MAGAZINE,		1,		ItemDomeTent},
 	{Loot_MAGAZINE,		3,		TrapBear},
 	{Loot_MAGAZINE,		3,		12Rnd_Quiver_Wood},
 	{Loot_MAGAZINE,		2,		1Rnd_Bolt_Tranquilizer},
 	{Loot_MAGAZINE,		1,		1Rnd_Bolt_Explosive},
+	{Loot_MAGAZINE,		1,		20Rnd_762x51_DMR},
 	
 	{Loot_PILE,			10,		AmmoCivilian, 1, 3},
 	{Loot_PILE,			10,		Consumable, 1, 3},
-	{Loot_WEAPON,		3,		ItemMatchbox},
 	{Loot_GROUP,		10,		Generic}
 };
