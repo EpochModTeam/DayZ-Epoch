2f39dc01b9073373845bdd14cce5394b4ff066c6
worldwidesorrow <jsnmerrill@yahoo.com>
2020-01-09 18:11:15 -0600

    Give military industrial groups their own file.

---
+++ SQF/dayz_code/Configs/CfgLoot/Groups/Buildings/MilitaryIndustrial.hpp
@@ -0,0 +1,85 @@
+// Military and Military Industrial buildings spawn matching loot based on RU, US, EU and CZ groups.
+
+MilitaryIndustrial[] = 
+{
+	{Loot_NONE,		1,		MilitaryIndustrialRU, MilitaryIndustrialSmallRU}, //Russian Weapons, Attachments, Ammo
+	{Loot_NONE,		1,		MilitaryIndustrialUS, MilitaryIndustrialSmallUS},	//American Weapons, Attachments, Ammo
+	{Loot_NONE,		1,		MilitaryIndustrialEU, MilitaryIndustrialSmallEU}, //European Weapons, Attachments, Ammo
+	{Loot_NONE,		1,		MilitaryIndustrialCZ, MilitaryIndustrialSmallCZ} //Czech Weapons, Attachments, Ammo
+};
+
+MilitaryIndustrialRU[] = {
+	{Loot_GROUP,		15,		Parts},
+	{Loot_MAGAZINE,		1,		PartVRotor},
+	{Loot_MAGAZINE,		4,		FoodMRE},
+	{Loot_GROUP,		1,		FuelBarrel},
+	{Loot_MAGAZINE,		1,		ItemGenerator},
+	{Loot_MAGAZINE,		1,		fuel_pump_kit},
+	{Loot_MAGAZINE,		1,		ItemTankTrap},
+	{Loot_MAGAZINE,		4,		ItemSandbag},
+	{Loot_MAGAZINE,		1,		ItemWire},
+	{Loot_PILE,			5,		Trash, 1, 2},
+	{Loot_PILE,			3,		Consumable, 1, 2},
+	{Loot_GROUP, 		10, 	MilitaryRU}
+};
+
+MilitaryIndustrialSmallRU[] = {
+	{Loot_GROUP, 		1, 	MilitarySmallRU}
+};
+
+MilitaryIndustrialUS[] = {
+	{Loot_GROUP,		15,		Parts},
+	{Loot_MAGAZINE,		1,		PartVRotor},
+	{Loot_MAGAZINE,		4,		FoodMRE},
+	{Loot_GROUP,		1,		FuelBarrel},
+	{Loot_MAGAZINE,		1,		ItemGenerator},
+	{Loot_MAGAZINE,		1,		fuel_pump_kit},
+	{Loot_MAGAZINE,		1,		ItemTankTrap},
+	{Loot_MAGAZINE,		4,		ItemSandbag},
+	{Loot_MAGAZINE,		1,		ItemWire},
+	{Loot_PILE,			5,		Trash, 1, 2},
+	{Loot_PILE,			3,		Consumable, 1, 2},
+	{Loot_GROUP, 		10, 	MilitaryUS}
+};
+
+MilitaryIndustrialSmallUS[] = {
+	{Loot_GROUP, 		1, 	MilitarySmallUS}
+};
+
+MilitaryIndustrialEU[] = {
+	{Loot_GROUP,		15,		Parts},
+	{Loot_MAGAZINE,		1,		PartVRotor},
+	{Loot_MAGAZINE,		4,		FoodMRE},
+	{Loot_GROUP,		1,		FuelBarrel},
+	{Loot_MAGAZINE,		1,		ItemGenerator},
+	{Loot_MAGAZINE,		1,		fuel_pump_kit},
+	{Loot_MAGAZINE,		1,		ItemTankTrap},
+	{Loot_MAGAZINE,		4,		ItemSandbag},
+	{Loot_MAGAZINE,		1,		ItemWire},
+	{Loot_PILE,			5,		Trash, 1, 2},
+	{Loot_PILE,			3,		Consumable, 1, 2},
+	{Loot_GROUP, 		10, 	MilitaryEU}
+};
+
+MilitaryIndustrialSmallEU[] = {
+	{Loot_GROUP, 		1, 	MilitarySmallEU}
+};
+
+MilitaryIndustrialCZ[] = {
+	{Loot_GROUP,		15,		Parts},
+	{Loot_MAGAZINE,		1,		PartVRotor},
+	{Loot_MAGAZINE,		4,		FoodMRE},
+	{Loot_GROUP,		1,		FuelBarrel},
+	{Loot_MAGAZINE,		1,		ItemGenerator},
+	{Loot_MAGAZINE,		1,		fuel_pump_kit},
+	{Loot_MAGAZINE,		1,		ItemTankTrap},
+	{Loot_MAGAZINE,		4,		ItemSandbag},
+	{Loot_MAGAZINE,		1,		ItemWire},
+	{Loot_PILE,			5,		Trash, 1, 2},
+	{Loot_PILE,			3,		Consumable, 1, 2},
+	{Loot_GROUP, 		10, 	MilitaryCZ}
+};
+
+MilitaryIndustrialSmallCZ[] = {
+	{Loot_GROUP, 		1, 	MilitarySmallCZ}
+};
