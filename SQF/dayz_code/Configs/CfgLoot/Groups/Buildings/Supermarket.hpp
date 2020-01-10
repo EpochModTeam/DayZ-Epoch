3a7f1bd90cd959aa120aaada6155e3778b2b5056
worldwidesorrow <jsnmerrill@yahoo.com>
2020-01-09 18:14:02 -0600

    Update Supermarket.hpp
    File reorganized.

--- SQF/dayz_code/Configs/CfgLoot/Groups/Buildings/Supermarket.hpp
+++ SQF/dayz_code/Configs/CfgLoot/Groups/Buildings/Supermarket.hpp
@@ -1,70 +1,45 @@
 Supermarket[] =
 {
 	//Tools
-	{Loot_WEAPON,		5,		ItemWatch},
-	{Loot_WEAPON,		1,		ItemCompass},
-	{Loot_WEAPON,		6,		ItemMap},
-	{Loot_WEAPON,		5,		ItemFlashlight},
-	{Loot_WEAPON,		2,		ItemKnife},
-	{Loot_WEAPON,		3,		Binocular},
-//	{Loot_WEAPON,		2,		ItemDIY_wood},
-//	{Loot_WEAPON,		1,		ItemDIY_Gate},
+	{Loot_GROUP,		5,		ToolsSurvival},
 	
 	//Pistols
-	{Loot_GROUP,		10,		pistols},
-	{Loot_GROUP,		5,		shotgunsingleshot},
+	{Loot_GROUP,		5,		pistols},
+	
 	//Rifles
-/*	{Loot_WEAPON,		1,		LeeEnfield_DZ},
-	{Loot_WEAPON,		1,		Mosin_DZ},
-	{Loot_WEAPON,		2,		Winchester1866_DZ},
-	{Loot_WEAPON,		2,		MR43_DZ},
-	{Loot_WEAPON,		2,		Crossbow_DZ},*/
+	{Loot_GROUP,		1,		shotgunsingleshot},
 	
 	//Backpacks
 	{Loot_GROUP,		5,		backpacks},
 	
 	//Items
-	//{Loot_MAGAZINE,	1,		ItemTent}, //IN TENTS GROUP
 	{Loot_MAGAZINE,		1,		ItemMixOil},
 	{Loot_MAGAZINE,		1,		PartPlywoodPack},
-	//{Loot_MAGAZINE,	0.5,	ItemDomeTent}, //IN TENTS GROUP
 	{Loot_MAGAZINE,		1,		forest_net_kit},
-	{Loot_MAGAZINE,		0.1,	ItemComboLock},
-	{Loot_MAGAZINE,		3,		ItemBandage},
-	{Loot_MAGAZINE,		2,		ItemPainkiller},
-	{Loot_MAGAZINE,		2,		ItemAntibacterialWipe},
-	{Loot_MAGAZINE,		1,		equip_nails},
+	
+	// Piles
+	{Loot_PILE,			5,		Consumable, 1, 3},
+	{Loot_PILE,			5,		AmmoCivilian, 1, 3},
 	
 	//Groups
-	{Loot_GROUP,		40,		Consumable},
-	{Loot_GROUP,		20,		Generic},
-	{Loot_GROUP,		10,		AmmoCivilian},
+	{Loot_GROUP,		30,		Consumable},
+	{Loot_GROUP,		10,		Generic},
 	{Loot_GROUP,		3,		AttachmentsGeneric},
 	{Loot_GROUP,		3,		tents},
-	{Loot_WEAPON,		3,		ItemMatchbox},
-//	{Loot_GROUP,		3,		MedicalLow},
-	{Loot_GROUP,		10,		Trash}
+	{Loot_GROUP,		3,		Trash},
+	{Loot_CONTAINER,	1,		DZ_CardboardBox, Consumable, 10, 20}
 };
 
 SupermarketSmall[] =
 {
-	{Loot_WEAPON,		5,		ItemWatch},
-	{Loot_WEAPON,		1,		ItemCompass},
-	{Loot_WEAPON,		6,		ItemMap},
-	{Loot_WEAPON,		2,		ItemKnife},
-	{Loot_WEAPON,		3,		Binocular},
-	{Loot_GROUP,		10,		pistols},
-	{Loot_MAGAZINE,		1,		ItemMixOil},
-	{Loot_MAGAZINE,		0.1,	ItemComboLock},
-	{Loot_MAGAZINE,		3,		ItemBandage},
-	{Loot_MAGAZINE,		2,		ItemPainkiller},
-	{Loot_MAGAZINE,		2,		ItemAntibacterialWipe},
-	{Loot_MAGAZINE,		5,		equip_nails},
-	{Loot_GROUP,		40,		Consumable},
-	{Loot_GROUP,		20,		GenericSmall},
+	{Loot_GROUP,		1,		ToolsSurvival},
+	{Loot_MAGAZINE,		2,		ItemMixOil},
+	{Loot_MAGAZINE,		1,		ItemComboLock},
+	{Loot_GROUP,		3,		VanillaSurvival},
+	{Loot_GROUP,		30,		Consumable},
+	{Loot_GROUP,		10,		GenericSmall},
 	{Loot_GROUP,		10,		AmmoCivilian},
 	{Loot_GROUP,		3,		AttachmentsGeneric},
-	{Loot_WEAPON,		3,		ItemMatchbox},
 	{Loot_GROUP,		10,		Trash},
 	{Loot_GROUP,		3,		MedicalLow}
 };
