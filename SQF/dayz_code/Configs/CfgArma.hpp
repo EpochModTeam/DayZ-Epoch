892ca2dd773bdec655bff4f59f5714ea59ebc1d4
A Man <AirwavesMan@users.noreply.github.com>
2020-01-18 18:11:00 +0100

    Change CfgAISkill values back to default
    If anyone knows the reason why it got changed please message me on discord.

--- SQF/dayz_code/Configs/CfgArma.hpp
+++ SQF/dayz_code/Configs/CfgArma.hpp
@@ -147,12 +147,12 @@ class CfgAISkill {
 	aimingshake[] = {0, 0, 1, 1};
 	aimingspeed[] = {0, 0, 1, 1};
 	commanding[] = {0, 0, 1, 1};
-	courage[] = {0, 5, 10, 10};
-	endurance[] = {0, 0, 1, 0.1};
-	general[] = {0, 0, 1, 0.1};
+	courage[] = {0, 0, 1, 1};
+	endurance[] = {0, 0, 1, 1};
+	general[] = {0, 0, 1, 1};
 	reloadspeed[] = {0, 0, 1, 1};
-	spotdistance[] = {0, 0, 1, 0.6};
-	spottime[] = {0, 0, 1, 0.1};
+	spotdistance[] = {0, 0, 1, 1};
+	spottime[] = {0, 0, 1, 1};
 };
 
 class CfgLoadingTexts {
