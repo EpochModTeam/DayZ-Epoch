6f9afafaeb0784e2e87b1d132ff39b478d5a06cb
A Man <AirwavesMan@users.noreply.github.com>
2020-01-20 00:55:47 +0100

    Add more BRDM2 versions

--- SQF/dayz_code/Configs/CfgVehicles/Car/BRDM2_DZ.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Car/BRDM2_DZ.hpp
@@ -45,3 +45,77 @@ class BRDM2_HQ_TK_GUE_EP1_DZE: BRDM2_HQ_TK_GUE_EP1_DZ {
 		};
 	};		
 };
+
+class BRDM2_Base;
+class BRDM2_TK_EP1: BRDM2_Base {
+	class Turrets; // External class reference
+	class MainTurret; // External class reference	
+};
+class BRDM2_TK_EP1_DZ: BRDM2_TK_EP1 {
+	displayname = $STR_VEH_NAME_BRDM2_TK;
+	crew = "";
+	typicalCargo[] = {};
+	class TransportMagazines {};
+	class TransportWeapons {};
+	armor = 85;
+	damageResistance = 0.032;
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	fuelCapacity = 220;	
+	class Turrets: Turrets
+	{
+		class MainTurret: MainTurret
+		{
+			weapons[] = {"PKTBC"};
+			magazines[] = {"100Rnd_762x54_PK","100Rnd_762x54_PK","100Rnd_762x54_PK","100Rnd_762x54_PK"};
+			class ViewOptics
+			{
+				initAngleX = 0;
+				minAngleX = -30;
+				maxAngleX = 30;
+				initAngleY = 0;
+				minAngleY = -100;
+				maxAngleY = 100;
+				initFov = 0.2;
+				minFov = 0.05;
+				maxFov = 0.2;
+				gunnerOpticsEffect[] = {"TankGunnerOptics1","OpticsBlur2","OpticsCHAbera2"};
+				visionMode[] = {"Normal","NVG"};
+			};
+		};
+	};
+};
+
+class BRDM2_TK_EP1_DZE: BRDM2_TK_EP1_DZ {
+	class Turrets: Turrets {
+		class MainTurret: MainTurret {
+			magazines[] = {};
+		};
+	};		
+};
+
+class BRDM2_TK_GUE_EP1_DZ: BRDM2_TK_EP1_DZ {
+	displayname = $STR_VEH_NAME_BRDM2_GUE;
+	hiddenSelectionsTextures[] = {"\ca\wheeled\data\BDRM2_KHK_01_CO.paa"};
+	hiddenSelections[] = {"Camo1"};	
+};
+
+class BRDM2_TK_GUE_EP1_DZE: BRDM2_TK_EP1_DZE {
+	displayname = $STR_VEH_NAME_BRDM2_GUE;
+	hiddenSelectionsTextures[] = {"\ca\wheeled\data\BDRM2_KHK_01_CO.paa"};
+	hiddenSelections[] = {"Camo1"};	
+};
+
+class BRDM2_Desert_ACR_DZ: BRDM2_TK_EP1_DZ {
+	displayname = $STR_VEH_NAME_BRDM2_DES;
+	hiddenSelections[] = {"camo1","camo2"};
+	hiddenSelectionsTextures[] = {"\CA\Wheeled_ACR\Data\BDRM2_01_ACR_DES_CO.paa","\CA\Wheeled_ACR\Data\BDRM2_02_ACR_DES_CO.paa"};
+};
+
+class BRDM2_Desert_ACR_DZE: BRDM2_TK_EP1_DZE {
+	displayname = $STR_VEH_NAME_BRDM2_DES;
+	hiddenSelections[] = {"camo1","camo2"};
+	hiddenSelectionsTextures[] = {"\CA\Wheeled_ACR\Data\BDRM2_01_ACR_DES_CO.paa","\CA\Wheeled_ACR\Data\BDRM2_02_ACR_DES_CO.paa"};
+};
+
