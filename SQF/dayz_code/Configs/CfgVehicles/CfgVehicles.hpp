16b426bd2d43c973900428af85b609784552bd1d
worldwidesorrow <jsnmerrill@yahoo.com>
2020-01-21 15:04:22 -0600

    Update CfgVehicles.hpp

--- SQF/dayz_code/Configs/CfgVehicles/CfgVehicles.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/CfgVehicles.hpp
@@ -392,6 +392,8 @@ class CfgVehicles
 	//ZEDS
 	#include "Zeds\Zeds.hpp" //old type zeds
 	#include "Zeds\ViralZeds.hpp" //Viral type zeds
+	#include "Zeds\WildZeds.hpp" //Viral type zeds
+	#include "Zeds\SwarmZeds.hpp" //Swarm
 	#include "Zeds\PlayerZeds.hpp"
 	//Skins
 	#include "Skins\Female.hpp"
