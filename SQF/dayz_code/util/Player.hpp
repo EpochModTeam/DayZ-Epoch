0bbf4b41189a96fb392e00f9a16e4caf5661a843
worldwidesorrow <jsnmerrill@yahoo.com>
2020-01-21 11:58:08 -0600

    Update Player.hpp

--- SQF/dayz_code/util/Player.hpp
+++ SQF/dayz_code/util/Player.hpp
@@ -11,7 +11,7 @@ Author: Foxy
 //True if player is on a ladder, otherwise false
 #define Player_IsOnLadder() ((getNumber (configFile >> "CfgMovesMaleSdr" >> "States" >> (animationState player) >> "onLadder")) == 1)
 
-#define Player_IsInVehicle() (player != vehicle player);
+#define Player_IsInVehicle (player != vehicle player)
 
 //Gets the classname of the player's sidearm. nil if the player has no sidearm.
 #define Player_GetSidearm() ({ if ((getNumber (configFile >> "CfgWeapons" >> _x >> "type")) == 2) exitWith { _x }; nil } foreach weapons player)
