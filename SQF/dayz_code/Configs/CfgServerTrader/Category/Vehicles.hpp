5843125a122e3ed6c1dffc15166ed71e0e24a01e
A Man <AirwavesMan@users.noreply.github.com>
2020-01-25 21:37:04 +0100

    Update Vehicles.hpp

--- SQF/dayz_code/Configs/CfgServerTrader/Category/Vehicles.hpp
+++ SQF/dayz_code/Configs/CfgServerTrader/Category/Vehicles.hpp
@@ -1,7 +1,7 @@
 //Bikes/ATV
 class Category_37 {
 	class MMT_Civ_DZE {type = "trade_any_bicycle";buy[] = {4,"ItemSilverBar"};sell[] = {2,"ItemSilverBar"};};
-	class Old_bike_TK_INS_EP1_DZE {type = "trade_any_bicycle";buy[] = {4,"ItemSilverBar"};sell[] = {2,"ItemSilverBar"};};
+	class Old_bike_TK_CIV_EP1_DZE {type = "trade_any_bicycle";buy[] = {4,"ItemSilverBar"};sell[] = {2,"ItemSilverBar"};};
 	class Old_moto_TK_Civ_EP1_DZE {type = "trade_any_vehicle";buy[] = {2,"ItemGoldBar"};sell[] = {1,"ItemGoldBar"};};
 	class M1030_US_DES_EP1_DZE {type = "trade_any_vehicle";buy[] = {2,"ItemGoldBar"};sell[] = {1,"ItemGoldBar"};};
 	class TT650_Civ_DZE {type = "trade_any_vehicle";buy[] = {2,"ItemGoldBar"};sell[] = {1,"ItemGoldBar"};};
