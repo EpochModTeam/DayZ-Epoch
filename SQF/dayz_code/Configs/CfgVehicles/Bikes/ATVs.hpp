503e4c4864beed0392889b776347d7ae7e8a3a98
A Man <AirwavesMan@users.noreply.github.com>
2020-01-25 21:38:04 +0100

    Update ATVs

--- SQF/dayz_code/Configs/CfgVehicles/Bikes/ATVs.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Bikes/ATVs.hpp
@@ -2,50 +2,60 @@ class ATV_CZ_EP1;
 class ATV_CZ_EP1_DZE: ATV_CZ_EP1 {
 	crew = "";
 	displayname = $STR_VEH_NAME_ATV_BEIGE;
+	vehicleClass = "DayZ Epoch Vehicles";	
 	scope = public;
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
+	supplyRadius = 1.2;
 };
 
 class BAF_ATV_W;
 class BAF_ATV_W_DZE: BAF_ATV_W {
 	crew = "";
 	displayname = $STR_VEH_NAME_ATV_GREEN;
+	vehicleClass = "DayZ Epoch Vehicles";	
 	scope = public;
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
+	supplyRadius = 1.2;
 };
 
 class ATV_CIV_CP;
 class ATV_CIV_CP_DZE: ATV_CIV_CP {
 	crew = "";
 	displayname = $STR_VEH_NAME_ATV_WHITE;
+	vehicleClass = "DayZ Epoch Vehicles";	
 	scope = public;
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
+	supplyRadius = 1.2;
 };
 
 class ATV_CIV_Grey_CP;
 class ATV_CIV_Grey_CP_DZE: ATV_CIV_Grey_CP {
 	crew = "";
 	displayname = $STR_VEH_NAME_ATV_GREY;
+	vehicleClass = "DayZ Epoch Vehicles";	
 	scope = public;
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
+	supplyRadius = 1.2;
 };
 
 class ATV_CIV_Red_CP;
 class ATV_CIV_Red_CP_DZE: ATV_CIV_Red_CP {
 	crew = "";
 	displayname = $STR_VEH_NAME_ATV_RED;
+	vehicleClass = "DayZ Epoch Vehicles";	
 	scope = public;
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
+	supplyRadius = 1.2;
 };
 
 class ATV_CIV_Green_CP;
@@ -56,16 +66,19 @@ class ATV_CIV_Green_CP_DZE: ATV_CIV_Green_CP {
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
+	supplyRadius = 1.2;
 };
 
 class ATV_CIV_Blue_CP;
 class ATV_CIV_Blue_CP_DZE: ATV_CIV_Blue_CP {
 	crew = "";
 	displayname = $STR_VEH_NAME_ATV_BLUE;
+	vehicleClass = "DayZ Epoch Vehicles";	
 	scope = public;
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
+	supplyRadius = 1.2;
 };
 
 class ATV_CIV_Yellow_CP;
@@ -76,26 +89,31 @@ class ATV_CIV_Yellow_CP_DZE: ATV_CIV_Yellow_CP {
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
+	supplyRadius = 1.2;
 };
 
 class ATV_CIV_Purple_CP;
 class ATV_CIV_Purple_CP_DZE: ATV_CIV_Purple_CP {
 	crew = "";
 	displayname = $STR_VEH_NAME_ATV_PURPLE;
+	vehicleClass = "DayZ Epoch Vehicles";	
 	scope = public;
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
+	supplyRadius = 1.2;
 };
 
 class ATV_CIV_Black_CP;
 class ATV_CIV_Black_CP_DZE: ATV_CIV_Black_CP {
 	crew = "";
 	displayname = $STR_VEH_NAME_ATV_BLACK;
+	vehicleClass = "DayZ Epoch Vehicles";	
 	scope = public;
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
+	supplyRadius = 1.2;
 };
 
 
