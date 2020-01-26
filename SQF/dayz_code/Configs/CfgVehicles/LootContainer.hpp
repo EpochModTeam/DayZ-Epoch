3f5fa9a8c58fe4174efe728a648250cfd521bf8f
A Man <AirwavesMan@users.noreply.github.com>
2020-01-26 10:25:49 +0100

    Add ammo crates with more backpack slots

--- SQF/dayz_code/Configs/CfgVehicles/LootContainer.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/LootContainer.hpp
@@ -45,3 +45,79 @@ class DZ_MedBox : ReammoBox
 	model = "z\addons\dayz_communityassets\models\medical_freezbox.p3d";
 	displayName = $STR_DAYZ_OBJ_2;
 };
+
+class DZ_AmmoBoxSmallUS : ReammoBox
+{
+	scope = public;
+	
+	model = "\ca\weapons\AmmoBoxes\USBasicAmmo.p3d";
+	displayName = $STR_DAYZ_OBJ_3;
+	transportMaxMagazines = 10000;
+	transportMaxWeapons = 10000;
+	transportMaxBackpacks = 100;
+};
+
+class DZ_AmmoBoxFlatUS : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\USLaunchers.p3d";
+};
+
+class DZ_AmmoBoxMedium1US : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\USBasicWeapons.p3d";
+};
+
+class DZ_AmmoBoxMedium2US : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\USSpecialWeapons.p3d";
+};
+
+class DZ_AmmoBoxBigUS : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\USVehicleAmmo.p3d";
+};
+
+class DZ_AmmoBoxSmallRU : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\RUBasicAmmo.p3d";
+};
+
+class DZ_AmmoBoxLong1RU : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\RULaunchers.p3d";
+};
+
+class DZ_AmmoBoxLong2RU : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\RUSpecialWeapons.p3d";
+};
+
+class DZ_AmmoBoxFlatRU : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\RUOrdnance.p3d";
+};
+
+class DZ_AmmoBoxBigRU : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\RUVehicleAmmo.p3d";
+};
+
+class DZ_AmmoBoxSmallUN : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\LocalBasicAmmo.p3d";
+};
+
+class DZ_AmmoBoxFlatUN : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\LocalBasicWeapons.p3d";
+};
+
+class DZ_AmmoBoxSmallGUE : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\GuerillaCache.p3d";
+};
+
+class DZ_AmmoBoxLongGUE : DZ_AmmoBoxSmallUS
+{	
+	model = "\ca\weapons\AmmoBoxes\SpecialWeapons.p3d";
+};
