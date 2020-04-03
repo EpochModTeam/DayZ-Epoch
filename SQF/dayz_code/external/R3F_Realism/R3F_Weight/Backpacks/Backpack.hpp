838e4a066a6ac752d696c7303b79646c4120d6ce
A Man <AirwavesMan@users.noreply.github.com>
2020-04-03 12:46:57 +0200

    Add backpack weight config
    Thx @Damian6666 for making this file.
    
    Updated configs for the other items will be added soon.

---
+++ SQF/dayz_code/external/R3F_Realism/R3F_Weight/Backpacks/Backpack.hpp
@@ -0,0 +1,353 @@
+class DZ_Patrol_Pack_EP1
+{
+	weight = 2;
+};
+class DZ_Patrol_Pack_DZE1
+{
+	weight = 2.2;
+};
+class DZ_Patrol_Pack_DZE2
+{
+	weight = 2.4;
+};
+class DZ_TerminalPack_EP1
+{
+	weight = 2;
+};
+class TerminalPack_DZE1
+{
+	weight = 2.2;
+};
+class TerminalPack_DZE2
+{
+	weight = 2.4;
+};
+class TinyPack_DZE1
+{
+	weight = 2.2;
+};
+class TinyPack_DZE2
+{
+	weight = 2.4;
+};
+class DZ_Assault_Pack_EP1
+{
+	weight = 2;
+};
+class Assault_Pack_DZE1
+{
+	weight = 2.2;
+};
+class Assault_Pack_DZE2
+{
+	weight = 2.4;
+};
+class DZ_Czech_Vest_Pouch
+{
+	weight = 2;
+};
+class  Czech_Vest_Pouch_DZE1
+{
+	weight = 2.2;
+};
+class Czech_Vest_Pouch_DZE2
+{
+	weight = 2.4;
+};
+class DZ_ALICE_Pack_EP1
+{
+	weight = 3;
+};
+class ALICE_Pack_DZE1
+{
+	weight = 3.2;
+};
+class ALICE_Pack_DZE2
+{
+	weight = 3.4;
+};
+class DZ_CompactPack_EP1
+{
+	weight = 3;
+};
+class CompactPack_DZE1
+{
+	weight = 3.2;
+};
+class CompactPack_DZE2
+{
+	weight = 3.4;
+};
+class DZ_TK_Assault_Pack_EP1
+{
+	weight = 3;
+};
+class TK_Assault_Pack_DZE1
+{
+	weight = 3.2;
+};
+class TK_Assault_Pack_DZE2
+{
+	weight = 3.4;
+};
+class DZ_British_ACU
+{
+	weight = 4;
+};
+class British_ACU_DZE1
+{
+	weight = 4.2;
+};
+class British_ACU_DZE2
+{
+	weight = 4.4;
+};
+class DZ_GunBag_EP1
+{
+	weight = 4;
+};
+class GunBag_DZE1
+{
+	weight = 4.2;
+};
+class GunBag_DZE2
+{
+	weight = 4.4;
+};
+class NightPack_DZE1
+{
+	weight = 4.2;
+};
+class NightPack_DZE2
+{
+	weight = 4.4;
+};
+class SurvivorPack_DZE1
+{
+	weight = 5.2;
+};
+class SurvivorPack_DZE2
+{
+	weight = 5.4;
+};
+class  DZ_CivilBackpack_EP1
+{
+	weight = 5;
+};
+class CzechBackpack_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_DZE2
+{
+	weight = 5.4;
+};
+
+class CzechBackpack_OD_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_OD_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_DES_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_DES_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_3DES_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_3DES_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_WDL_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_WDL_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_MAR_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_MAR_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_DMAR_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_DMAR_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_UCP_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_UCP_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_6DES_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_6DES_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_TAK_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_TAK_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_NVG_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_NVG_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_BLK_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_BLK_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_DPM_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_DPM_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_FIN_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_FIN_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_MTC_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_MTC_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_NOR_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_NOR_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_WIN_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_WIN_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_ATC_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_ATC_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_MTL_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_MTL_DZE2
+{
+	weight = 5.4;
+};
+class CzechBackpack_FTN_DZE1
+{
+	weight = 5.2;
+};
+class CzechBackpack_FTN_DZE2
+{
+	weight = 5.4;
+};
+class DZ_Backpack_EP1
+{
+	weight = 6;
+};
+class  CoyoteBackpack_DZE1
+{
+	weight = 6.2;
+};
+class  CoyoteBackpack_DZE2
+{
+	weight = 6.4;
+};
+class CoyoteBackpackDes_DZE1
+{
+	weight = 6.2;
+};
+class CoyoteBackpackDes_DZE2
+{
+	weight = 6.4;
+};
+class CoyoteBackpackWdl_DZE1
+{
+	weight = 6.2;
+};
+class CoyoteBackpackWdl_DZE2
+{
+	weight = 6.4;
+};
+class WandererBackpack_DZE1
+{
+	weight = 6.2;
+};
+class WandererBackpack_DZE2
+{
+	weight = 6.4;
+};
+class LegendBackpack_DZE1
+{
+	weight = 6.2;
+};
+class LegendBackpack_DZE2
+{
+	weight = 6.4;
+};
+class DZ_LargeGunBag_EP1
+{
+	weight = 6;
+};
+class LargeGunBag_DZE1
+{
+	weight = 6.2;
+};
+class LargeGunBag_DZE2
+{
+	weight = 6.4;
+};
