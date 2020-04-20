bd0f0a5c7d1450f4cea5615c1ccf00d556bc7547
A Man <AirwavesMan@users.noreply.github.com>
2020-04-20 11:39:48 +0200

    Update AS50 ammo

--- SQF/dayz_code/Configs/CfgMagazines/Magazines/127x99.hpp
+++ SQF/dayz_code/Configs/CfgMagazines/Magazines/127x99.hpp
@@ -13,18 +13,6 @@ class 10Rnd_127x99_m107 : CA_Magazine
 };
 
 //AS50
-class 5Rnd_127x99_as50_CP : CA_Magazine
-{
-	displayName = $STR_DZ_MAG_5Rnd_127x99_AS50_NAME;
-	model = "\z\addons\dayz_epoch_w\magazine\dze_as50_mag.p3d";
-	
-	cartridgeName = "127x99";
-	
-	class ItemActions
-	{
-		COMBINE_MAG
-	};
-};
 class 5Rnd_127x99_as50 : CA_Magazine
 {
 	displayName = $STR_DZ_MAG_5Rnd_127x99_AS50_NAME;
@@ -37,4 +25,9 @@ class 5Rnd_127x99_as50 : CA_Magazine
 		COMBINE_MAG
 	};
 };
+class 5Rnd_127x99_as50_CP : 5Rnd_127x99_as50
+{	
+	ammo = "B_127x99_Ball_noTracer";
+};
+
 
