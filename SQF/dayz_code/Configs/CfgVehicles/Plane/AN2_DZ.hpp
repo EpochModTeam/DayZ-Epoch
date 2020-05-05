d98834aa59d965524056f41d78a74484d4cad9bc
A Man <AirwavesMan@users.noreply.github.com>
2020-05-05 22:05:58 +0200

    Add AN2 with Vickers and M134

--- SQF/dayz_code/Configs/CfgVehicles/Plane/AN2_DZ.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Plane/AN2_DZ.hpp
@@ -22,6 +22,18 @@ class AN2_DZ: An2_Base_EP1
 	fuelCapacity = 757;
 };
 
+class AN2_VICKER_DZ: AN2_DZ 
+{
+	displayname = $STR_VEH_NAME_AN2_VICKER;
+	weapons[] = {"TwinVickers"};
+	magazines[] = {"500Rnd_TwinVickers","500Rnd_TwinVickers"};
+};
+
+class AN2_VICKER_DZE: AN2_VICKER_DZ
+{
+	magazines[] = {};
+};
+
 class AN2_2_DZ : AN2_DZ
 {
 	displayname = $STR_VEH_NAME_AN2_WHITERED;	
@@ -32,6 +44,18 @@ class AN2_2_DZ : AN2_DZ
 		"ca\Air_E\An2\Data\an2_2_A_CO",
 		"ca\Air_E\An2\Data\an2_wings_A_CO"
 	};
+};
+
+class AN2_M134_DZ: AN2_2_DZ 
+{
+	displayname = $STR_VEH_NAME_AN2_M134;
+	weapons[] = {"TwinM134"};
+	magazines[] = {"2000Rnd_762x51_M134"};
+};
+
+class AN2_M134_DZE: AN2_M134_DZ
+{
+	magazines[] = {};
 };
 
 class An2_2_TK_CIV_EP1;
