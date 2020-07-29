4061f7a743c5782b3088f83e17867aac1f6473f5
worldwidesorrow <jsnmerrill@yahoo.com>
2020-07-29 15:35:54 -0500

    Update RscPlayerUI.hpp

--- SQF/dayz_code/Configs/RscDisplay/RscPlayerUI.hpp
+++ SQF/dayz_code/Configs/RscDisplay/RscPlayerUI.hpp
@@ -883,4 +883,34 @@ class RscTitles
 			class Icon100:Icons {idc = 200;};
 		};
 	};
+	class ZSC_Money_Display {
+		idd = -1;
+		fadeout = 0;
+		fadein = 0;
+		duration = 100000;
+		name= "ZSC_Money_Display";
+		onLoad = "uiNamespace setVariable ['ZSC_Money_Display', _this select 0]";
+		class controlsBackground {
+			class ZSC_Status
+			{
+				idc = 4900;
+				type = 13;
+				size = 0.040;
+				x = safezoneX + (safezoneW -0.35);
+				y = safezoneY + 0.43 * safezoneW;
+				w = 0.35; h = 0.20;
+				colorText[] = {1,1,1,1};
+				lineSpacing = 3;
+				colorBackground[] = {0,0,0,0};
+				text = "";
+				style = 0x02;
+				shadow = 2;
+				font = "Zeppelin32";
+				sizeEx = 0.023;
+				class Attributes {
+					align = "right";
+				};
+			};
+		};
+	};
 };
