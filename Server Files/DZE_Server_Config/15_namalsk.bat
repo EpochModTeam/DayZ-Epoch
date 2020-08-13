7ef03c869cb3299918d9fb076c18d005bcff4b99
worldwidesorrow <jsnmerrill@yahoo.com>
2020-08-12 21:47:20 -0500

    Fix incorrect config file.

--- Server Files/DZE_Server_Config/15_namalsk.bat
+++ Server Files/DZE_Server_Config/15_namalsk.bat
@@ -1,2 +1,2 @@
 @echo off
-start "arma2" /min /high arma2oaserver.exe -port=2302 "-config=C:\DZE_Server_Config\config.cfg" "-cfg=C:\DZE_Server_Config\basic.cfg" "-profiles=C:\DZE_Server_Config" -name=server "-mod=@namalsk;@DayZ_Epoch;@DayZ_Epoch_Server;"
+start "arma2" /min /high arma2oaserver.exe -port=2302 "-config=C:\DZE_Server_Config\15_namalsk.cfg" "-cfg=C:\DZE_Server_Config\basic.cfg" "-profiles=C:\DZE_Server_Config" -name=server "-mod=@namalsk;@DayZ_Epoch;@DayZ_Epoch_Server;"
