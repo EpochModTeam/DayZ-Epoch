cc101c948e9a012e6bede47832244a188658e822
AirwavesMan <AirwavesMan@users.noreply.github.com>
2020-08-18 14:18:03 +0200

    Update MG pick-ups

--- SQF/dayz_code/Configs/CfgVehicles/Car/Offroad_DSHKM_INS.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Car/Offroad_DSHKM_INS.hpp
@@ -5,7 +5,7 @@ class Offroad_DSHKM_Gue_DZ: Offroad_DSHKM_Gue {
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
-	displayName = $STR_VEH_NAME_PICKUP_DSHKM;
+	displayName = "$STR_VEH_NAME_PICKUP_DSHKM";
 	vehicleClass = "DayZ Epoch Vehicles";
 	class Turrets;
 	class MainTurret;
@@ -34,96 +34,81 @@ class Offroad_DSHKM_Gue_DZE: Offroad_DSHKM_Gue_DZ
 	};
 	
 	class Upgrades {
-		ItemORP[] = {"Offroad_DSHKM_Gue_DZE1",{},{{"ItemORP",1},{ "PartEngine",2}}};
+		ItemORP[] = {"Offroad_DSHKM_Gue_DZE1",{"ItemToolbox"},{},{{"ItemORP",1},{"PartEngine",1},{"PartWheel",4},{"ItemScrews",2}}};
 	};
 };
 
 // Performance 1
 class Offroad_DSHKM_Gue_DZE1: Offroad_DSHKM_Gue_DZE
 {
+	displayName = "$STR_VEH_NAME_PICKUP_DSHKM+";
 	original = "Offroad_DSHKM_Gue_DZE";
 	maxspeed = 170; // Offroad_DSHKM_base 150 | car 100
 	
 	class Upgrades {
-		ItemAVE[] = {"Offroad_DSHKM_Gue_DZE2",{},{{"ItemAVE",1},{"PartGeneric",2},{"ItemTent",1}}};
+		ItemAVE[] = {"Offroad_DSHKM_Gue_DZE2",{"ItemToolbox"},{},{{"ItemAVE",1},{"PartGeneric",6},{"ItemScrews",4}}};
 	};
 };
 // Armmor 2
 class Offroad_DSHKM_Gue_DZE2: Offroad_DSHKM_Gue_DZE1
 {
+	displayName = "$STR_VEH_NAME_PICKUP_DSHKM++";
 	armor = 55; // car 20
 	damageResistance = 0.02099;
-	class HitPoints: HitPoints
-	{
-		class HitLFWheel: HitLFWheel
-		{
+	class HitPoints: HitPoints {
+		class HitLFWheel: HitLFWheel {
 			armor = 0.3;
 		};
-		class HitLBWheel: HitLBWheel
-		{
+		class HitLBWheel: HitLBWheel {
 			armor = 0.3;
 		};
-		class HitRFWheel: HitRFWheel
-		{
+		class HitRFWheel: HitRFWheel {
 			armor = 0.3;
 		};
-		class HitRBWheel: HitRBWheel
-		{
+		class HitRBWheel: HitRBWheel {
 			armor = 0.3;
 		};
-		class HitFuel
-		{
-			armor = 0.28;
-			material = -1;
-			name = "palivo";
-			visual = "";
-			passThrough = 1;
+		class HitFuel: HitFuel {
+			armor = 0.5;
 		};
-		class HitEngine
-		{
+		class HitEngine: HitEngine {
 			armor = 1;
-			material = -1;
-			name = "motor";
-			visual = "";
-			passThrough = 1;
 		};
-		class HitGlass1: HitGlass1
-		{
-			armor = 0.1;
+		class HitGlass1: HitGlass1 {
+			armor = 0.3;
 		};
-		class HitGlass2: HitGlass2
-		{
-			armor = 0.1;
+		class HitGlass2: HitGlass2 {
+			armor = 0.3;
 		};
-		class HitGlass3: HitGlass3
-		{
-			armor = 0.1;
+		class HitGlass3: HitGlass3 {
+			armor = 0.3;
 		};
-		class HitGlass4: HitGlass4
-		{
-			armor = 0.1;
+		class HitGlass4: HitGlass4 {
+			armor = 0.3;
 		};
 	};
 	
 	class Upgrades {
-		ItemLRK[] = {"Offroad_DSHKM_Gue_DZE3",{},{{"ItemLRK",1},{"PartGeneric",2},{"ItemTent",1}}};
+		ItemLRK[] = {"Offroad_DSHKM_Gue_DZE3",{"ItemToolbox"},{},{{"ItemLRK",1},{"PartGeneric",2},{"ItemWoodCrateKit",1},{"ItemGunRackKit",1},{"ItemScrews",2}}};
 	};
 
 };
 // Cargo 3
 class Offroad_DSHKM_Gue_DZE3: Offroad_DSHKM_Gue_DZE2
 {
+	displayName = "$STR_VEH_NAME_PICKUP_DSHKM+++";
 	transportMaxWeapons = 20;  // car 10
 	transportMaxMagazines = 100; // car 50 
 	transportmaxbackpacks = 4; // car 2
 	
 	class Upgrades	{
-		ItemTNK[] = {"Offroad_DSHKM_Gue_DZE4",{},{{"ItemTNK",1},{"PartFueltank",2}}};
+		ItemTNK[] = {"Offroad_DSHKM_Gue_DZE4",{"ItemToolbox"},{},{{"ItemTNK",1},{"PartGeneric",2},{"PartFueltank",1},{"ItemJerrycan",2},{"ItemScrews",1}}};
 	};
 };
 
 // Fuel 4
 class Offroad_DSHKM_Gue_DZE4: Offroad_DSHKM_Gue_DZE3
 {
+	displayName = "$STR_VEH_NAME_PICKUP_DSHKM++++";
 	fuelCapacity = 210; // car 100
 };
--- SQF/dayz_code/Configs/CfgVehicles/Car/Pickup_PK_INS.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Car/Pickup_PK_INS.hpp
@@ -1,41 +1,7 @@
 class Pickup_PK_GUE;
 class Pickup_PK_GUE_DZ: Pickup_PK_GUE {
 	scope = 2;
-	displayName = $STR_VEH_NAME_PICKUP_GUE_PKT;
-	vehicleClass = "DayZ Epoch Vehicles";
-
-	crew = "";
-	typicalCargo[] = {};
-	class TransportMagazines {};
-	class TransportWeapons {};
-
-	terrainCoef = 2.5;
-
-	class Turrets; // External class reference
-	class MainTurret; // External class reference
-	supplyRadius = 1.3;
-};
-class Pickup_PK_TK_GUE_EP1;
-class Pickup_PK_TK_GUE_EP1_DZ: Pickup_PK_TK_GUE_EP1 {
-	scope = 2;
-	displayName = $STR_VEH_NAME_PICKUP_TK_PKT;
-	vehicleClass = "DayZ Epoch Vehicles";
-
-	crew = "";
-	typicalCargo[] = {};
-	class TransportMagazines {};
-	class TransportWeapons {};
-
-	terrainCoef = 2.5;
-
-	class Turrets; // External class reference
-	class MainTurret; // External class reference
-	supplyRadius = 1.3;
-};
-class Pickup_PK_INS;
-class Pickup_PK_INS_DZ: Pickup_PK_INS {
-	scope = 2;
-	displayName = $STR_VEH_NAME_PICKUP_INS_PKT;
+	displayName = "$STR_VEH_NAME_PICKUP_GUE_PKT";
 	vehicleClass = "DayZ Epoch Vehicles";
 
 	crew = "";
@@ -50,7 +16,6 @@ class Pickup_PK_INS_DZ: Pickup_PK_INS {
 	supplyRadius = 1.3;
 };
 
-// Ammo Less
 class Pickup_PK_GUE_DZE: Pickup_PK_GUE_DZ {
 	class Turrets: Turrets {
 		class MainTurret : MainTurret {
@@ -70,9 +35,97 @@ class Pickup_PK_GUE_DZE: Pickup_PK_GUE_DZ {
 	class HitGlass4; // External class reference
 
 	class Upgrades {
-		ItemORP[] = {"Pickup_PK_GUE_DZE1",{},{{"ItemORP",1},{"PartEngine",2}}};
+		ItemORP[] = {"Pickup_PK_GUE_DZE1",{"ItemToolbox"},{},{{"ItemORP",1},{"PartEngine",1},{"PartWheel",4},{"ItemScrews",2}}};
 	};
 };
+
+class Pickup_PK_GUE_DZE1: Pickup_PK_GUE_DZE {
+	displayName = "$STR_VEH_NAME_PICKUP_GUE_PKT+";
+	original = "Pickup_PK_GUE_DZE";
+	maxspeed = 150; // max engine limit 125-130
+	terrainCoef = 1.8;
+
+	class Upgrades {
+		ItemAVE[] = {"Pickup_PK_GUE_DZE2",{"ItemToolbox"},{},{{"ItemAVE",1},{"PartGeneric",6},{"ItemScrews",4}}};
+	};
+};
+
+class Pickup_PK_GUE_DZE2: Pickup_PK_GUE_DZE1 {
+	displayName = "$STR_VEH_NAME_PICKUP_GUE_PKT++";
+	armor = 55; // car 20
+	damageResistance = 0.02099;
+	class HitPoints: HitPoints {
+		class HitLFWheel: HitLFWheel {
+			armor = 0.3;
+		};
+		class HitLBWheel: HitLBWheel {
+			armor = 0.3;
+		};
+		class HitRFWheel: HitRFWheel {
+			armor = 0.3;
+		};
+		class HitRBWheel: HitRBWheel {
+			armor = 0.3;
+		};
+		class HitFuel: HitFuel {
+			armor = 0.5;
+		};
+		class HitEngine: HitEngine {
+			armor = 1;
+		};
+		class HitGlass1: HitGlass1 {
+			armor = 0.3;
+		};
+		class HitGlass2: HitGlass2 {
+			armor = 0.3;
+		};
+		class HitGlass3: HitGlass3 {
+			armor = 0.3;
+		};
+		class HitGlass4: HitGlass4 {
+			armor = 0.3;
+		};
+	};
+
+	class Upgrades {
+		ItemLRK[] = {"Pickup_PK_GUE_DZE3",{"ItemToolbox"},{},{{"ItemLRK",1},{"PartGeneric",2},{"ItemWoodCrateKit",1},{"ItemGunRackKit",1},{"ItemScrews",2}}};
+	};
+};
+
+class Pickup_PK_GUE_DZE3: Pickup_PK_GUE_DZE2 {
+	displayName = "$STR_VEH_NAME_PICKUP_GUE_PKT+++";
+	transportMaxWeapons = 20;  // car 10
+	transportMaxMagazines = 100; // car 50
+    transportmaxbackpacks = 4; // car 2
+
+	class Upgrades {
+		ItemTNK[] = {"Pickup_PK_GUE_DZE4",{"ItemToolbox"},{},{{"ItemTNK",1},{"PartGeneric",2},{"PartFueltank",1},{"ItemJerrycan",2},{"ItemScrews",1}}};
+	};
+};
+
+class Pickup_PK_GUE_DZE4: Pickup_PK_GUE_DZE3 {
+	displayName = "$STR_VEH_NAME_PICKUP_GUE_PKT++++";
+	fuelCapacity = 210; // car 100
+};
+
+class Pickup_PK_TK_GUE_EP1;
+class Pickup_PK_TK_GUE_EP1_DZ: Pickup_PK_TK_GUE_EP1 {
+	scope = 2;
+	displayName = "$STR_VEH_NAME_PICKUP_TK_PKT";
+	vehicleClass = "DayZ Epoch Vehicles";
+
+	crew = "";
+	typicalCargo[] = {};
+	class TransportMagazines {};
+	class TransportWeapons {};
+
+	terrainCoef = 2.5;
+
+	class Turrets; // External class reference
+	class MainTurret; // External class reference
+	supplyRadius = 1.3;
+};
+
 class Pickup_PK_TK_GUE_EP1_DZE: Pickup_PK_TK_GUE_EP1_DZ {
 	class Turrets: Turrets {
 		class MainTurret : MainTurret {
@@ -92,9 +145,97 @@ class Pickup_PK_TK_GUE_EP1_DZE: Pickup_PK_TK_GUE_EP1_DZ {
 	class HitGlass4; // External class reference
 
 	class Upgrades {
-		ItemORP[] = {"Pickup_PK_TK_GUE_EP1_DZE1",{},{{"ItemORP",1},{"PartEngine",2}}};
+		ItemORP[] = {"Pickup_PK_TK_GUE_EP1_DZE1",{"ItemToolbox"},{},{{"ItemORP",1},{"PartEngine",1},{"PartWheel",4},{"ItemScrews",2}}};
 	};
 };
+
+class Pickup_PK_TK_GUE_EP1_DZE1: Pickup_PK_TK_GUE_EP1_DZE {
+	displayName = "$STR_VEH_NAME_PICKUP_TK_PKT+";
+	original = "Pickup_PK_TK_GUE_EP1_DZE";
+	maxspeed = 150; // max engine limit 125-130
+	terrainCoef = 1.8;
+
+	class Upgrades {
+		ItemAVE[] = {"Pickup_PK_TK_GUE_EP1_DZE2",{"ItemToolbox"},{},{{"ItemAVE",1},{"PartGeneric",6},{"ItemScrews",4}}};
+	};
+};
+
+class Pickup_PK_TK_GUE_EP1_DZE2: Pickup_PK_TK_GUE_EP1_DZE1 {
+	displayName = "$STR_VEH_NAME_PICKUP_TK_PKT++";
+	armor = 55; // car 20
+	damageResistance = 0.02099;
+	class HitPoints: HitPoints {
+		class HitLFWheel: HitLFWheel {
+			armor = 0.3;
+		};
+		class HitLBWheel: HitLBWheel {
+			armor = 0.3;
+		};
+		class HitRFWheel: HitRFWheel {
+			armor = 0.3;
+		};
+		class HitRBWheel: HitRBWheel {
+			armor = 0.3;
+		};
+		class HitFuel: HitFuel {
+			armor = 0.5;
+		};
+		class HitEngine: HitEngine {
+			armor = 1;
+		};
+		class HitGlass1: HitGlass1 {
+			armor = 0.3;
+		};
+		class HitGlass2: HitGlass2 {
+			armor = 0.3;
+		};
+		class HitGlass3: HitGlass3 {
+			armor = 0.3;
+		};
+		class HitGlass4: HitGlass4 {
+			armor = 0.3;
+		};
+	};
+
+	class Upgrades {
+		ItemLRK[] = {"Pickup_PK_TK_GUE_EP1_DZE3",{"ItemToolbox"},{},{{"ItemLRK",1},{"PartGeneric",2},{"ItemWoodCrateKit",1},{"ItemGunRackKit",1},{"ItemScrews",2}}};
+	};
+};
+
+class Pickup_PK_TK_GUE_EP1_DZE3: Pickup_PK_TK_GUE_EP1_DZE2 {
+	displayName = "$STR_VEH_NAME_PICKUP_TK_PKT+++";
+	transportMaxWeapons = 20;  // car 10
+	transportMaxMagazines = 100; // car 50
+    transportmaxbackpacks = 4; // car 2
+
+	class Upgrades {
+		ItemTNK[] = {"Pickup_PK_TK_GUE_EP1_DZE4",{"ItemToolbox"},{},{{"ItemTNK",1},{"PartGeneric",2},{"PartFueltank",1},{"ItemJerrycan",2},{"ItemScrews",1}}};
+	};
+};
+
+class Pickup_PK_TK_GUE_EP1_DZE4: Pickup_PK_TK_GUE_EP1_DZE3 {
+	displayName = "$STR_VEH_NAME_PICKUP_TK_PKT++++";
+	fuelCapacity = 210; // car 100
+};
+
+class Pickup_PK_INS;
+class Pickup_PK_INS_DZ: Pickup_PK_INS {
+	scope = 2;
+	displayName = "$STR_VEH_NAME_PICKUP_INS_PKT";
+	vehicleClass = "DayZ Epoch Vehicles";
+
+	crew = "";
+	typicalCargo[] = {};
+	class TransportMagazines {};
+	class TransportWeapons {};
+
+	terrainCoef = 2.5;
+
+	class Turrets; // External class reference
+	class MainTurret; // External class reference
+	supplyRadius = 1.3;
+};
+
 class Pickup_PK_INS_DZE: Pickup_PK_INS_DZ {
 	class Turrets: Turrets {
 		class MainTurret : MainTurret {
@@ -114,137 +255,23 @@ class Pickup_PK_INS_DZE: Pickup_PK_INS_DZ {
 	class HitGlass4; // External class reference
 
 	class Upgrades {
-		ItemORP[] = {"Pickup_PK_INS_DZE1",{},{{"ItemORP",1},{"PartEngine",2}}};
+		ItemORP[] = {"Pickup_PK_INS_DZE1",{"ItemToolbox"},{},{{"ItemORP",1},{"PartEngine",1},{"PartWheel",4},{"ItemScrews",2}}};
 	};
 };
 
-// Performance 1
-class Pickup_PK_GUE_DZE1: Pickup_PK_GUE_DZE {
-	original = "Pickup_PK_GUE_DZE";
-	maxspeed = 150; // max engine limit 125-130
-	terrainCoef = 1.8;
-
-	class Upgrades {
-		ItemAVE[] = {"Pickup_PK_GUE_DZE2",{},{{"ItemAVE",1},{"PartGeneric",4},{"ItemTankTrap",2}}};
-	};
-};
-class Pickup_PK_TK_GUE_EP1_DZE1: Pickup_PK_TK_GUE_EP1_DZE {
-	original = "Pickup_PK_TK_GUE_EP1_DZE";
-	maxspeed = 150; // max engine limit 125-130
-	terrainCoef = 1.8;
-
-	class Upgrades {
-		ItemAVE[] = {"Pickup_PK_TK_GUE_EP1_DZE2",{},{{"ItemAVE",1},{"PartGeneric",4},{"ItemTankTrap",2}}};
-	};
-};
 class Pickup_PK_INS_DZE1: Pickup_PK_INS_DZE {
+	displayName = "$STR_VEH_NAME_PICKUP_INS_PKT+";
 	original = "Pickup_PK_INS_DZE";
 	maxspeed = 150; // max engine limit 125-130
 	terrainCoef = 1.8;
 
 	class Upgrades {
-		ItemAVE[] = {"Pickup_PK_INS_DZE2",{},{{"ItemAVE",1},{"PartGeneric",4},{"ItemTankTrap",2}}};
+		ItemAVE[] = {"Pickup_PK_INS_DZE2",{"ItemToolbox"},{},{{"ItemAVE",1},{"PartGeneric",6},{"ItemScrews",4}}};
 	};
 };
 
-// Armor 2
-class Pickup_PK_GUE_DZE2: Pickup_PK_GUE_DZE1 {
-	armor = 55; // car 20
-	damageResistance = 0.02099;
-	class HitPoints: HitPoints {
-		class HitLFWheel: HitLFWheel {
-			armor = 0.3;
-		};
-		class HitLBWheel: HitLBWheel {
-			armor = 0.3;
-		};
-		class HitRFWheel: HitRFWheel {
-			armor = 0.3;
-		};
-		class HitRBWheel: HitRBWheel {
-			armor = 0.3;
-		};
-		class HitFuel {
-			armor = 0.28;
-			material = -1;
-			name = "palivo";
-			visual = "";
-			passThrough = 1;
-		};
-		class HitEngine {
-			armor = 1;
-			material = -1;
-			name = "motor";
-			visual = "";
-			passThrough = 1;
-		};
-		class HitGlass1: HitGlass1 {
-			armor = 0.1;
-		};
-		class HitGlass2: HitGlass2 {
-			armor = 0.1;
-		};
-		class HitGlass3: HitGlass3 {
-			armor = 0.1;
-		};
-		class HitGlass4: HitGlass4 {
-			armor = 0.1;
-		};
-	};
-
-	class Upgrades {
-		ItemLRK[] = {"Pickup_PK_GUE_DZE3",{},{{"ItemLRK",1},{"PartGeneric",2},{"ItemTent",1}}};
-	};
-};
-class Pickup_PK_TK_GUE_EP1_DZE2: Pickup_PK_TK_GUE_EP1_DZE1 {
-	armor = 55; // car 20
-	damageResistance = 0.02099;
-	class HitPoints: HitPoints {
-		class HitLFWheel: HitLFWheel {
-			armor = 0.3;
-		};
-		class HitLBWheel: HitLBWheel {
-			armor = 0.3;
-		};
-		class HitRFWheel: HitRFWheel {
-			armor = 0.3;
-		};
-		class HitRBWheel: HitRBWheel {
-			armor = 0.3;
-		};
-		class HitFuel {
-			armor = 0.28;
-			material = -1;
-			name = "palivo";
-			visual = "";
-			passThrough = 1;
-		};
-		class HitEngine {
-			armor = 1;
-			material = -1;
-			name = "motor";
-			visual = "";
-			passThrough = 1;
-		};
-		class HitGlass1: HitGlass1 {
-			armor = 0.1;
-		};
-		class HitGlass2: HitGlass2 {
-			armor = 0.1;
-		};
-		class HitGlass3: HitGlass3 {
-			armor = 0.1;
-		};
-		class HitGlass4: HitGlass4 {
-			armor = 0.1;
-		};
-	};
-
-	class Upgrades {
-		ItemLRK[] = {"Pickup_PK_TK_GUE_EP1_DZE3",{},{{"ItemLRK",1},{"PartGeneric",2},{"ItemTent",1}}};
-	};
-};
 class Pickup_PK_INS_DZE2: Pickup_PK_INS_DZE1 {
+	displayName = "$STR_VEH_NAME_PICKUP_INS_PKT++";
 	armor = 55; // car 20
 	damageResistance = 0.02099;
 	class HitPoints: HitPoints {
@@ -260,75 +287,43 @@ class Pickup_PK_INS_DZE2: Pickup_PK_INS_DZE1 {
 		class HitRBWheel: HitRBWheel {
 			armor = 0.3;
 		};
-		class HitFuel {
-			armor = 0.28;
-			material = -1;
-			name = "palivo";
-			visual = "";
-			passThrough = 1;
+		class HitFuel: HitFuel {
+			armor = 0.5;
 		};
-		class HitEngine {
+		class HitEngine: HitEngine {
 			armor = 1;
-			material = -1;
-			name = "motor";
-			visual = "";
-			passThrough = 1;
 		};
 		class HitGlass1: HitGlass1 {
-			armor = 0.1;
+			armor = 0.3;
 		};
 		class HitGlass2: HitGlass2 {
-			armor = 0.1;
+			armor = 0.3;
 		};
 		class HitGlass3: HitGlass3 {
-			armor = 0.1;
+			armor = 0.3;
 		};
 		class HitGlass4: HitGlass4 {
-			armor = 0.1;
+			armor = 0.3;
 		};
 	};
 
 	class Upgrades {
-		ItemLRK[] = {"Pickup_PK_INS_DZE3",{},{{"ItemLRK",1},{"PartGeneric",2},{"ItemTent",1}}};
+		ItemLRK[] = {"Pickup_PK_INS_DZE3",{"ItemToolbox"},{},{{"ItemLRK",1},{"PartGeneric",2},{"ItemWoodCrateKit",1},{"ItemGunRackKit",1},{"ItemScrews",2}}};
 	};
 };
 
-// Cargo 3
-class Pickup_PK_GUE_DZE3: Pickup_PK_GUE_DZE2 {
-	transportMaxWeapons = 20;  // car 10
-	transportMaxMagazines = 100; // car 50
-    transportmaxbackpacks = 4; // car 2
-
-	class Upgrades {
-		ItemTNK[] = {"Pickup_PK_GUE_DZE4",{},{{"ItemTNK",1},{"PartFueltank",2}}};
-	};
-};
-class Pickup_PK_TK_GUE_EP1_DZE3: Pickup_PK_TK_GUE_EP1_DZE2 {
-	transportMaxWeapons = 20;  // car 10
-	transportMaxMagazines = 100; // car 50
-    transportmaxbackpacks = 4; // car 2
-
-	class Upgrades {
-		ItemTNK[] = {"Pickup_PK_TK_GUE_EP1_DZE4",{},{{"ItemTNK",1},{"PartFueltank",2}}};
-	};
-};
 class Pickup_PK_INS_DZE3: Pickup_PK_INS_DZE2 {
+	displayName = "$STR_VEH_NAME_PICKUP_INS_PKT+++";
 	transportMaxWeapons = 20;  // car 10
 	transportMaxMagazines = 100; // car 50
     transportmaxbackpacks = 4; // car 2
 
 	class Upgrades {
-		ItemTNK[] = {"Pickup_PK_INS_DZE4",{},{{"ItemTNK",1},{"PartFueltank",2}}};
+		ItemTNK[] = {"Pickup_PK_INS_DZE4",{"ItemToolbox"},{},{{"ItemTNK",1},{"PartGeneric",2},{"PartFueltank",1},{"ItemJerrycan",2},{"ItemScrews",1}}};
 	};
 };
 
-// Fuel 4
-class Pickup_PK_GUE_DZE4: Pickup_PK_GUE_DZE3 {
-	fuelCapacity = 210; // car 100
-};
-class Pickup_PK_TK_GUE_EP1_DZE4: Pickup_PK_TK_GUE_EP1_DZE3 {
-	fuelCapacity = 210; // car 100
-};
 class Pickup_PK_INS_DZE4: Pickup_PK_INS_DZE3 {
+	displayName = "$STR_VEH_NAME_PICKUP_INS_PKT++++";
 	fuelCapacity = 210; // car 100
 };
