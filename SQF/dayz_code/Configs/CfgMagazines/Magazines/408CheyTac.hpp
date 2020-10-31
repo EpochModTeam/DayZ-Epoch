785166384291d18ff5a4c3f56ddd3267e6aecd19
AirwavesMan <AirwavesMan@users.noreply.github.com>
2020-10-31 10:06:00 +0100

    Update 408CheyTac.hpp

--- SQF/dayz_code/Configs/CfgMagazines/Magazines/408CheyTac.hpp
+++ SQF/dayz_code/Configs/CfgMagazines/Magazines/408CheyTac.hpp
@@ -2,7 +2,7 @@ class 5Rnd_408_CheyTac : CA_Magazine {
 	scope = 2;
 	displayName = $STR_DZ_MAG_5RND_408_CHEYTAC_NAME;
 	descriptionShort = $STR_DZ_MAG_5RND_CHEYTAC_DESC;
-	picture	="\z\addons\dayz_epoch_w\m200\data\m_cheytac_CA.paa";
+	picture	="\dayz_epoch_c\icons\magazines\m_m200.paa";
 	model ="\z\addons\dayz_epoch_w\m200\h4_cheytac_mag.p3d";
 	ammo = "B_408_CheyTac_Ball";	
 	count = 5;
