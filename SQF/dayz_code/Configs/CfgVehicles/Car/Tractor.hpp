d0f647c0b55b36dec60cb67930f0f4d16162766c
AirwavesMan <AirwavesMan@users.noreply.github.com>
2020-11-15 09:58:34 +0100

    Add armored tractor
    Model made by @Helion4

--- SQF/dayz_code/Configs/CfgVehicles/Car/Tractor.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Car/Tractor.hpp
@@ -32,6 +32,9 @@ class TractorOld_DZE: tractorOld {
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
+	class HitPoints;
+	class HitFuel;
+	class HitEngine;
 	class Reflectors
 	{
 		class Left
@@ -45,6 +48,53 @@ class TractorOld_DZE: tractorOld {
 			size = 1;
 			brightness = 0.5;
 			angle = 120;
+		};
+	};
+};
+
+class Tractor_Armored_DZE: TractorOld_DZE
+{
+	displayname = $STR_VEH_NAME_TRACTOR_ARMORED;	
+	model = "\z\addons\dayz_epoch_v\vehicles\tractor\dze_tractor";
+	armor = 120;
+	maxSpeed = 75;
+	terrainCoef = 0.5;
+	transportMaxWeapons = 20;
+	transportMaxMagazines = 80;
+    transportmaxbackpacks = 10;	
+	
+	class HitPoints: HitPoints {
+		class HitFuel: HitFuel {
+			armor = 1;
+		};
+		class HitEngine: HitEngine {
+			armor = 2;
+		};
+	};	
+	
+	hiddenselections[] = {"camo1","camo2"};
+	hiddenselectionstextures[]= 
+	{
+		"\z\addons\dayz_epoch_v\vehicles\tractor\data\tractor_2_rusty_co.paa",
+		"\z\addons\dayz_epoch_v\vehicles\tractor\data\rustymetal_01_co.paa"			
+	};
+	class damage
+	{
+		tex[]={};
+		mat[]=
+		{
+			"z\addons\dayz_epoch_v\vehicles\tractor\data\rustymetal_01.rvmat",
+			"z\addons\dayz_epoch_v\vehicles\tractor\data\rustymetal_01_damage.rvmat",
+			"z\addons\dayz_epoch_v\vehicles\tractor\data\rustymetal_01_destruct.rvmat",
+
+			"ca\wheeled\data\traktor_2.rvmat",
+			"ca\wheeled\data\traktor_2.rvmat",
+			"ca\wheeled\data\traktor_2_destruct.rvmat",
+			
+			"ca\wheeled\data\traktor_2_skla.rvmat",
+			"ca\wheeled\data\traktor_2_skla.rvmat",
+			"ca\wheeled\data\traktor_2_skla_destruct.rvmat"
+			
 		};
 	};
 };
