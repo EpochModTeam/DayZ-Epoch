c5d83056a0c9a682a9c5718e53434885a52ad3ce
AirwavesMan <AirwavesMan@users.noreply.github.com>
2020-11-22 14:39:07 +0100

    Add seperate file for building previews

---
+++ SQF/dayz_code/Configs/CfgVehicles/Buildables/ModularPreviews.hpp
@@ -0,0 +1,132 @@
+// Ghost previews
+class DZE_Ghost_Previews: NonStrategic {
+	scope = 0;
+	destrType = "DestructNo";
+	icon = "\ca\data\data\Unknown_object.paa";
+	mapSize = 2;
+	armor = 1000;
+	vehicleClass = "DayZ Epoch Build Previews";
+	offset[] = {0,1.5,0};
+};
+
+class CinderWallSmallDoorway_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\Steel_door_frame_ghost.p3d"; // todo change this
+	displayName = "Cinder Doorway Preview";	
+};
+class CinderWallDoorway_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\steel_garage_frame_ghost.p3d";
+	displayName = "Cinder Garage Doorway Preview";
+};
+class MetalFloor_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\metal_floor_ghost.p3d";
+	displayName = "Metal Floor Preview";
+};
+class WoodRamp_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;	
+	model = "\z\addons\dayz_epoch\models\Wood_Ramp_ghost.p3d";
+	displayName = "Wood Ramp Preview";
+};
+class CinderWallHalf_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\cinder_wall_half_ghost.p3d";
+	displayName = "Cinder Block Wall 1/2 Preview";
+};
+class CinderWall_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\cinder_wall_full_ghost.p3d";
+	displayName = "Cinder Block Wall Preview";
+};
+
+class WoodFloorQuarter_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\quarter_floor_ghost.p3d";
+	displayName = "Floor 1/4 Preview";
+};
+class WoodFloorHalf_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\half_floor_ghost.p3d";
+	displayName = "Floor 1/2 Preview";
+};
+class WoodSmallWall_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\small_wall_ghost.p3d";
+	displayName = "Small Wall Preview";
+};
+class WoodSmallWallThird_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\third_wall_ghost.p3d";
+	displayName = "Small Wall Preview 1/3";
+};
+class WoodSmallWallWin_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\small_wall_win_ghost.p3d";
+	displayName = "Small Wall Window Preview";
+};
+class WoodFloor_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\Wood_Floor_ghost.p3d";
+	displayName = "Wood Floor Preview";
+};
+class WoodSmallWallDoor_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\small_wall_door_ghost.p3d";
+	displayName = "Small Wall Door Preview";
+};
+class WoodLargeWall_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\large_wall_ghost.p3d";
+	displayName = "Large Wall Preview";
+};
+class WoodLargeWallDoor_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\large_wall_door_ghost.p3d";
+	displayName = "Large Wall Door Preview";
+};
+class WoodLargeWallWin_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\large_wall_win_ghost.p3d";
+	displayName = "Large Wall Window Preview";
+};
+class WoodStairs_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\wood_stairs_ghost.p3d";
+	displayName = "Wood Stairs Preview";
+};
+class WoodStairsSans_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\wood_stairs_sans_ghost.p3d";
+	displayName = "Wood Stairs Sans Preview";
+};
+class WoodLadder_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\wood_ladder_ghost.p3d";
+	displayName = "Wood Stairs Sans Preview";
+};
+class WoodDoor_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\small_wall_door_anim_ghost.p3d";
+	displayName = "Door Preview";
+};
+class LargeWoodDoor_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\large_wall_door_anim_ghost.p3d";
+	displayName = "Large Door Preview";
+};
+class GarageWoodDoor_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch\models\Garage_door_anim_ghost.p3d";
+	displayName = "Garage Door Preview";
+};
+class WoodGate_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch_v\base_building\wood\high_wood_garage\high_wood_garage_ghost.p3d";
+	displayName = "Wood Gate Preview";
+};
+class CinderGate_Preview_DZ: DZE_Ghost_Previews {
+	scope = 2;
+	model = "\z\addons\dayz_epoch_v\base_building\cinder\cinder_high_garage\high_steel_garage_frame_ghost.p3d";
+	displayName = "Cinder Gate Preview";
+};
