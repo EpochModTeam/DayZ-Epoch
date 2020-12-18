81dfc6975f6ca14ef2e8bf377a192d6301665cfd
AirwavesMan <AirwavesMan@users.noreply.github.com>
2020-12-18 15:38:04 +0100

    Add mag models for netting attachments for weapons
    Models made by @Helion4

--- SQF/dayz_code/Configs/CfgMagazines/Attachments/Ghillie.hpp
+++ SQF/dayz_code/Configs/CfgMagazines/Attachments/Ghillie.hpp
@@ -3,7 +3,7 @@ class Attachment_Ghillie : CA_Magazine
 	scope = 2;
 	count = 1;
 	type = 256;	
-	model = "ca\weapons\mag_univ.p3d";
+	model = "\z\addons\dayz_epoch_w\magazine\dze_camoroll_woodland.p3d";
 	picture = "\z\addons\dayz_communityweapons\attachments\ghillie\data\m_ghillie_ca.paa";	
 	displayName = $STR_DZ_ATT_GHIL_NAME;
 	descriptionShort = $STR_DZ_ATT_GHIL_DESC;
@@ -15,26 +15,23 @@ class Attachment_Ghillie : CA_Magazine
 			text = $STR_DZ_ATT_ACT_TO_PRIMARY;
 			script = "; [_id,1] call player_attachAttachment";
 		};
-		
-		/*class AttachToSecondary
-		{
-			text = $STR_DZ_ATT_ACT_TO_SECONDARY;
-			script = "; [_id,0] call player_attachAttachment";
-		};*/
 	};
 };
 class Attachment_Ghillie_Desert : Attachment_Ghillie
 {
+	model = "\z\addons\dayz_epoch_w\magazine\dze_camoroll_desert.p3d";
 	picture = "\dayz_epoch_c\icons\attachments\attachment_netting_desert.paa";
 	displayName = $STR_DZE_ATTACHMENT_GHIL_DES_NAME;
 };
 class Attachment_Ghillie_Snow : Attachment_Ghillie
 {
+	model = "\z\addons\dayz_epoch_w\magazine\dze_camoroll_snow.p3d";
 	picture = "\dayz_epoch_c\icons\attachments\attachment_netting_snow.paa";
 	displayName = $STR_DZE_ATTACHMENT_GHIL_SNOW_NAME;
 };
 class Attachment_Ghillie_Urban : Attachment_Ghillie
 {
+	model = "\z\addons\dayz_epoch_w\magazine\dze_camoroll_urban.p3d";
 	picture = "\dayz_epoch_c\icons\attachments\attachment_netting_urban.paa";
 	displayName = $STR_DZE_ATTACHMENT_GHIL_URB_NAME;
 };
