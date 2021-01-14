2a5489963bf9d131998870df744bc39a3cd16b23
AirwavesMan <AirwavesMan@users.noreply.github.com>
2021-01-14 17:53:48 +0100

    Add Hero and Bandit zombies, update bodyguard zombie

---
+++ SQF/dayz_code/Configs/CfgLoot/Groups/Zombies/Bandit.hpp
@@ -0,0 +1,14 @@
+ZombieBandit[] =
+{
+	{Loot_MAGAZINE,		5,		FoodMRE},
+	{Loot_MAGAZINE,		1,	 	ItemHotwireKit},
+	{Loot_GROUP,		3,		MedicalLow},
+	{Loot_GROUP,		10,		AmmoMilitaryZed},
+	{Loot_GROUP,		2,		Consumable}
+};
+
+ZombieBanditViral[] =
+{
+	{Loot_GROUP,		10,		ZombieMilitary},
+	{Loot_MAGAZINE,		1,		ItemAntibiotic1}
+};
---
+++ SQF/dayz_code/Configs/CfgLoot/Groups/Zombies/Hero.hpp
@@ -0,0 +1,14 @@
+ZombieHero[] =
+{
+	{Loot_MAGAZINE,		5,		FoodMRE},
+	{Loot_MAGAZINE,		1,	 	ItemHotwireKit},
+	{Loot_GROUP,		3,		MedicalLow},
+	{Loot_GROUP,		10,		AmmoMilitaryZed},
+	{Loot_GROUP,		2,		Consumable}
+};
+
+ZombieHeroViral[] =
+{
+	{Loot_GROUP,		10,		ZombieMilitary},
+	{Loot_MAGAZINE,		1,		ItemAntibiotic1}
+};
