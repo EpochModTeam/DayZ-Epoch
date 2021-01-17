193aa9bab0bf413b7a0cf6909f24d099a21a59ae
AirwavesMan <AirwavesMan@users.noreply.github.com>
2021-01-17 15:49:23 +0100

    Update Bridges.hpp

--- SQF/dayz_code/Configs/CfgVehicles/Buildables/Bridges.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Buildables/Bridges.hpp
@@ -6,60 +6,37 @@ class Metal_Drawbridge_DZ: CinderWallDoor_DZ_Base
 	GhostPreview = "Drawbridge_Preview_DZ";
 	offset[] = {0,2.5,0};
 	upgradeBuilding[] = {"Metal_DrawbridgeLocked_DZ",{},{{"ItemComboLock",1}}};
-	class AnimationSources {
-		class Open_inner {
+	class AnimationSources 
+	{
+		class Open_inner 
+		{
 			source = "user";
 			animPeriod = 4;
-			initPhase = 0; 
+			initPhase = 1; 
 		};
-		class Open_outer {
+		class Open_outer 
+		{
 			source = "user";
 			animPeriod = 4;
-			initPhase = 0; 
-		};			
+			initPhase = 1; 
+		};
 	};
 	class UserActions
 	{			
-		class Open_inner
+		class Raise_Bridge
 		{
-			displayName = $STR_EPOCH_ACTION_LOWER_BRIDGE;
+			displayName = $STR_EPOCH_ACTION_RAISE_BRIDGE;
 			onlyforplayer = true;
 			position = "action";
-			radius = 3;
+			radius = 7;
 			condition = "this animationPhase ""Open_inner"" < 0.5";
 			statement = "this animate [""Open_inner"", 1];this animate [""Open_outer"", 1];";
 		};
-		class Close_Door : Open_inner
+		class Lower_Bridge : Raise_Bridge
 		{
-			displayName = $STR_EPOCH_ACTION_RAISE_BRIDGE;
+			displayName = $STR_EPOCH_ACTION_LOWER_BRIDGE;
 			condition = "this animationPhase ""Open_inner"" >= 0.5";
 			statement = "this animate [""Open_inner"", 0];this animate [""Open_outer"", 0];";
-		};
-	};
-	class DestructionEffects {
-		class Sound {
-			simulation = "sound";
-			type = "DestrHouse";
-			position = "destructionEffect1";
-			intensity = 1;
-			interval = 1;
-			lifeTime = 0.05;
-		};
-		class DestroyPhase1 {
-			simulation = "destroy";
-			type = "DelayedDestruction";
-			lifeTime = 2.5;
-			position = "";
-			intensity = 1;
-			interval = 1;
-		};
-		class DamageAround1 {
-			simulation = "damageAround";
-			type = "DamageAroundHouse";
-			position = "";
-			intensity = 0.1;
-			interval = 1;
-			lifeTime = 1;
 		};
 	};
 };
@@ -77,13 +54,13 @@ class Metal_DrawbridgeLocked_DZ: CinderWallDoorLocked_DZ_Base
 		{
 			source = "user";
 			animPeriod = 4;
-			initPhase = 0; 
+			initPhase = 1; 
 		};
 		class Open_outer 
 		{
 			source = "user";
 			animPeriod = 4;
-			initPhase = 0; 
+			initPhase = 1; 
 		};
 		class Open_latch 
 		{
@@ -94,64 +71,38 @@ class Metal_DrawbridgeLocked_DZ: CinderWallDoorLocked_DZ_Base
 	};
 	class UserActions
 	{			
-		class Open_inner
+		class Raise_Bridge
 		{
-			displayName = $STR_EPOCH_ACTION_LOWER_BRIDGE;
+			displayName = $STR_EPOCH_ACTION_RAISE_BRIDGE;
 			onlyforplayer = true;
 			position = "action";
-			radius = 3;
+			radius = 7;
 			condition = "(this animationPhase ""Open_inner"" == 0) && (this animationPhase ""Open_latch"" == 1)";
 			statement = "this animate [""Open_inner"", 1];this animate [""Open_outer"", 1];";
 		};
-		class Close_Door : Open_inner
+		class Lower_Bridge : Raise_Bridge
 		{
-			displayName = $STR_EPOCH_ACTION_RAISE_BRIDGE;
+			displayName = $STR_EPOCH_ACTION_LOWER_BRIDGE;
 			condition = "(this animationPhase ""Open_inner"" == 1) && (this animationPhase ""Open_latch"" == 1)";
 			statement = "this animate [""Open_inner"", 0];this animate [""Open_outer"", 0];";
 		};
-		class Lock_Door : Open_inner
+		class Lock_Door : Raise_Bridge
 		{
 			displayName = $STR_EPOCH_ACTION_LOCK_BRIDGE;
-			condition = "(this animationPhase ""Open_outer"" == 0) and (this animationPhase ""Open_latch"" == 1)";
+			condition = "(this animationPhase ""Open_outer"" == 1) and (this animationPhase ""Open_latch"" == 1)";
 			statement = "PVDZE_handleSafeGear = [player,this,4];publicVariableServer ""PVDZE_handleSafeGear"";this animate [""Open_latch"", 0]";
 		};
-		class Unlock_Door : Open_inner
+		class Unlock_Door : Raise_Bridge
 		{
 			displayName = $STR_EPOCH_ACTION_UNLOCK_BRIDGE;
-			condition = "(!keypadCancel and DZE_Lock_Door == (this getvariable['CharacterID','0'])) and (this animationPhase ""Open_outer"" == 0) and (this animationPhase ""Open_latch"" == 0)";
+			condition = "(!keypadCancel and DZE_Lock_Door == (this getvariable['CharacterID','0'])) and (this animationPhase ""Open_outer"" == 1) and (this animationPhase ""Open_latch"" == 0)";
 			statement = "this animate [""Open_latch"", 1];PVDZE_handleSafeGear = [player,this,5,DZE_Lock_Door];publicVariableServer ""PVDZE_handleSafeGear"";";
 		};
-		class Unlock_Door_Dialog : Open_inner
+		class Unlock_Door_Dialog : Raise_Bridge
 		{
 			displayName = $STR_EPOCH_ACTION_UNLOCK_BRIDGE;
 			condition = "!keypadCancel and DZE_Lock_Door != (this getvariable['CharacterID','0'])";
 			statement = "dayz_selectedDoor = this;DZE_topCombo = 0;DZE_midCombo = 0;DZE_botCombo = 0;if(DZE_doorManagement) then {createdialog ""DoorAccess"";} else {createdialog ""ComboLockUI"";};";
-		};
-	};
-	class DestructionEffects {
-		class Sound {
-			simulation = "sound";
-			type = "DestrHouse";
-			position = "destructionEffect1";
-			intensity = 1;
-			interval = 1;
-			lifeTime = 0.05;
-		};
-		class DestroyPhase1 {
-			simulation = "destroy";
-			type = "DelayedDestruction";
-			lifeTime = 2.5;
-			position = "";
-			intensity = 1;
-			interval = 1;
-		};
-		class DamageAround1 {
-			simulation = "damageAround";
-			type = "DamageAroundHouse";
-			position = "";
-			intensity = 0.1;
-			interval = 1;
-			lifeTime = 1;
 		};
 	};
 };
