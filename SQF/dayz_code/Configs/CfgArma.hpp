57ed3e77956529c130d959a3c0f445c90f209f89
AirwavesMan <AirwavesMan@users.noreply.github.com>
2021-01-20 14:35:29 +0100

    Fix unlimited stamina
    Thx Жека [Makaveli] for reporting

--- SQF/dayz_code/Configs/CfgArma.hpp
+++ SQF/dayz_code/Configs/CfgArma.hpp
@@ -148,7 +148,7 @@ class CfgAISkill {
 	aimingspeed[] = {0, 0, 1, 1};
 	commanding[] = {0, 0, 1, 1};
 	courage[] = {0, 0, 1, 1};
-	endurance[] = {0, 0, 1, 1};
+	endurance[] = {0, 0, 1, 0.1};
 	general[] = {0, 0, 1, 1};
 	reloadspeed[] = {0, 0, 1, 1};
 	spotdistance[] = {0, 0, 1, 1};
