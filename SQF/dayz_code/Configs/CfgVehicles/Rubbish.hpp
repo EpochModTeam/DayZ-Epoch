8c16cbd7f0b98df939aec4c59e155e4cea4332f0
seelenapparat <chaosveteran@gmx.de>
2022-01-02 11:00:22 +0100

    Added several new rubbish objects with gear
    For admins to use in custom events/ as roadblocks etc.

--- SQF/dayz_code/Configs/CfgVehicles/Rubbish.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Rubbish.hpp
@@ -63,3 +63,143 @@ class Rubbish5_DZE : Rubbish5
 	transportMaxWeapons = 20;
 	transportMaxBackpacks = 5;
 };
+class Misc_Backpackheap : Strategic 
+{
+	scope = 2;
+	model = "\Ca\misc\Misc_Backpackheap";
+	mapSize = 2;
+	displayName = $STR_EPOCH_PILE_RUBBISH;
+	vehicleClass = "DayZ Epoch Misc";
+};
+class Rubbish6_DZE : Misc_Backpackheap
+{
+	transportMaxMagazines = 80;
+	transportMaxWeapons = 20;
+	transportMaxBackpacks = 5;	
+};
+class Land_Misc_Rubble_EP1 : Strategic
+{
+	scope = 2;
+	 model = "\ca\Structures_E\Misc\Misc_Construction\Misc_Rubble_EP1";
+	mapSize = 2;
+	displayName = $STR_EPOCH_PILE_RUBBISH;
+	vehicleClass = "DayZ Epoch Misc";	
+};
+class Rubbish7_DZE : Land_Misc_Rubble_EP1
+{
+	transportMaxMagazines = 80;
+	transportMaxWeapons = 20;
+	transportMaxBackpacks = 5;	
+};
+class MAP_rubble_metal_plates_01 : Strategic
+{
+	scope = 2;
+	model = "\Ca\Structures\Ruins\rubble_metal_plates_01.p3d";
+	mapSize = 2;
+	displayName = $STR_EPOCH_PILE_RUBBISH;
+	vehicleClass = "DayZ Epoch Misc";	
+};
+class Rubbish8_DZE : MAP_rubble_metal_plates_01
+{
+	transportMaxMagazines = 80;
+	transportMaxWeapons = 20;
+	transportMaxBackpacks = 5;	
+};
+class MAP_rubble_metal_plates_03 : Strategic
+{
+	scope = 2;
+	model = "\Ca\Structures\Ruins\rubble_metal_plates_03.p3d";
+	mapSize = 2;
+	displayName = $STR_EPOCH_PILE_RUBBISH;
+	vehicleClass = "DayZ Epoch Misc";
+};
+class Rubbish9_DZE : MAP_rubble_metal_plates_03
+{
+	transportMaxMagazines = 80;
+	transportMaxWeapons = 20;
+	transportMaxBackpacks = 5;	
+};
+class MAP_rubble_bricks_01 : Strategic
+{
+	scope = 2;
+	model = "\Ca\Structures\Ruins\rubble_bricks_01.p3d";
+	mapSize = 2;
+	displayName = $STR_EPOCH_PILE_RUBBISH;
+	vehicleClass = "DayZ Epoch Misc";	
+};
+class Rubbish10_DZE : MAP_rubble_bricks_01
+{
+	transportMaxMagazines = 80;
+	transportMaxWeapons = 20;
+	transportMaxBackpacks = 5;	
+};
+class MAP_rubble_metal_plates_04 : Strategic
+{
+	scope = 2;
+	model = "\Ca\Structures\Ruins\rubble_metal_plates_04.p3d";
+	mapSize = 2;
+	displayName = $STR_EPOCH_PILE_RUBBISH;
+	vehicleClass = "DayZ Epoch Misc";	
+};
+class Rubbish11_DZE : MAP_rubble_metal_plates_04
+{
+	transportMaxMagazines = 80;
+	transportMaxWeapons = 20;
+	transportMaxBackpacks = 5;	
+};
+class Land_Misc_Garb_Heap_EP1 : Strategic
+{
+	scope = 2;
+	model = "\ca\Structures_E\Misc\Misc_Garbage\Misc_Garb_Heap_EP1";
+	mapSize = 2;
+	displayName = $STR_EPOCH_PILE_RUBBISH;
+	vehicleClass = "DayZ Epoch Misc";	
+};
+class Rubbish12_DZE : Land_Misc_Garb_Heap_EP1
+{
+	transportMaxMagazines = 80;
+	transportMaxWeapons = 20;
+	transportMaxBackpacks = 5;	
+};
+class MAP_rubble_wood_01 : Strategic
+{
+	scope = 2;
+	model = "\Ca\Structures\Ruins\rubble_wood_01.p3d";
+	mapSize = 2;
+	displayName = $STR_EPOCH_PILE_RUBBISH;
+	vehicleClass = "DayZ Epoch Misc";	
+};
+class Rubbish13_DZE : MAP_rubble_wood_01
+{
+	transportMaxMagazines = 80;
+	transportMaxWeapons = 20;
+	transportMaxBackpacks = 5;	
+};
+class MAP_rubble_wood_02 : Strategic
+{
+	scope = 2;
+	model = "\Ca\Structures\Ruins\rubble_wood_02.p3d";
+	mapSize = 2;
+	displayName = $STR_EPOCH_PILE_RUBBISH;
+	vehicleClass = "DayZ Epoch Misc";	
+};
+class Rubbish14_DZE : MAP_rubble_wood_02
+{
+	transportMaxMagazines = 80;
+	transportMaxWeapons = 20;
+	transportMaxBackpacks = 5;	
+};
+class MAP_Misc_TyreHeap : Strategic
+{
+	scope = 2;
+	model = "\CA\misc\Misc_TyreHeap.p3d";
+	mapSize = 2;
+	displayName = $STR_EPOCH_PILE_RUBBISH;
+	vehicleClass = "DayZ Epoch Misc";	
+};
+class Rubbish15_DZE : MAP_Misc_TyreHeap
+{
+	transportMaxMagazines = 80;
+	transportMaxWeapons = 20;
+	transportMaxBackpacks = 5;	
+};
