08c46540a0d1023938452f08cc37465d9e5005e6
seelenapparat <chaosveteran@gmx.de>
2022-01-02 10:49:16 +0100

    Fix for wrong description of Lee Enfield

--- SQF/dayz_code/Configs/CfgWeapons/Rifles/LeeEnfield.hpp
+++ SQF/dayz_code/Configs/CfgWeapons/Rifles/LeeEnfield.hpp
@@ -1,5 +1,6 @@
 class LeeEnfield_DZ : LeeEnfield
 {
+	descriptionShort = $STR_DZ_303BRITISH_DESC;	
 	model = "z\addons\dayz_communityweapons\LeeEnfield\LeeEnfield.p3d";
 	magazines[] = {10Rnd_303British};
 };
