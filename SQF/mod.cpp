68169c43106a692e885a086bed20d7b8d8f67f30
A Man <AirwavesMan@users.noreply.github.com>
2022-07-07 10:40:30 +0200

    Update mod.cpp

--- SQF/mod.cpp
+++ SQF/mod.cpp
@@ -1,5 +1,5 @@
 dir = "@DayZ_Epoch";
-name = "DayZ Epoch 1.0.7";
+name = "DayZ Epoch 1.0.7.1";
 picture = "z\addons\dayz_code\gui\loadingscreen.paa";
 actionName = "Website";
 action = "http://www.dayzepoch.com";
